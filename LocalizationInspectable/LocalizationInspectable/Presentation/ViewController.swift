a202f57af5973f412a3e4799492157f2656eb584
Bondar Yaroslav <bonyadmitr@gmail.com>
2018-03-31 10:03:57 +0300

    LocalizationInspectable

---
+++ LocalizationInspectable/LocalizationInspectable/Presentation/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  LocalizationInspectable
+//
+//  Created by Bondar Yaroslav on 17/04/2017.
+//  Copyright © 2017 Bondar Yaroslav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
