016ae9d725485f48de609aba9fc6ed23fc2e5ab1
Yaroslav Bondar <yaroslav.bondar@life.com.by>
2018-04-19 08:54:28 +0300

    added email regex

--- TextFieldHandlers/TextFieldHandlers/ViewController.swift
+++ TextFieldHandlers/TextFieldHandlers/ViewController.swift
@@ -35,10 +35,26 @@ extension ViewController: UITextFieldDelegate {
 }
 
 
+
+
+
 final class Validators {
     
+    /// https://www.regextester.com/
+    //
+    /// https://stackoverflow.com/a/39550723/5893286
+    //let regex = "(?:[\\p{L}0-9!#$%\\&'*+/=?\\^_`{|}~-]+(?:\\.[\\p{L}0-9!#$%\\&'*+/=?\\^_`{|}" +
+    //    "~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\" +
+    //    "x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[\\p{L}0-9](?:[a-" +
+    //    "z0-9-]*[\\p{L}0-9])?\\.)+[\\p{L}0-9](?:[\\p{L}0-9-]*[\\p{L}0-9])?|\\[(?:(?:25[0-5" +
+    //    "]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-" +
+    //    "9][0-9]?|[\\p{L}0-9-]*[\\p{L}0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21" +
+    //"-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])"
+    //
+    /// .*  - 0 or any number
+    /// .+  - 1 or any number
     /// .*@.*\..*
-   static func isEmail(_ email: String) -> Bool {
+    static func isEmail(_ email: String) -> Bool {
         let emailRegEx = "^.+@.+\\..{2,}$"
         return check(text: email, regEx: emailRegEx)
     }
