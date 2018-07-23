e12e2bb824fb5597a92559bb9991289f5b38a87c
Yaroslav Bondar <bonyadmitr@gmail.com>
2018-07-23 16:04:54 +0300

    added XCGLogger

--- Background/Background/ViewController.swift
+++ Background/Background/ViewController.swift
@@ -10,6 +10,55 @@ import UIKit
 import CoreLocation
 import MapKit
 
+import XCGLogger
+
+extension XCGLogger {
+    static func documentsFolderUrl(withComponent: String) -> URL {
+        let documentsUrls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return documentsUrls[documentsUrls.endIndex - 1].appendingPathComponent(withComponent)
+    }
+}
+
+// the global reference to logging mechanism to be available in all files
+let log: XCGLogger = {
+    let log = XCGLogger(identifier: "advancedLogger", includeDefaultDestinations: false)
+    
+    let logPath = XCGLogger.documentsFolderUrl(withComponent: "app.log")
+    
+    let autoRotatingFileDestination = AutoRotatingFileDestination(owner: log,
+                                                                  writeToFile: logPath,
+                                                                  identifier: "advancedLogger.fileDestination",
+                                                                  shouldAppend: true,
+                                                                  appendMarker: "-- Relaunched App --",
+                                                                  attributes: [.protectionKey : FileProtectionType.completeUntilFirstUserAuthentication],
+                                                                  maxFileSize: 5_242_880 * 10,
+                                                                  maxTimeInterval: 24 * 60 * 60 * 10,
+                                                                  archiveSuffixDateFormatter: nil)
+    autoRotatingFileDestination.outputLevel = .debug
+    autoRotatingFileDestination.showLogIdentifier = true
+    autoRotatingFileDestination.showFunctionName = true
+    autoRotatingFileDestination.showThreadName = true
+    autoRotatingFileDestination.showLevel = true
+    autoRotatingFileDestination.showFileName = true
+    autoRotatingFileDestination.showLineNumber = true
+    autoRotatingFileDestination.showDate = true
+    autoRotatingFileDestination.logQueue = XCGLogger.logQueue
+    
+    log.add(destination: autoRotatingFileDestination)
+    
+    log.logAppDetails()
+    
+    return log
+}()
+
+func debugLog(_ closure: @autoclosure () -> Any?, functionName: StaticString = #function, fileName: StaticString = #file, lineNumber: Int = #line, userInfo: [String: Any] = [:]) {
+    
+    log.logln(.debug, functionName: functionName, fileName: fileName, lineNumber: lineNumber, userInfo: userInfo, closure: closure)
+    
+    let resultString = String(describing: closure() ?? "nil") 
+    print(resultString)
+}
+
 class ViewController: UIViewController {
 
     @IBOutlet private weak var mapView: MKMapView! {
@@ -49,20 +98,19 @@ extension ViewController: LocationManagerDelegate {
         
         if let lastLocation = lastLocation {
             let distance = lastLocation.distance(from: location) /// meters
-            print("distance", distance)
+            debugLog("distance: \(distance)")
         }
         
         if let lastDate = lastDate {
             let newDate = Date()
             let timeDifference = -lastDate.timeIntervalSince(newDate)
-            print("timeDifference", timeDifference)
-            print()
+            debugLog("timeDifference: \(timeDifference)")
             self.lastDate = newDate
         } else {
             lastDate = Date()
         }
         
         lastLocation = location
-//        print(location)
+//        debugLog(location)
     }
 }
