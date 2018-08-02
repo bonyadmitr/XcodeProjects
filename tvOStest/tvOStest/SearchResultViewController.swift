02c2c50843ebe202acc10895a566b68b20420252
Yaroslav Bondar <bonyadmitr@gmail.com>
2018-08-02 11:24:54 +0300

    created tvOStest project

---
+++ tvOStest/tvOStest/SearchResultViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SearchResultViewController.swift
+//  tvOStest
+//
+//  Created by Bondar Yaroslav on 8/2/18.
+//  Copyright © 2018 Bondar Yaroslav. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultViewController: UIViewController {
+    
+    
+    
+}
---
+++ tvOStest/tvOStest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  tvOStest
+//
+//  Created by Bondar Yaroslav on 8/2/18.
+//  Copyright © 2018 Bondar Yaroslav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet private weak var testButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
+
+/// protocol UIFocusEnvironment
+extension ViewController {
+    override var preferredFocusEnvironments: [UIFocusEnvironment] {
+        return [testButton]
+    }
+}
