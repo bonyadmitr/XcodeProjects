d333093f3bc88e57d8872ea599a07458733f1bfa
Yaroslav Bondar <bonyadmitr@gmail.com>
2018-08-09 14:15:43 +0300

    cleared code

--- SplitController/SplitController/SplitController.swift
+++ SplitController/SplitController/SplitController.swift
@@ -34,28 +34,3 @@ class SplitController: UISplitViewController {
         }
     }
 }
-
-
-extension UISplitViewController {
-    func toggleDisplayMode() {
-        guard let mode = delegate?.targetDisplayModeForAction?(in: self) else {
-            toggleDisplayModeManual()
-            return
-        }
-        
-        /// 0.3 is too may for this animation
-        UIView.animate(withDuration: 0.2) {
-            self.preferredDisplayMode = mode
-        }
-    }
-    
-    func toggleDisplayModeManual() {
-        UIView.animate(withDuration: 0.2) {
-            if self.displayMode == .allVisible {
-                self.preferredDisplayMode = .primaryHidden
-            } else {
-                self.preferredDisplayMode = .allVisible
-            }
-        }
-    }
-}
