8b9ccbb89ed11c2857b4cd9bd7af0ff672a6daec
Yaroslav Bondar <bonyadmitr@gmail.com>
2018-08-20 08:56:57 +0300

    added new code for logger

--- Logger/Logger/Logger/Logger.swift
+++ Logger/Logger/Logger/Logger.swift
@@ -99,3 +99,73 @@ final public class Logger {
         print(res)
     }
 }
+
+// TODO: check all, create writing to file
+
+//func logPath() -> URL {
+//    let docs = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).last!
+//    return docs.appendingPathComponent("logger.txt")
+//}
+//
+//let logger = Logger(destination: logPath())
+//
+//class Logger {
+//    let destination: URL
+//    lazy fileprivate var dateFormatter: DateFormatter = {
+//        let formatter = DateFormatter()
+//        formatter.locale = Locale.current
+//        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss.SSS"
+//        
+//        return formatter
+//    }()
+//    lazy fileprivate var fileHandle: FileHandle? = {
+//        let path = self.destination.path
+//        FileManager.default.createFile(atPath: path, contents: nil, attributes: nil)
+//        
+//        do {
+//            let fileHandle = try FileHandle(forWritingTo: self.destination)
+//            print("Successfully logging to: \(path)")
+//            return fileHandle
+//        } catch let error as NSError {
+//            print("Serious error in logging: could not open path to log file. \(error).")
+//        }
+//        
+//        return nil
+//    }()
+//    
+//    init(destination: URL) {
+//        self.destination = destination
+//    }
+//    
+//    deinit {
+//        fileHandle?.closeFile()
+//    }
+//    
+//    func log(_ message: String, function: String = #function, file: String = #file, line: Int = #line) {
+//        let logMessage = stringRepresentation(message, function: function, file: file, line: line)
+//        
+//        printToConsole(logMessage)
+//        printToDestination(logMessage)
+//    }
+//}
+//
+//private extension Logger {
+//    func stringRepresentation(_ message: String, function: String, file: String, line: Int) -> String {
+//        let dateString = dateFormatter.string(from: Date())
+//        
+//        let file = URL(fileURLWithPath: file).lastPathComponent 
+//        return "\(dateString) [\(file):\(line)] \(function): \(message)\n"
+//    }
+//    
+//    func printToConsole(_ logMessage: String) {
+//        print(logMessage)
+//    }
+//    
+//    func printToDestination(_ logMessage: String) {
+//        if let data = logMessage.data(using: String.Encoding.utf8) {
+//            fileHandle?.write(data)
+//        } else {
+//            print("Serious error in logging: could not encode logged string into data.")
+//        }
+//    }
+//}
