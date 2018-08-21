d898009ad51c4321fa4c34cf066909b79b49a220
Yaroslav Bondar <bonyadmitr@gmail.com>
2018-08-21 09:00:37 +0300

    added showCheckmarkAlertSheet

--- iOSComponents/iOSComponents/ViewController.swift
+++ iOSComponents/iOSComponents/ViewController.swift
@@ -28,12 +28,90 @@ class ViewController: UIViewController {
         print(q)
         
         
-        
         print(Formatters.bytes(from: Device.totalDiskSpace))
         print(Formatters.bytes(from: Device.freeDiskSpace))
         print(Formatters.bytes(from: Device.usedDiskSpace))
     }
+    
+    @IBAction private func onCheckmarkAlertSheet(_ sender: UIButton) {
+        showCheckmarkAlertSheet()
+    }
 }
+
+// MARK: - showCheckmarkAlertSheet
+extension ViewController {
+    private func showCheckmarkAlertSheet() {
+        let vc = UIAlertController(title: "title", message: nil, preferredStyle: .actionSheet)
+        
+        let action1 = UIAlertAction(title: "one ✔︎", style: .default, handler: nil)
+        
+        let action2 = UIAlertAction(title: "two", style: .default, handler: nil)
+        action2.setValue(true, forKey: "checked")
+        
+        let action3 = UIAlertAction(title: "three", style: .default, handler: nil)
+        if let image = textToImage("✔︎") {
+            action3.setValue(image.withRenderingMode(.alwaysOriginal), forKey: "image")
+        }
+        
+        vc.addAction(action1)
+        vc.addAction(action2)
+        vc.addAction(action3)
+        
+        present(vc, animated: true, completion: nil)
+    }
+    
+    private func textToImage(_ text: String) -> UIImage? {
+        
+        //        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+        //        
+        //        let img = renderer.image { ctx in
+        //            // 2
+        //            let paragraphStyle = NSMutableParagraphStyle()
+        //            paragraphStyle.alignment = .center
+        //            
+        //            // 3
+        //            let attrs = [NSAttributedStringKey.font: UIFont(name: "HelveticaNeue-Thin", size: 36)!, NSAttributedStringKey.paragraphStyle: paragraphStyle]
+        //            
+        //            // 4
+        //            let string = "The best-laid schemes o'\nmice an' men gang aft agley"
+        //            string.draw(with: CGRect(x: 32, y: 32, width: 448, height: 448), options: .usesLineFragmentOrigin, attributes: attrs, context: nil)
+        //            
+        //            // 5
+        //            let mouse = UIImage(named: "mouse")
+        //            mouse?.draw(at: CGPoint(x: 300, y: 150))
+        //        }
+        //        
+        //        // 6
+        //        imageView.image = img
+        
+        
+        let imageSize = CGSize(width: 40, height: 40)
+        let textColor = UIColor.defaultBlue
+        let textFont = UIFont(name: "Helvetica Bold", size: 20)!
+        
+        UIGraphicsBeginImageContextWithOptions(imageSize, false, UIScreen.main.scale)
+        
+        //        let paragraphStyle = NSMutableParagraphStyle()
+        //        paragraphStyle.alignment = .left
+        //        .paragraphStyle: paragraphStyle
+        
+        let textAttributes: [NSAttributedStringKey: Any] = [.font: textFont,
+                                                            .foregroundColor: textColor]
+        
+        let textSize = (text as NSString).size(withAttributes: textAttributes)
+        
+        let startPoint = CGPoint(x: textSize.width / 2, y: textSize.height / 2)
+        
+        let rect = CGRect(origin: startPoint, size: imageSize)
+        text.draw(in: rect, withAttributes: textAttributes)
+        
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage
+    }
+}
+
 
 func roundAppCorners() {
     /// CHECK with animations
