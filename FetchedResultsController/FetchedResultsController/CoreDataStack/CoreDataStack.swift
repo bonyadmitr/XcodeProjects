8fedddc1f982613022fb77bedf32444cd192e9f9
Yaroslav Bondar <bonyadmitr@gmail.com>
2018-09-18 09:56:14 +0300

    cleared CoreDataStack

--- FetchedResultsController/FetchedResultsController/CoreDataStack/CoreDataStack.swift
+++ FetchedResultsController/FetchedResultsController/CoreDataStack/CoreDataStack.swift
@@ -9,18 +9,47 @@
 import UIKit
 import CoreData
 
-
-//typealias ManagedObjectSaveStatusHandler = (ManagedObjectSaveStatus) -> Void
-//
-//enum ManagedObjectSaveStatus {
-//    case saved
-//    case error(Error)
-//}
-
 /// pass "*.xcdatamodeld" name 
 /// in "private lazy var persistentContainer: NSPersistentContainer"
 /// and "private lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator?"
 final class CoreDataStack: NSObject {
+    
+    // MARK: Public methods
+    
+    static let shared = CoreDataStack()
+    
+    var newBackgroundContext: NSManagedObjectContext {
+        if #available(iOS 10.0, *) {
+            /// don't set parent for newBackgroundContext(), it will crash
+            /// with error "Context already has a coordinator; cannot replace"
+            return persistentContainer.newBackgroundContext()
+        } else {
+            let context = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)
+            context.parent = mainContext
+            return context
+        }
+    }
+    
+    var mainContext: NSManagedObjectContext {
+        if #available(iOS 10.0, *) {
+            return persistentContainer.viewContext
+        } else {
+            return managedObjectContext
+        }
+    }
+    
+    func performBackgroundTask(_ block: @escaping (NSManagedObjectContext) -> Void) {
+        if #available(iOS 10.0, *) {
+            persistentContainer.performBackgroundTask(block)
+        } else {
+            let context = newBackgroundContext
+            context.perform { 
+                block(context)
+            }
+        }
+    }
+    
+    // MARK: Private methods
     
     private override init() {
         super.init()
@@ -30,16 +59,13 @@ final class CoreDataStack: NSObject {
         } else {
             NotificationCenter.default.addObserver(self, selector: #selector(managedObjectContextDidSave), name: .NSManagedObjectContextDidSave, object: nil)
         }
-        
-        /// for tests
-        //NotificationCenter.default.addObserver(self, selector: #selector(managedObjectContextDidSave), name: .NSManagedObjectContextDidSave, object: nil)
     }
     
     deinit {
         NotificationCenter.default.removeObserver(self)
     }
     
-    @objc func managedObjectContextDidSave(_ notification: Notification) {
+    @objc private func managedObjectContextDidSave(_ notification: Notification) {
         guard let context = notification.object as? NSManagedObjectContext else {
             return
         }
@@ -78,62 +104,4 @@ final class CoreDataStack: NSObject {
         return managedObjectContext
     }()
     ///---
-    
-    // MARK: Public methods
-    
-    static let shared = CoreDataStack()
-    
-    var newBackgroundContext: NSManagedObjectContext {
-        if #available(iOS 10.0, *) {
-            /// don't set parent for newBackgroundContext(), it will crash
-            /// with error "Context already has a coordinator; cannot replace"
-            return persistentContainer.newBackgroundContext()
-        } else {
-            let context = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)
-            context.parent = mainContext
-            return context
-        }
-        /// for tests
-//        let context = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)
-//        context.parent = mainContext
-//        return context
-    }
-    
-    var mainContext: NSManagedObjectContext {
-        if #available(iOS 10.0, *) {
-            return persistentContainer.viewContext
-        } else {
-            return managedObjectContext
-        }
-    }
-    
-    /// for background fetch
-    //lazy var backgroundContext = newBackgroundContext
-    
-    func performBackgroundTask(_ block: @escaping (NSManagedObjectContext) -> Void) {
-        if #available(iOS 10.0, *) {
-            persistentContainer.performBackgroundTask(block)
-        } else {
-            let context = newBackgroundContext
-            context.perform { 
-                block(context)
-            }
-        }
-    }
 }
-
-
-/// iOS 10 fetchRequest.execute() test
-//func execute2 <T>(fetchRequest: NSFetchRequest<T>) {
-//    let context = CoreDataStack.shared.backgroundContext
-//    context.perform {
-//        if #available(iOS 10.0, *) {
-//            let q = try? fetchRequest.execute()
-//            print("---", q?.count)
-//        } else {
-//            let q = try? context.fetch(fetchRequest)
-//            print("---", q?.count)
-//        }
-//    }
-//}
-
