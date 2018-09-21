c420229bb407a58f3562624bff071dc6340ecf53
Yaroslav Bondar <bonyadmitr@gmail.com>
2018-09-21 11:47:36 +0300

    created ContactsManager project

---
+++ ContactsManager/ContactsManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactsManager
+//
+//  Created by Bondar Yaroslav on 9/21/18.
+//  Copyright © 2018 Bondar Yaroslav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
+
