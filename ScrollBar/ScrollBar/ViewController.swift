53d324ee5dc4b138d050821e6ae54881ee282556
Yaroslav Bondar <bonyadmitr@gmail.com>
2018-09-25 11:33:42 +0300

    created project ScrollBar

---
+++ ScrollBar/ScrollBar/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  ScrollBar
+//
+//  Created by Bondar Yaroslav on 9/25/18.
+//  Copyright © 2018 Bondar Yaroslav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet private weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        cell.textLabel?.text = "Row \(indexPath.row + 1)"
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
