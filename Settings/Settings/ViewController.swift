f4102f5877245d3e0a0fe67ab92f3d8d10d73f2f
Yaroslav Bondar <bonyadmitr@gmail.com>
2018-09-27 15:49:41 +0300

    created Settings project

---
+++ Settings/Settings/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Settings
+//
+//  Created by Bondar Yaroslav on 9/27/18.
+//  Copyright © 2018 Bondar Yaroslav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
+
