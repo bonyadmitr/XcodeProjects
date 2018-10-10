69ac04fb3b6a2a144865e0783c87915b5f7c959d
Yaroslav Bondar <bonyadmitr@gmail.com>
2018-10-10 15:47:59 +0300

    added todo

--- Settings/Settings/App/AppDelegate.swift
+++ Settings/Settings/App/AppDelegate.swift
@@ -11,6 +11,7 @@ import UIKit
 // TODO: fix restoration layout in appearnce
 // TODO: there is degrease performance after set theme or language a lot of times
 
+// TODO: large titles support
 // TODO: window round coners 
 // TODO: swiftgen strings file
 // TODO: UIKeyCommand (from SplitController project)
