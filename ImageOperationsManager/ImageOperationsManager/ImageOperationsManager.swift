5542ce2263a91360d36c321c9f10a68cd77dc702
Yaroslav Bondar <bonyadmitr@gmail.com>
2018-10-13 17:35:45 +0300

    created logic with 3 operations

--- ImageOperationsManager/ImageOperationsManager/ImageOperationsManager.swift
+++ ImageOperationsManager/ImageOperationsManager/ImageOperationsManager.swift
@@ -15,53 +15,88 @@ final class ImageOperationsManager {
     lazy var downloadQueue: OperationQueue = {
         var queue = OperationQueue()
         //queue.name = "Download queue"
-        //queue.maxConcurrentOperationCount = 1
+//        queue.maxConcurrentOperationCount = 1
         queue.qualityOfService = .userInteractive
         return queue
     }()
     
+    private let queue = DispatchQueue(label: "qweqweqwe")
+    
     private var inProgressOperations = [URL: Operation]()
+    private var inProgressBlurOperations = [URL: Operation]()
     
     
     func load(webPhoto: WebPhoto, completion: @escaping (WebPhoto, UIImage?) -> Void) {
+//        queue.async {
+            self.load2(webPhoto: webPhoto, completion: completion)
+//        }
+    }
+    
+    func load2(webPhoto: WebPhoto, completion: @escaping (WebPhoto, UIImage?) -> Void) {
         
-        if let image = ImageDownloaderOperation.cache.object(forKey: webPhoto.url.absoluteString as NSString) {
-            completion(webPhoto, image)
-            return
-        }
+//        if let image = ImageDownloaderOperation.cache.object(forKey: webPhoto.url.absoluteString as NSString) {
+//            completion(webPhoto, image)
+//            return
+//        }
+//        
+//        if let image = ImageDownloaderOperation.cache.object(forKey: webPhoto.thumbnailUrl.absoluteString as NSString) {
+//            completion(webPhoto, image)
+//            
+//            
+//            let urlOperation = ImageDownloaderOperation(url: webPhoto.url)
+//            urlOperation.completionBlock = { [unowned urlOperation] in
+//                if urlOperation.isCancelled {
+//                    return
+//                }
+//                self.inProgressOperations.removeValue(forKey: webPhoto.url)
+//                completion(webPhoto, urlOperation.image)
+//            }
+//            
+//            inProgressOperations[webPhoto.url] = urlOperation
+//            downloadQueue.addOperation(urlOperation)
+//            
+//            return
+//        }
         
-        if let image = ImageDownloaderOperation.cache.object(forKey: webPhoto.thumbnailUrl.absoluteString as NSString) {
-            completion(webPhoto, image)
-            
-            
-            let urlOperation = ImageDownloaderOperation(url: webPhoto.url)
-            urlOperation.completionBlock = {
-                if urlOperation.isCancelled {
-                    return
-                }
-                self.inProgressOperations.removeValue(forKey: webPhoto.url)
-                completion(webPhoto, urlOperation.image)
-            }
-            
-            inProgressOperations[webPhoto.url] = urlOperation
-            downloadQueue.addOperation(urlOperation)
-            
-            return
-        }
+        let blurOperation = ImageBlurOperation()
         
         let thumbnailOperation = ImageDownloaderOperation(url: webPhoto.thumbnailUrl)
-        thumbnailOperation.completionBlock = {
+        thumbnailOperation.queuePriority = .veryHigh
+        thumbnailOperation.completionBlock = { [weak blurOperation] in
             if thumbnailOperation.isCancelled {
                 return
             }
+            blurOperation?.inputImage = thumbnailOperation.image
             self.inProgressOperations.removeValue(forKey: webPhoto.thumbnailUrl)
             completion(webPhoto, thumbnailOperation.image)
         }
         inProgressOperations[webPhoto.thumbnailUrl] = thumbnailOperation
         downloadQueue.addOperation(thumbnailOperation)
         
+        
+        
+        blurOperation.addDependency(thumbnailOperation)
+        thumbnailOperation.queuePriority = .normal
+        blurOperation.completionBlock = {
+            if blurOperation.isCancelled {
+                return
+            }
+            self.inProgressBlurOperations.removeValue(forKey: webPhoto.thumbnailUrl)
+            if blurOperation.resultImage == nil {
+                return
+            }
+            
+            completion(webPhoto, blurOperation.resultImage)
+        }
+        inProgressBlurOperations[webPhoto.thumbnailUrl] = blurOperation
+        downloadQueue.addOperation(blurOperation)
+        
+        
+        
         let urlOperation = ImageDownloaderOperation(url: webPhoto.url)
         urlOperation.addDependency(thumbnailOperation)
+        urlOperation.addDependency(blurOperation)
+        thumbnailOperation.queuePriority = .veryLow
         urlOperation.completionBlock = {
             if urlOperation.isCancelled {
                 return
@@ -69,18 +104,40 @@ final class ImageOperationsManager {
             self.inProgressOperations.removeValue(forKey: webPhoto.url)
             completion(webPhoto, urlOperation.image)
         }
+        
         inProgressOperations[webPhoto.url] = urlOperation
-        downloadQueue.addOperation(urlOperation)
+//        downloadQueue.addOperation(urlOperation)
+        
+        downloadQueue.addOperations([urlOperation], waitUntilFinished: false)
     }
     
     func cancel(webPhoto: WebPhoto) {
-//        inProgressOperations[webPhoto.thumbnailUrl]?.cancel()
-//        inProgressOperations[webPhoto.url]?.cancel()
+//        queue.async {
+            
+            //        inProgressOperations[webPhoto.thumbnailUrl]?.cancel()
+            //        inProgressOperations[webPhoto.url]?.cancel()
+            
+            //        inProgressOperations[webPhoto.thumbnailUrl]!.cancel()
+            self.inProgressOperations.removeValue(forKey: webPhoto.thumbnailUrl)?.cancel()
+            //        inProgressOperations[webPhoto.url]!.cancel()
+            self.inProgressBlurOperations.removeValue(forKey: webPhoto.thumbnailUrl)?.cancel()
+            self.inProgressOperations.removeValue(forKey: webPhoto.url)?.cancel()
+            
+            
         
-//        inProgressOperations[webPhoto.thumbnailUrl]!.cancel()
-        inProgressOperations.removeValue(forKey: webPhoto.thumbnailUrl)?.cancel()
-//        inProgressOperations[webPhoto.url]!.cancel()
-        inProgressOperations.removeValue(forKey: webPhoto.url)?.cancel()
+//        if let op1 = self.inProgressOperations.removeValue(forKey: webPhoto.url) {
+//            op1.cancel()
+//            
+//            if let op2 = self.inProgressBlurOperations.removeValue(forKey: webPhoto.thumbnailUrl) {
+//                op2.cancel()
+//                op1.removeDependency(op2)
+//            } 
+//            
+//            
+//        }
+//        
+//        }
+
     }
 }
 
@@ -133,6 +190,92 @@ final class ImageDownloaderOperation: Operation {
         super.cancel()
         task?.cancel()
         semaphore.signal()
+    }
+}
+
+final class ImageBlurOperation: Operation {
+    
+    var inputImage: UIImage?
+    var resultImage: UIImage?
+    
+    override func main() {
+        guard !isCancelled, let image = inputImage else {
+            return
+        }
+        
+        //resultImage = blurEffect(image: image)
+        resultImage = image.grayScaleImage
+    }
+    
+    private func blurEffect(image: UIImage) -> UIImage? {
+        
+        guard
+            let gaussianFilter = CIFilter(name: "CIGaussianBlur"),
+            let cropFilter = CIFilter(name: "CICrop"),
+            let inputImage = CIImage(image: image),
+            !isCancelled
+        else {
+            return nil
+        }
+        
+        gaussianFilter.setValue(1, forKey: kCIInputRadiusKey)
+        gaussianFilter.setValue(inputImage, forKey: kCIInputImageKey)
+        
+        guard !isCancelled, let gaussianImage = gaussianFilter.outputImage else {
+            return nil
+        }
+        
+        cropFilter.setValue(gaussianImage, forKey: kCIInputImageKey)
+        cropFilter.setValue(CIVector(cgRect: inputImage.extent), forKey: "inputRectangle")
+        
+        
+        guard !isCancelled, let croppedImage = cropFilter.outputImage else {
+            return nil
+        }
+        
+        return UIImage(ciImage: croppedImage)
+    }
+}
+
+extension UIImage {
+    func mask(with color: UIColor) -> UIImage? {
+        UIGraphicsBeginImageContextWithOptions(size, false, 0)
+        guard let context = UIGraphicsGetCurrentContext(), let cgImage = cgImage else {
+            return nil
+        }
+        color.setFill()
+        context.translateBy(x: 0, y: size.height)
+        context.scaleBy(x: 1.0, y: -1.0)
+        context.setBlendMode(.normal)
+        let rect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+        context.draw(cgImage, in: rect)
+        context.clip(to: rect, mask: cgImage)
+        context.addRect(rect)
+        context.drawPath(using: .fill)
+        let coloredImg = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return coloredImg
+    }
+    
+    var grayScaleImage: UIImage? {
+        /// Selim's method
+        guard let cgImage = cgImage,
+            let context = CGContext(data: nil,
+                                    width: Int(size.width),
+                                    height: Int(size.height),
+                                    bitsPerComponent: 8,
+                                    bytesPerRow: 0,
+                                    space: CGColorSpaceCreateDeviceGray(),
+                                    bitmapInfo: CGImageAlphaInfo.none.rawValue)
+            else { return nil }
+        
+        let imageRect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+        context.draw(cgImage, in: imageRect)
+        
+        guard let imageRef = context.makeImage() else {
+            return nil
+        }
+        return UIImage(cgImage: imageRef)
     }
 }
 
