63939a4bccaa273d7cc698374aa9b1318d4952e0
Yaroslav Bondar <bonyadmitr@gmail.com>
2018-10-13 12:14:56 +0300

    added CollectionController

--- ImageOperationsManager/ImageOperationsManager/ViewController.swift
+++ ImageOperationsManager/ImageOperationsManager/ViewController.swift
@@ -8,18 +8,171 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class CollectionController: UIViewController {
+    
+    @IBOutlet private weak var collectionView: UICollectionView! {
+        willSet {
+            newValue.dataSource = self
+            newValue.delegate = self
+            newValue.register(ImageTextColCell.self, forCellWithReuseIdentifier: "ImageTextColCell")
+            newValue.register(CollectionHeader.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: "CollectionHeader")
+            
+            newValue.alwaysBounceVertical = true
+            
+            if let layout = newValue.collectionViewLayout as? UICollectionViewFlowLayout {
+                layout.minimumLineSpacing = 1
+                layout.minimumInteritemSpacing = 1
+                layout.sectionInset = .init(top: 1, left: 1, bottom: 1, right: 1)
+            }
+        }
+    }
+    
+    private var items = [[Date]]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let initialDate = Date()
+        
+        items = [
+            (0...10).map({ initialDate.addingTimeInterval(TimeInterval(3600 * 24 * $0)) }),
+            (30...45).map({ initialDate.addingTimeInterval(TimeInterval(3600 * 24 * $0)) }),
+            (70...90).map({ initialDate.addingTimeInterval(TimeInterval(3600 * 24 * $0)) })
+        ]
+        
+        collectionView.reloadData()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        collectionView.collectionViewLayout.invalidateLayout()
     }
-
-
 }
 
+extension CollectionController: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return items.count
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return items[section].count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return collectionView.dequeueReusableCell(withReuseIdentifier: "ImageTextColCell", for: indexPath)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        return collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "CollectionHeader", for: indexPath)
+    }
+}
+
+extension CollectionController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        guard let cell = cell as? ImageTextColCell else {
+            assertionFailure()
+            return
+        }
+        let date = items[indexPath.section][indexPath.row]
+        cell.textLabel.text = "\(date)"
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, willDisplaySupplementaryView view: UICollectionReusableView, forElementKind elementKind: String, at indexPath: IndexPath) {
+        
+        /// fixing iOS11 UICollectionSectionHeader clipping scroll indicator
+        /// https://stackoverflow.com/a/46930410/5893286
+        if #available(iOS 11.0, *), elementKind == UICollectionElementKindSectionHeader {
+            view.layer.zPosition = 0
+        }
+        
+        guard let view = view as? CollectionHeader else {
+            return
+        }
+        let date = items[indexPath.section][indexPath.row]
+        view.textLabel.text = "\(date)"
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(indexPath)
+    }
+}
+
+extension CollectionController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = collectionView.bounds.width / 4 - 3
+        return CGSize(width: width, height: width)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: 0, height: 44)
+    }
+}
+
+final class ImageTextColCell: UICollectionViewCell {
+    
+    let imageView = UIImageView()
+    let textLabel = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    private func setup() {
+        backgroundColor = UIColor.lightGray
+        
+        imageView.frame = bounds
+        imageView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        
+        textLabel.frame = bounds
+        textLabel.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        
+        textLabel.textAlignment = .center
+        textLabel.font = UIFont.systemFont(ofSize: 14)
+        textLabel.numberOfLines = 0
+        
+        addSubview(imageView)
+        addSubview(textLabel)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        textLabel.frame = bounds
+        imageView.frame = bounds
+    }
+}
+
+final class CollectionHeader: UICollectionReusableView {
+    
+    let textLabel = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    private func setup() {
+        backgroundColor = UIColor.darkGray
+        
+        textLabel.frame = bounds
+        textLabel.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        textLabel.textAlignment = .left
+        textLabel.font = UIFont.systemFont(ofSize: 14)
+        
+        addSubview(textLabel)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        textLabel.frame = bounds
+    }
+}
