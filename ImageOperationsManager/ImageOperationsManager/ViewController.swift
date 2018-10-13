c62fbb9336482a8e3d361ad5c775a1474abdafaa
Yaroslav Bondar <bonyadmitr@gmail.com>
2018-10-13 12:11:09 +0300

    created ImageOperationsManager

---
+++ ImageOperationsManager/ImageOperationsManager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageOperationsManager
+//
+//  Created by Bondar Yaroslav on 10/13/18.
+//  Copyright © 2018 Bondar Yaroslav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
