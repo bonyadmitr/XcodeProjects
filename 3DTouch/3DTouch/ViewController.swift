874a64d02ecaab597d3888caaa375e49868b1acb
Yaroslav Bondar <bonyadmitr@gmail.com>
2018-11-05 08:55:30 +0300

    handleForceTouch

--- 3DTouch/3DTouch/ViewController.swift
+++ 3DTouch/3DTouch/ViewController.swift
@@ -15,5 +15,37 @@ class ViewController: UIViewController {
         
         
     }
+    
+    public override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesMoved(touches, with: event)
+        
+        guard let touch = touches.first else {
+            assertionFailure()
+            return
+        }
+        
+        handleForceTouch(touch)
+    }
+    
+    private func handleForceTouch(_ touch: UITouch) {
+        guard #available(iOS 9.0, *), traitCollection.forceTouchCapability == .available else {
+            return
+        }
+        
+        if touch.force >= touch.maximumPossibleForce {
+            print("385+ grams")
+            //forceLabel.text = "385+ grams"
+        } else {
+            let force = touch.force / touch.maximumPossibleForce
+            let grams = force * 385
+            let roundGrams = Int(grams)
+            print("\(roundGrams) grams")
+        }
+    }
+    
+    public override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
+        print("0 gram")
+    }
 }
 
