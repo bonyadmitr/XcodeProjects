9bb8ce5adc35e74dd4aad69c741ab1172c30d121
Yaroslav Bondar <bonyadmitr@gmail.com>
2018-11-13 12:26:21 +0300

    cleared code

--- Settings/Settings/Controllers/AboutController.swift
+++ Settings/Settings/Controllers/AboutController.swift
@@ -142,14 +142,14 @@ extension AboutController: UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
-        guard let view = view as? AboutHeader else {
+        guard let header = view as? AboutHeader else {
             assertionFailure()
             return
         }
         
-        view.label.text = L10n.settings
-        view.imageView.image = #imageLiteral(resourceName: "ic_settings")
-        view.versionLabel.text = UIApplication.shared.version
+        header.label.text = L10n.settings
+        header.imageView.image = #imageLiteral(resourceName: "ic_settings")
+        header.versionLabel.text = UIApplication.shared.version
     }
     
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
@@ -174,92 +174,5 @@ extension AboutController: UITableViewDelegate {
             let vc = DeveloperAppsController()
             navigationController?.pushViewController(vc, animated: true)
         }
-    }
-}
-
-final class AboutHeader: UITableViewHeaderFooterView {
-    
-    let imageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.contentMode = .scaleToFill
-        return imageView
-    }()
-    
-    let label: UILabel = {
-        let label = UILabel()
-        label.font = UIFont.preferredFont(forTextStyle: .title3)
-        label.textAlignment = .center
-        return label
-    }()
-    
-    let versionLabel: UILabel = {
-        let label = UILabel()
-        label.font = UIFont.preferredFont(forTextStyle: .body)
-        label.textAlignment = .center
-        return label
-    }()
-    
-    /// for UIAppearance (not working with UIViews)
-    @objc dynamic var color: UIColor = .black {
-        didSet {
-            imageView.tintColor = color /// can be used window.tintColor (remove this set)
-            label.textColor = color
-            versionLabel.textColor = color
-        }
-    }
-    
-    override init(reuseIdentifier: String?) {
-        super.init(reuseIdentifier: reuseIdentifier)
-        setup()
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        setup()
-    }
-    
-    private func setup() {
-//        if #available(iOS 11.0, *) {
-//            safeAreaLayoutGuide.topAnchor
-//            directionalLayoutMargins
-//        } else {
-//        }
-        
-        /// addSubview before activate constraints
-        addSubview(label)
-        addSubview(versionLabel)
-        addSubview(imageView)
-        
-        let constant: CGFloat = 8
-        
-        label.translatesAutoresizingMaskIntoConstraints = false
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        versionLabel.translatesAutoresizingMaskIntoConstraints = false
-        
-        /// defailt constraint priority is 1000
-        imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor).isActive = true
-        imageView.topAnchor.constraint(equalTo: topAnchor, constant: constant).isActive = true
-        imageView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
-        imageView.setContentHuggingPriority(UILayoutPriority(250), for: .vertical)
-        //imageView.heightAnchor.constraint(equalToConstant: 100).isActive = true
-        //imageView.setContentCompressionResistancePriority(UILayoutPriority(100), for: .vertical)
-        //layoutMargins.bottom
-        
-        label.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
-        label.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
-        label.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: constant).isActive = true
-        label.bottomAnchor.constraint(equalTo: versionLabel.topAnchor, constant: -constant).isActive = true
-        label.setContentHuggingPriority(UILayoutPriority(251), for: .vertical)
-        
-        versionLabel.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
-        versionLabel.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
-        versionLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -constant).isActive = true
-        versionLabel.setContentHuggingPriority(UILayoutPriority(252), for: .vertical)
-    }
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        
-        
     }
 }
