19c8121df5b0e5d971adad50c9af119911de6d7b
Yaroslav Bondar <bonyadmitr@gmail.com>
2018-11-13 10:39:14 +0300

    updated swiftgen template for force localization

--- Settings/Settings/App/L10n.swift
+++ Settings/Settings/App/L10n.swift
@@ -8,47 +8,89 @@ import Foundation
 // swiftlint:disable explicit_type_interface identifier_name line_length nesting type_body_length type_name
 enum L10n {
   /// About
-  static let about = L10n.tr("Localizable", "about")
+  static var about: String {
+    return L10n.tr("Localizable", "about")
+  }
   /// appearance
-  static let appearance = L10n.tr("Localizable", "Appearance")
+  static var appearance: String {
+    return L10n.tr("Localizable", "Appearance")
+  }
   /// checked
-  static let checked = L10n.tr("Localizable", "checked")
+  static var checked: String {
+    return L10n.tr("Localizable", "checked")
+  }
   /// Developer Apps
-  static let developerApps = L10n.tr("Localizable", "developer_apps")
+  static var developerApps: String {
+    return L10n.tr("Localizable", "developer_apps")
+  }
   /// Do you like the app?
-  static let doYouLikeApp = L10n.tr("Localizable", "do_you_like_app")
+  static var doYouLikeApp: String {
+    return L10n.tr("Localizable", "do_you_like_app")
+  }
   /// Don't show it again
-  static let dontShowItAgain = L10n.tr("Localizable", "dont_show_it_again")
+  static var dontShowItAgain: String {
+    return L10n.tr("Localizable", "dont_show_it_again")
+  }
   /// Full screen
-  static let fullScreen = L10n.tr("Localizable", "full_screen")
+  static var fullScreen: String {
+    return L10n.tr("Localizable", "full_screen")
+  }
   /// Installed
-  static let installed = L10n.tr("Localizable", "installed")
+  static var installed: String {
+    return L10n.tr("Localizable", "installed")
+  }
   /// Language
-  static let language = L10n.tr("Localizable", "language")
+  static var language: String {
+    return L10n.tr("Localizable", "language")
+  }
   /// More apps from me
-  static let moreAppsFromMe = L10n.tr("Localizable", "more_apps_from_me")
+  static var moreAppsFromMe: String {
+    return L10n.tr("Localizable", "more_apps_from_me")
+  }
   /// More apps from me at App Store
-  static let moreAppsFromMeAppStore = L10n.tr("Localizable", "more_apps_from_me_app_store")
+  static var moreAppsFromMeAppStore: String {
+    return L10n.tr("Localizable", "more_apps_from_me_app_store")
+  }
   /// New apps
-  static let newApps = L10n.tr("Localizable", "new_apps")
+  static var newApps: String {
+    return L10n.tr("Localizable", "new_apps")
+  }
   /// Open in App Store
-  static let openInAppStore = L10n.tr("Localizable", "open_in_app_store")
+  static var openInAppStore: String {
+    return L10n.tr("Localizable", "open_in_app_store")
+  }
   /// Privacy Policy
-  static let privacyPolicy = L10n.tr("Localizable", "privacy_policy")
+  static var privacyPolicy: String {
+    return L10n.tr("Localizable", "privacy_policy")
+  }
   /// Rate now
-  static let rateNow = L10n.tr("Localizable", "rate_now")
+  static var rateNow: String {
+    return L10n.tr("Localizable", "rate_now")
+  }
   /// Rate Us
-  static let rateUs = L10n.tr("Localizable", "rate_us")
+  static var rateUs: String {
+    return L10n.tr("Localizable", "rate_us")
+  }
   /// Rate us, please, to share it with others!
-  static let rateUsShareIt = L10n.tr("Localizable", "rate_us_share_it")
+  static var rateUsShareIt: String {
+    return L10n.tr("Localizable", "rate_us_share_it")
+  }
   /// Remind me later
-  static let remindMeLater = L10n.tr("Localizable", "remind_me_later")
+  static var remindMeLater: String {
+    return L10n.tr("Localizable", "remind_me_later")
+  }
   /// Send feedback
-  static let sendFeedback = L10n.tr("Localizable", "send_feedback")
+  static var sendFeedback: String {
+    return L10n.tr("Localizable", "send_feedback")
+  }
   /// Settings
-  static let settings = L10n.tr("Localizable", "settings")
+  static var settings: String {
+    return L10n.tr("Localizable", "settings")
+  }
   /// Support
-  static let support = L10n.tr("Localizable", "support")
+  static var support: String {
+    return L10n.tr("Localizable", "support")
+  }
 }
 // swiftlint:enable explicit_type_interface identifier_name line_length nesting type_body_length type_name
 
