0e60e7130409a1f3357adf338c34eb17fad1ff25
Yaroslav Bondar <bonyadmitr@gmail.com>
2018-11-26 16:35:57 +0300

    added FIXME

--- Settings/Settings/App/AppDelegate.swift
+++ Settings/Settings/App/AppDelegate.swift
@@ -37,8 +37,9 @@ import UIKit
 // There is no registration (Registration don't need)
 // Using latest version of Swift for best performance of app
 
+// FIXME: cell height. iOS 10. steps: settings screen - scroll to bottom - landscape - portrait
+// FIXME: window tintColor for standart controllers
 // TODO: clear appDelegate
-// TODO: fix window tintColor for standart controllers
 // TODO: check all present on iPad (share app)
 // TODO: Copyable version label
 // TODO: custom cell with switcher
