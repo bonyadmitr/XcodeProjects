0fc21e5fa62ac97734e67fe5fe1a1dab508174da
Yaroslav Bondar <bonyadmitr@gmail.com>
2018-11-28 16:56:57 +0300

    created project UnitTestsCoreData

---
+++ UnitTestsCoreData/UnitTestsCoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UnitTestsCoreData
+//
+//  Created by Bondar Yaroslav on 11/28/18.
+//  Copyright © 2018 Bondar Yaroslav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
