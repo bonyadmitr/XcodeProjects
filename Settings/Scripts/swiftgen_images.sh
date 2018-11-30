3c63674ef0fa615fffd9521db1d7d7ef118d8b57
Yaroslav Bondar <bonyadmitr@gmail.com>
2018-11-30 16:00:52 +0300

    * created swiftgen_images.sh script * created images_swift4.stencil swiftgen template

---
+++ Settings/Scripts/swiftgen_images.sh
@@ -0,0 +1,13 @@
+## add to xcode run script
+# /bin/sh ./Scripts/swiftgen.sh
+
+## without pod
+if which swiftgen >/dev/null; then
+    swiftgen xcassets "$PROJECT_DIR/Settings/App/Assets.xcassets" --output "$PROJECT_DIR/Settings/App/Images.swift"  -p "$PROJECT_DIR/Scripts/images_swift4.stencil"
+#-t swift4
+else
+    echo "warning: SwiftGen not installed, download it from https://github.com/SwiftGen/SwiftGen"
+fi
+
+## pod version
+#$PODS_ROOT/SwiftGen/bin/swiftgen strings "$PROJECT_DIR/ScriptsTest/Resources/en.lproj/Localizable.strings" --output "$PROJECT_DIR/ScriptsTest/Resources/L10n.swift" -t structured-swift3
