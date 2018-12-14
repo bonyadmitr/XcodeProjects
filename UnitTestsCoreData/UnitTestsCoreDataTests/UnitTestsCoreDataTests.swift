5ef9d1a373326efc6a818acd5bef5b9e6da36978
Yaroslav Bondar <bonyadmitr@gmail.com>
2018-12-14 10:24:31 +0300

    cleared code in tests

--- UnitTestsCoreData/UnitTestsCoreDataTests/UnitTestsCoreDataTests.swift
+++ UnitTestsCoreData/UnitTestsCoreDataTests/UnitTestsCoreDataTests.swift
@@ -21,7 +21,7 @@ final class FetchDelegate: NSObject, NSFetchedResultsControllerDelegate {
 
 class BaseCoreDataTests: XCTestCase {
     /// need for override
-    static var coreDataStack = CoreDataStack(storeType: .memory, modelName: modelName, oldApi: false)
+    static var coreDataStack = CoreDataStack(storeType: .sqlite, modelName: modelName)
     
     /// will be nil after every test
     var coreDataStack: CoreDataStack!
@@ -57,6 +57,11 @@ class BaseCoreDataTests: XCTestCase {
 }
 
 class UnitTestsCoreDataTests: BaseCoreDataTests {
+    
+    override class func setUp() {
+        super.setUp()
+        coreDataStack = CoreDataStack(storeType: .sqlite, modelName: modelName, oldApi: false)
+    }
     
     private let fetchDelegate = FetchDelegate()
     
@@ -99,7 +104,6 @@ class UnitTestsCoreDataTests: BaseCoreDataTests {
         
         XCTAssertEqual(events?.count, 0)
     }
-    
 }
 
 /// https://developer.apple.com/documentation/xctest/xctestcase/understanding_setup_and_teardown_for_test_methods
@@ -125,49 +129,22 @@ final class CoreDataSQLTests: CoreDataMemoryTests {
     }
 }
 
-class CoreDataMemoryTests: XCTestCase {
-    
-    /// need for override
-    static var coreDataStack = CoreDataStack(storeType: .sqlite, modelName: modelName)
-    
-    /// will be nil after every test
-    private var coreDataStack: CoreDataStack!
-    
-    override func setUp() {
+class CoreDataMemoryTests: BaseCoreDataTests {
+
+    override class func setUp() {
         super.setUp()
-        
-        coreDataStack = type(of: self).coreDataStack
-        coreDataStack.deleteAll()
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+        coreDataStack = CoreDataStack(storeType: .memory, modelName: modelName)
     }
     
-    override class func tearDown() {
-        super.tearDown()
-        coreDataStack.deleteAll()
-    }
-
     func testCoreDataSave() {
-        let expec = expectation(description: "expec")
-        
-        coreDataStack.performBackgroundTask { context in
-            let event = DBEvent(managedObjectContext: context)
-            event.name = "Some event"
-            context.saveSyncUnsafe()
-            expec.fulfill()
-        }
-        
-        wait(for: [expec], timeout: 1)
+        createEvent()
         
         let fetchRequest: NSFetchRequest<DBEvent> = DBEvent.fetchRequest()
         let events = try? coreDataStack.viewContext.fetch(fetchRequest)
         
         XCTAssertNotNil(events)
         XCTAssertEqual(events?.count, 1)
-        XCTAssert(events?.first?.name == "Some event")
+        XCTAssert(events?.first?.name == eventName)
         
         /// Called when test method ends.
         addTeardownBlock {
@@ -176,16 +153,7 @@ class CoreDataMemoryTests: XCTestCase {
     }
     
     func testClearAll() {
-        let expec = expectation(description: "expec1")
-        
-        coreDataStack.performBackgroundTask { context in
-            let event = DBEvent(managedObjectContext: context)
-            event.name = "Some event"
-            context.saveSyncUnsafe()
-            expec.fulfill()
-        }
-        
-        wait(for: [expec], timeout: 1)
+        createEvent()
         
         coreDataStack.clearAll()
 //        coreDataStack.deleteAll()
@@ -198,28 +166,18 @@ class CoreDataMemoryTests: XCTestCase {
     }
     
     func testClearAllAndSave() {
-        let expec = expectation(description: "expec")
-        
-        coreDataStack.performBackgroundTask { context in
-            let event = DBEvent(managedObjectContext: context)
-            event.name = "Some event"
-            context.saveSyncUnsafe()
-            expec.fulfill()
-        }
-        
-        wait(for: [expec], timeout: 1)
+        createEvent()
         
         coreDataStack.clearAll()
-        let expec2 = expectation(description: "expec2")
         
+        let expec = expectation(description: "expec")
         coreDataStack.performBackgroundTask { context in
             let event = DBEvent(managedObjectContext: context)
             event.name = "Some event 2"
             context.saveSyncUnsafe()
-            expec2.fulfill()
+            expec.fulfill()
         }
-        
-        wait(for: [expec2], timeout: 1)
+        wait(for: [expec], timeout: 1)
         
         let fetchRequest: NSFetchRequest<DBEvent> = DBEvent.fetchRequest()
         let events = try? coreDataStack.viewContext.fetch(fetchRequest)
@@ -228,33 +186,17 @@ class CoreDataMemoryTests: XCTestCase {
         XCTAssertEqual(events?.count, 1)
         XCTAssert(events?.first?.name == "Some event 2")
     }
-
-//        let expec = expectation(description: "1")
-//        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-//            expec.fulfill()
-//        }
-//        waitForExpectations(timeout: 2, handler: nil)
     
     /// working ONLY with NSSQLiteStoreType
     func testDeleteAll() {
-//        let coreDataStack = CoreDataStack(storeType: .sqlite, modelName: modelName)
-        let expec = expectation(description: "expec")
+        createEvent()
         
-        coreDataStack.performBackgroundTask { context in
-            let event = DBEvent(managedObjectContext: context)
-            event.name = "Some event"
-            context.saveSyncUnsafe()
+        let expec = expectation(description: "expec")
+        coreDataStack.deleteAll { status in
             expec.fulfill()
         }
         
         wait(for: [expec], timeout: 1)
-        
-        let expec2 = expectation(description: "expec2")
-        coreDataStack.deleteAll { status in
-            expec2.fulfill()
-        }
-        
-        wait(for: [expec2], timeout: 1)
         
         let fetchRequest: NSFetchRequest<DBEvent> = DBEvent.fetchRequest()
         let events = try? coreDataStack.viewContext.fetch(fetchRequest)
