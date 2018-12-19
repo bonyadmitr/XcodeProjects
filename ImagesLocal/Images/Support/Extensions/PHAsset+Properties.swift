722b97d3a9b7c12752c6041037d1163117de45c1
Yaroslav Bondar <bonyadmitr@gmail.com>
2018-12-20 00:34:36 +0300

    updated PHAsset+Properties

--- ImagesLocal/Images/Support/Extensions/PHAsset+Properties.swift
+++ ImagesLocal/Images/Support/Extensions/PHAsset+Properties.swift
@@ -10,19 +10,47 @@ import Photos
 
 extension PHAsset {
     
-    var resource: PHAssetResource? {
+    private func mainResource() -> PHAssetResource? {
         return PHAssetResource.assetResources(for: self).first
     }
     
-    var originalFilename: String? {
+    /// original or iOS one
+    func originalFilename() -> String? {
         if #available(iOS 9.0, *) {
-            return resource?.originalFilename
+            return self.mainResource()?.originalFilename
         } else {
             return value(forKey: "filename") as? String
         }
     }
     
-    var uniformTypeIdentifier: String? {
+    /// will be different for edited photos
+    func originalFileSize() -> Int64? {
+        return self.mainResource()?.fileSize()
+    }
+    
+    /// edited or original one
+    func fileSize() -> Int64? {
+        let resources = PHAssetResource.assetResources(for: self)
+        let resource = resources[safe: 1] ?? resources.first
+        return resource?.fileSize()
+    }
+    
+    func isEdited() -> Bool {
+        return PHAssetResource.assetResources(for: self).count > 1
+    }
+    
+    func originalUniformTypeIdentifier() -> String? {
+        return getUniformTypeIdentifier(for: self.mainResource())
+    }
+    
+    /// edited or original one
+    func uniformTypeIdentifier() -> String? {
+        let resources = PHAssetResource.assetResources(for: self)
+        let resource = resources[safe: 1] ?? resources.first
+        return getUniformTypeIdentifier(for: resource)
+    }
+    
+    private func getUniformTypeIdentifier(for resource: PHAssetResource?) -> String? {
         if #available(iOS 9.0, *) {
             return resource?.uniformTypeIdentifier
         } else {
@@ -30,14 +58,37 @@ extension PHAsset {
         }
     }
     
-    /// will be different for edited photos
-    var originalFileSize: Int64? {
-        return resource?.fileSize()
+    /// optmized to get all values
+    func allProperties() -> AllResourcesProperties? {
+        let resources = PHAssetResource.assetResources(for: self)
+        
+        guard let resource = resources[safe: 1] ?? resources.first else {
+            assertionFailure("assetResources empty")
+            return nil
+        }
+        
+        let fileSize = resource.fileSize() ?? 0
+        assert(fileSize != 0, "could not get fileSize for resource")
+        
+        return (fileSize: fileSize,
+                uniformTypeIdentifier: resource.uniformTypeIdentifier,
+                filename: resource.originalFilename,
+                isEdited: resources.count > 1)
     }
 }
+
+typealias AllResourcesProperties = (fileSize: Int64, uniformTypeIdentifier: String, filename: String, isEdited: Bool)
 
 extension PHAssetResource {
     func fileSize() -> Int64? {
         return value(forKey: "fileSize") as? Int64
+    }
+}
+
+extension Collection {
+    
+    /// Returns the element at the specified index if it is within bounds, otherwise nil.
+    subscript(safe index: Index) -> Element? {
+        return indices.contains(index) ? self[index] : nil
     }
 }
