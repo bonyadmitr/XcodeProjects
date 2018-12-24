a1afc5408ebb320a66e96ef376a24780e14af401
Yaroslav Bondar <bonyadmitr@gmail.com>
2018-12-24 19:35:23 +0300

    started tetris project

---
+++ Tetris/Tetris/AppDelegate.swift
@@ -0,0 +1,66 @@
+//
+//  AppDelegate.swift
+//  Tetris
+//
+//  Created by Yaroslav Bondar on 24/12/2018.
+//  Copyright © 2018 Yaroslav Bondar. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        let window = UIWindow()
+        window.rootViewController = GameController()
+        window.makeKeyAndVisible()
+        self.window = window
+
+        return true
+    }
+}
+
+import UIKit
+
+final class GameController: UIViewController {
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    private func setup() {
+        
+    }
+    
+    override func loadView() {
+        view = GameView()
+    }
+}
+
+final class GameView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    private func setup() {
+        backgroundColor = .white
+    }
+}
