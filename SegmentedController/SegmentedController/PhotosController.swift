46c0166a6865c5240959d5995000ac117c623543
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-01-19 18:24:51 +0300

    updated PhotoCell for images

--- SegmentedController/SegmentedController/PhotosController.swift
+++ SegmentedController/SegmentedController/PhotosController.swift
@@ -326,60 +326,15 @@ extension PhotosController: UIScrollViewDelegate {
     }
 }
 
-final class PhotoCell2: UICollectionViewCell {
-    
-    let imageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.contentMode = .scaleAspectFill
-        //imageView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.clipsToBounds = true
-        imageView.backgroundColor = .lightGray
-        imageView.isOpaque = true
-        return imageView
-    }()
-    
-    var representedIdentifier = ""
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        setup()
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        setup()
-    }
-    
-    private func setup() {
-        addSubview(imageView)
-        layer.borderColor = UIColor.red.cgColor
-    }
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        imageView.frame = bounds
-    }
-    
-    override func prepareForReuse() {
-        super.prepareForReuse()
-        imageView.image = nil
-    }
-    
-    override var isSelected: Bool {
-        didSet {
-            if isSelected {
-                layer.borderWidth = 3
-            } else {
-                layer.borderWidth = 0
-            }
-        }
-    }
-}
-
-
 final class PhotoCell: UICollectionViewCell {
     
+    private enum Constants {
+        static let favoriteImageViewSideSize: CGFloat = 24
+        static let edgeInset: CGFloat = 6
+        
+        static let checkmarkFillImage = UIImage(named: "checkmark_fill")
+    }
+    
     let imageView: UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFill
@@ -391,16 +346,24 @@ final class PhotoCell: UICollectionViewCell {
         return imageView
     }()
     
-    let sizeLabel: UILabel = {
-        let label = UILabel()
-        label.font = UIFont.preferredFont(forTextStyle: .body)
-        label.textAlignment = .center
-        label.backgroundColor = UIColor.black.withAlphaComponent(0.5)
-        label.textColor = .white
-        return label
+    private let selectionImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
     }()
     
-    var representedAssetIdentifier = ""
+    private let favoriteImageView: UIImageView = {
+        let rect = CGRect(x: 0, y: 0,
+                          width: Constants.favoriteImageViewSideSize,
+                          height: Constants.favoriteImageViewSideSize)
+        let imageView = UIImageView(frame: rect)
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    //private var representedAssetIdentifier = ""
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -414,40 +377,54 @@ final class PhotoCell: UICollectionViewCell {
     
     private func setup() {
         addSubview(imageView)
-        addSubview(sizeLabel)
-        assert(subviews.firstIndex(of: sizeLabel) ?? 0 > subviews.firstIndex(of: imageView) ?? 0)
+        addSubview(selectionImageView)
+        addSubview(favoriteImageView)
         layer.borderColor = UIColor.red.cgColor
+        
+        //favoriteImageView.image = Constants.checkmarkFillImage
+        //favoriteImageView.tintColor = UIColor.yellow
     }
-    
-    let sizeLabelHeight: CGFloat = 20
     
     override func layoutSubviews() {
         super.layoutSubviews()
+        updateFrames()
+    }
+    
+    private func updateFrames() {
         imageView.frame = bounds
-        sizeLabel.frame = CGRect(x: 0, y: bounds.height - sizeLabelHeight, width: bounds.width, height: sizeLabelHeight)
+        selectionImageView.frame = CGRect(x: bounds.width - Constants.favoriteImageViewSideSize - Constants.edgeInset,
+                                          y: Constants.edgeInset,
+                                          width: Constants.favoriteImageViewSideSize,
+                                          height: Constants.favoriteImageViewSideSize)
+        favoriteImageView.frame = CGRect(x: Constants.edgeInset,
+                                         y: Constants.edgeInset,
+                                         width: Constants.favoriteImageViewSideSize,
+                                         height: Constants.favoriteImageViewSideSize)
     }
     
     override func prepareForReuse() {
         super.prepareForReuse()
         imageView.image = nil
-        sizeLabel.text = ""
+        selectionImageView.image = nil
+        favoriteImageView.image = nil
     }
     
     func update(for selectionState: PhotosController.SelectionState) {
-        
         if self.isSelected {
-            self.sizeLabel.text = "ready"
+            selectionImageView.image = Constants.checkmarkFillImage
+            selectionImageView.tintColor = UIColor.blue
             layer.borderWidth = 3
         } else {
             layer.borderWidth = 0
             switch selectionState {
             case .selecting:
-                self.sizeLabel.text = "selecting"
+                selectionImageView.image = Constants.checkmarkFillImage
+                selectionImageView.tintColor = UIColor.black
+                break
             case .ended:
-                self.sizeLabel.text = ""
+                selectionImageView.image = nil
             }
         }
-        
     }
 }
 
