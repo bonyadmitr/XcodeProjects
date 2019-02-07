d9c1c713ff0144aff9c0231231ba3d40c31ddcaf
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-02-07 16:34:31 +0300

    added BatteryManager in Background project

---
+++ Background/Background/Classes/BatteryManager.swift
@@ -0,0 +1,86 @@
+//
+//  BatteryManager.swift
+//  Background
+//
+//  Created by Bondar Yaroslav on 2/7/19.
+//  Copyright © 2019 Bondar Yaroslav. All rights reserved.
+//
+
+import UIKit
+
+final class BatteryManager: NSObject {
+    
+    static var batteryLevel: Float {
+        return UIDevice.current.batteryLevel
+    }
+    
+    /// disable screen sleep
+    /// add in applicationDidBecomeActive
+    /// if isCharging {
+    ///     UIApplication.shared.isIdleTimerDisabled = true
+    /// }
+    static var isCharging: Bool {
+        return UIDevice.current.batteryState == .charging || UIDevice.current.batteryState == .full
+    }
+    
+    override init() {
+        super.init()
+        
+        /// Register for battery level and state change notifications.
+        UIDevice.current.isBatteryMonitoringEnabled = true
+        
+        //        NotificationCenter.default.addObserver(self, selector: #selector(batteryLevelChanged), name: UIDevice.batteryLevelDidChangeNotification, object: nil)
+        //        NotificationCenter.default.addObserver(self, selector: #selector(batteryStateChanged), name: UIDevice.batteryStateDidChangeNotification, object: nil)
+        /// swift 4.1
+        NotificationCenter.default.addObserver(self, selector: #selector(batteryLevelChanged), name: .UIDeviceBatteryLevelDidChange, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(batteryStateChanged), name: .UIDeviceBatteryStateDidChange, object: nil)
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    private func updateBatteryLevel() {
+        let batteryLevel = UIDevice.current.batteryLevel
+        let batteryLevelString: String
+        
+        if batteryLevel < 0.0 {
+            batteryLevelString = "Unknown"
+        } else {
+            batteryLevelString = String(batteryLevel * 100)
+        }
+        
+        debugLog("batteryLevelString: \(batteryLevelString)")
+    }
+    
+    private func updateBatteryState() {
+        let currentState = UIDevice.current.batteryState
+        //        print(currentState.rawValue) /// need to check
+        let batteryState: String
+        
+        switch currentState {
+        case .unknown:
+            batteryState = "unknown"
+        case .unplugged:
+            batteryState = "unplugged"
+        case .charging:
+            batteryState = "charging"
+        case .full:
+            batteryState = "full"
+        }
+        
+        debugLog("batteryState: \(batteryState)")
+    }
+    
+    @objc private func batteryLevelChanged(_ notification: Notification) {
+        debugLog("batteryLevelChanged: \(notification)")
+        updateBatteryLevel()
+        updateBatteryState()
+    }
+    
+    @objc private func batteryStateChanged(_ notification: Notification) {
+        debugLog("batteryStateChanged: \(notification)")
+        updateBatteryLevel()
+        updateBatteryState()
+    }
+}
