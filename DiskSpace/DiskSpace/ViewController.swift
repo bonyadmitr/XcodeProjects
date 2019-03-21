63244e39aeaa404b02543a97bab0b5d8cec02089
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-03-21 12:12:48 +0300

    created VolumeStorageCapacityProtocol

--- DiskSpace/DiskSpace/ViewController.swift
+++ DiskSpace/DiskSpace/ViewController.swift
@@ -39,39 +39,405 @@ class ViewController: UIViewController {
         super.viewWillAppear(animated)
         updateSizes()
     }
-
     
-    @IBAction private func refresh(_ sender: UIBarButtonItem) {
+    @IBAction private func onRefresh(_ sender: UIBarButtonItem) {
         updateSizes()
     }
+    
+    @IBAction private func onClear(_ sender: UIBarButtonItem) {
+        clearCache()
+        updateSizes()
+    }
+    
+    @IBAction private func onCreateCash(_ sender: UIBarButtonItem) {
+        createCash()
+        updateSizes()
+    }
+    
+    private func clearCache() {
+        let fileManager = FileManager.default
+        guard let documentsUrl =  fileManager.urls(for: .cachesDirectory, in: .userDomainMask).first else {
+            assertionFailure()
+            return
+        }
+        let documentsPath = documentsUrl.path
+        
+        do {
+            let fileNames = try fileManager.contentsOfDirectory(atPath: "\(documentsPath)")
+            print("--- all files in cache:\n\(fileNames)")
+            
+            for fileName in fileNames {
+                /// System Error Code=513:
+                /// "Snapshots" couldn’t be removed because you don’t have permission to access it.
+                guard fileName != "Snapshots" else {
+                    return
+                }
+                let filePathName = "\(documentsPath)/\(fileName)"
+                try fileManager.removeItem(atPath: filePathName)
+            }
+            
+            let files = try fileManager.contentsOfDirectory(atPath: "\(documentsPath)")
+            print("--- all files in cache after deleting images:\n\(files)")
+            
+        } catch {
+            assertionFailure("Could not clear temp folder: \(error)")
+        }
 
-    func updateSizes() {
+        /// #2
+        /// https://stackoverflow.com/a/50122279/5893286
+//        let caches = (NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.cachesDirectory, FileManager.SearchPathDomainMask.userDomainMask, true)[0])
+//        let appId = Bundle.main.infoDictionary!["CFBundleIdentifier"] as! String
+//        let path = String(format:"%@/%@/Cache.db-wal",caches, appId)
+//        do {
+//            try FileManager.default.removeItem(atPath: path)
+//        } catch {
+//            print("ERROR DESCRIPTION: \(error)")
+//        }
+        
+    }
+    
+    private func createCash() {
+        guard let temporaryDirectoryURL = try? FileManager.default.url(for: .cachesDirectory,
+                                                                   in: .userDomainMask,
+                                                                   appropriateFor: nil,
+                                                                   create: false)
+            else {
+                assertionFailure()
+                return
+        }
+        let fileName = ProcessInfo().globallyUniqueString
+        let temporaryFileURL = temporaryDirectoryURL.appendingPathComponent(fileName)
+        let data = Data(repeating: 1, count: 1024 * 1024 * 100)
+        do {
+            try data.write(to: temporaryFileURL, options: .atomicWrite)
+        } catch {
+            assertionFailure(error.localizedDescription)
+        }
+        print("ready")
+    }
+
+    private func updateSizes() {
         /// volumeAvailableCapacityKey - доступное место, тоже самое что файловый менеджер отдаст.
         /// volumeAvailableCapacityForImportantUsageKey - всё свободное, с учётом возможности удаления кэша, временных файлов и т.д.
         /// В случае с volumeAvailableCapacityForImportantUsageKey нас не  пускает система в “защищенное” пространство
         // TODO: check vs .volumeAvailableCapacityKey, .volumeAvailableCapacityForOpportunisticUsageKey
+        
         let fileURL = URL(fileURLWithPath: Device.homeFolder)
+//        let fileURL = URL(fileURLWithPath: "/")
+//        let fileURL = URL(fileURLWithPath: NSHomeDirectory())
+        
+        
         
         if #available(iOS 11.0, *) {
+
+            let mb: Double = 1024
+//            let mb: Double = 1000
+            
+            
+            
             let values1 = try? fileURL.resourceValues(forKeys: [.volumeAvailableCapacityKey])
-            sizeLabel1.text = String(Int64(values1?.volumeAvailableCapacity ?? 0))
+            let size1 = Int64(values1?.volumeAvailableCapacity ?? 0)
+            sizeLabel1.text = "\(size1)\n\(Double(size1)/mb/mb/mb)"
             
+            
+            /// not working with fileURL "URL(fileURLWithPath: "/")"
             let values2 = try? fileURL.resourceValues(forKeys: [.volumeAvailableCapacityForImportantUsageKey])
-            sizeLabel2.text = String(Int64(values2?.volumeAvailableCapacity ?? 0))
+            let size2 = Int64(values2?.volumeAvailableCapacityForImportantUsage ?? 0)
+            sizeLabel2.text = "\(size2)\n\(Double(size2)/mb/mb/mb)"
             
+            
+            /// not working with fileURL "URL(fileURLWithPath: "/")"
             let values3 = try? fileURL.resourceValues(forKeys: [.volumeAvailableCapacityForOpportunisticUsageKey])
-            sizeLabel3.text = String(Int64(values3?.volumeAvailableCapacity ?? 0))
+            let size3 = Int64(values3?.volumeAvailableCapacityForOpportunisticUsage ?? 0)
+            sizeLabel3.text = "\(size3)\n\(Double(size3)/mb/mb/mb)"
             
             let values4 = try? fileURL.resourceValues(forKeys: [.volumeTotalCapacityKey])
-            sizeLabel4.text = String(Int64(values4?.volumeAvailableCapacity ?? 0))
+            let size4 = Int64(values4?.volumeTotalCapacity ?? 0)
+            sizeLabel4.text = "\(size4)\n\(Double(size4)/mb/mb/mb)"
+            
+            sizeLabel1.text! += "\n\(Double(size4 - size1)/mb/mb/mb)"
+            sizeLabel2.text! += "\n\(Double(size4 - size2)/mb/mb/mb)"
+            sizeLabel3.text! += "\n\(Double(size4 - size3)/mb/mb/mb)"
         }
+        
+//        if #available(iOS 11.0, *) {
+//            let fileURL = URL(fileURLWithPath: Device.homeFolder)
+//
+//            //let mb: Double = 1024
+//            let mb: Double = 1000
+//
+//
+//
+//            let values1 = try? fileURL.resourceValues(forKeys: [.volumeAvailableCapacityKey])
+//            let size1 = Int64(values1?.volumeAvailableCapacity ?? 0)
+//            sizeLabel1.text! += "\n\(size1)\n\(Double(size1)/mb/mb/mb)"
+//
+//
+//            /// Failed to get available space for volume /: Error Domain=CacheDeleteErrorDomain Code=10 "(null)" UserInfo={Volume=/}
+//            //            let values2 = try? fileURL.resourceValues(forKeys: [.volumeAvailableCapacityForImportantUsageKey])
+//            //            sizeLabel2.text = String(Int64(values2?.volumeAvailableCapacityForImportantUsage ?? 0))
+//            //
+//            //            let values3 = try? fileURL.resourceValues(forKeys: [.volumeAvailableCapacityForOpportunisticUsageKey])
+//            //            sizeLabel3.text = String(Int64(values3?.volumeAvailableCapacityForOpportunisticUsage ?? 0))
+//
+//            let values4 = try? fileURL.resourceValues(forKeys: [.volumeTotalCapacityKey])
+//            let size4 = Int64(values4?.volumeTotalCapacity ?? 0)
+//            sizeLabel4.text! += "\n\n\(size4)\n\(Double(size4)/mb/mb/mb)"
+//            sizeLabel4.text! +=  "\n\(Double(size4 - size1)/mb/mb/mb)"
+//        }
         
         /// iOS 9
         let systemAttributes = try? FileManager.default.attributesOfFileSystem(forPath: Device.homeFolder)
         let freeSize = systemAttributes?[.systemFreeSize] as? NSNumber
-        sizeLabel5.text = String(freeSize?.int64Value ?? 0)
+        
+        let newFreeSize = freeSize?.int64Value ?? 0
+        print("--- size:", self.freeSize - newFreeSize)
+        self.freeSize = newFreeSize
+        sizeLabel5.text = String(self.freeSize)
+    }
+    
+    var freeSize: Int64 = 0
+}
+
+
+typealias AllVolumeStorageCapacity = (totalDiskSpace: Int64, freeDiskSpace: Int64, usedDiskSpace: Int64)
+
+protocol VolumeStorageCapacityProtocol {
+    func allVolumeStorageCapacity() -> AllVolumeStorageCapacity
+    func totalDiskSpace() -> Int64
+    func freeDiskSpace() -> Int64
+    func usedDiskSpace() -> Int64
+}
+
+final class VolumeStorageCapacityAny: VolumeStorageCapacityProtocol {
+    private let volumeStorageCapacity: VolumeStorageCapacityProtocol
+    
+    init() {
+        if #available(iOS 11.0, *) {
+            volumeStorageCapacity = VolumeStorageCapacityFromiOS11()
+        } else {
+            volumeStorageCapacity = VolumeStorageCapacityOld()
+        }
+    }
+    
+    func allVolumeStorageCapacity() -> AllVolumeStorageCapacity {
+        return volumeStorageCapacity.allVolumeStorageCapacity()
+    }
+    
+    func totalDiskSpace() -> Int64 {
+        return volumeStorageCapacity.totalDiskSpace()
+    }
+    
+    func freeDiskSpace() -> Int64 {
+        return volumeStorageCapacity.freeDiskSpace()
+    }
+    
+    func usedDiskSpace() -> Int64 {
+        return volumeStorageCapacity.usedDiskSpace()
     }
 }
+
+final class VolumeStorageCapacityOld: VolumeStorageCapacityProtocol {
+    
+    private let homeDir = NSHomeDirectory()
+    
+    func allVolumeStorageCapacity() -> AllVolumeStorageCapacity {
+        let systemAttributes = try? FileManager.default.attributesOfFileSystem(forPath: homeDir)
+        
+        let totalSpace = systemAttributes?[.systemSize] as? NSNumber
+        let freeSpace = systemAttributes?[.systemFreeSize] as? NSNumber
+        
+        let totalSpaceSize = totalSpace?.int64Value ?? 0
+        let freeSpaceSize = freeSpace?.int64Value ?? 0
+        
+        return (totalDiskSpace: totalSpaceSize,
+                freeDiskSpace: freeSpaceSize,
+                usedDiskSpace: totalSpaceSize - freeSpaceSize)
+    }
+    
+    func totalDiskSpace() -> Int64 {
+        let systemAttributes = try? FileManager.default.attributesOfFileSystem(forPath: homeDir)
+        let totalSpace = systemAttributes?[.systemSize] as? NSNumber
+        return totalSpace?.int64Value ?? 0
+    }
+    
+    func freeDiskSpace() -> Int64 {
+        let systemAttributes = try? FileManager.default.attributesOfFileSystem(forPath: homeDir)
+        let freeSpace = systemAttributes?[.systemFreeSize] as? NSNumber
+        return freeSpace?.int64Value ?? 0
+    }
+    
+    func usedDiskSpace() -> Int64 {
+        let systemAttributes = try? FileManager.default.attributesOfFileSystem(forPath: homeDir)
+        
+        let totalSpace = systemAttributes?[.systemSize] as? NSNumber
+        let freeSpace = systemAttributes?[.systemFreeSize] as? NSNumber
+        
+        let totalSpaceSize = totalSpace?.int64Value ?? 0
+        let freeSpaceSize = freeSpace?.int64Value ?? 0
+        
+        return totalSpaceSize - freeSpaceSize
+    }
+}
+
+@available(iOS 11.0, *)
+final class VolumeStorageCapacityFromiOS11: VolumeStorageCapacityProtocol {
+    
+    private let homeURL = URL(fileURLWithPath: NSHomeDirectory())
+    
+    func allVolumeStorageCapacity() -> AllVolumeStorageCapacity {
+        let values = try? homeURL.resourceValues(forKeys: [.volumeAvailableCapacityForImportantUsageKey,
+                                                           .volumeTotalCapacityKey])
+        let totalSpaceSize = Int64(values?.volumeTotalCapacity ?? 0)
+        let freeSpaceSize = values?.volumeAvailableCapacityForImportantUsage ?? 0
+        
+        return (totalDiskSpace: totalSpaceSize,
+                freeDiskSpace: freeSpaceSize,
+                usedDiskSpace: totalSpaceSize - freeSpaceSize)
+    }
+    
+    func totalDiskSpace() -> Int64 {
+        let values = try? homeURL.resourceValues(forKeys: [.volumeTotalCapacityKey])
+        return Int64(values?.volumeTotalCapacity ?? 0)
+    }
+    
+    /// like system one
+    func freeDiskSpace() -> Int64 {
+        let values = try? homeURL.resourceValues(forKeys: [.volumeAvailableCapacityForImportantUsageKey])
+        return Int64(values?.volumeAvailableCapacityForImportantUsage ?? 0)
+    }
+    
+    func usedDiskSpace() -> Int64 {
+        let values = try? homeURL.resourceValues(forKeys: [.volumeAvailableCapacityForImportantUsageKey,
+                                                           .volumeTotalCapacityKey])
+        let totalSpaceSize = Int64(values?.volumeTotalCapacity ?? 0)
+        let freeSpaceSize = values?.volumeAvailableCapacityForImportantUsage ?? 0
+        
+        return totalSpaceSize - freeSpaceSize
+    }
+}
+
+//    func MBFormatter(_ bytes: Int64) -> String {
+//        let formatter = ByteCountFormatter()
+//        formatter.allowedUnits = ByteCountFormatter.Units.useMB
+//        formatter.countStyle = ByteCountFormatter.CountStyle.decimal
+//        formatter.includesUnit = false
+//        return formatter.string(fromByteCount: bytes) as String
+//    }
+//
+//    //MARK: Get String Value
+//    var totalDiskSpaceInGB:String {
+//        return ByteCountFormatter.string(fromByteCount: totalDiskSpaceInBytes(), countStyle: ByteCountFormatter.CountStyle.decimal)
+//    }
+//
+//    var freeDiskSpaceInGB:String {
+//        return ByteCountFormatter.string(fromByteCount: freeDiskSpaceInBytes, countStyle: ByteCountFormatter.CountStyle.decimal)
+//    }
+//
+//    var usedDiskSpaceInGB:String {
+//        return ByteCountFormatter.string(fromByteCount: usedDiskSpaceInBytes, countStyle: ByteCountFormatter.CountStyle.decimal)
+//    }
+//
+//    var totalDiskSpaceInMB:String {
+//        return MBFormatter(totalDiskSpaceInBytes)
+//    }
+//
+//    var freeDiskSpaceInMB:String {
+//        return MBFormatter(freeDiskSpaceInBytes)
+//    }
+//
+//    var usedDiskSpaceInMB:String {
+//        return MBFormatter(usedDiskSpaceInBytes)
+//    }
+
+/// https://stackoverflow.com/a/47463829/5893286
+/// https://developer.apple.com/documentation/foundation/urlresourcekey/checking_volume_storage_capacity
+final class VolumeStorageCapacity: VolumeStorageCapacityProtocol {
+    
+    /// need only for iOS <= 10
+    private let homeDir: String
+    
+    /// need only for iOS 11
+    private let homeURL: URL
+    
+    init() {
+        homeDir = NSHomeDirectory()
+        homeURL = URL(fileURLWithPath: homeDir)
+    }
+    
+    func totalDiskSpace() -> Int64 {
+        if #available(iOS 11.0, *) {
+            let values = try? homeURL.resourceValues(forKeys: [.volumeTotalCapacityKey])
+            return Int64(values?.volumeTotalCapacity ?? 0)
+        } else {
+            let systemAttributes = try? FileManager.default.attributesOfFileSystem(forPath: homeDir)
+            let totalSpace = systemAttributes?[.systemSize] as? NSNumber
+            return totalSpace?.int64Value ?? 0
+        }
+    }
+    
+    /// like system one
+    func freeDiskSpace() -> Int64 {
+        if #available(iOS 11.0, *) {
+            let values = try? homeURL.resourceValues(forKeys: [.volumeAvailableCapacityForImportantUsageKey])
+            return Int64(values?.volumeAvailableCapacityForImportantUsage ?? 0)
+        } else {
+            let systemAttributes = try? FileManager.default.attributesOfFileSystem(forPath: homeDir)
+            let freeSpace = systemAttributes?[.systemFreeSize] as? NSNumber
+            return freeSpace?.int64Value ?? 0
+        }
+    }
+    
+    /// optmized version. not totalDiskSpace() - freeDiskSpace()
+    func usedDiskSpace() -> Int64 {
+        if #available(iOS 11.0, *) {
+            let values = try? homeURL.resourceValues(forKeys: [.volumeAvailableCapacityForImportantUsageKey,
+                                                               .volumeTotalCapacityKey])
+            let freeSpaceSize = values?.volumeAvailableCapacityForImportantUsage ?? 0
+            let totalSpaceSize = Int64(values?.volumeTotalCapacity ?? 0)
+            return totalSpaceSize - freeSpaceSize
+        } else {
+            let systemAttributes = try? FileManager.default.attributesOfFileSystem(forPath: homeDir)
+            
+            let freeSpace = systemAttributes?[.systemFreeSize] as? NSNumber
+            let totalSpace = systemAttributes?[.systemSize] as? NSNumber
+            
+            let freeSpaceSize = freeSpace?.int64Value ?? 0
+            let totalSpaceSize = totalSpace?.int64Value ?? 0
+            
+            return totalSpaceSize - freeSpaceSize
+        }
+    }
+    
+    func allVolumeStorageCapacity() -> AllVolumeStorageCapacity {
+        if #available(iOS 11.0, *) {
+            let values = try? homeURL.resourceValues(forKeys: [.volumeAvailableCapacityForImportantUsageKey,
+                                                               .volumeTotalCapacityKey])
+            let totalSpaceSize = Int64(values?.volumeTotalCapacity ?? 0)
+            let freeSpaceSize = values?.volumeAvailableCapacityForImportantUsage ?? 0
+            let usedSpaseSize = totalSpaceSize - freeSpaceSize
+            
+            return (totalDiskSpace: totalSpaceSize,
+                    freeDiskSpace: freeSpaceSize,
+                    usedDiskSpace: usedSpaseSize)
+        } else {
+            let systemAttributes = try? FileManager.default.attributesOfFileSystem(forPath: homeDir)
+            
+            let totalSpace = systemAttributes?[.systemSize] as? NSNumber
+            let freeSpace = systemAttributes?[.systemFreeSize] as? NSNumber
+            
+            let totalSpaceSize = totalSpace?.int64Value ?? 0
+            let freeSpaceSize = freeSpace?.int64Value ?? 0
+            
+            let usedSpaseSize = totalSpaceSize - freeSpaceSize
+            
+            return (totalDiskSpace: totalSpaceSize,
+                    freeDiskSpace: freeSpaceSize,
+                    usedDiskSpace: usedSpaseSize)
+        }
+    }
+}
+
 //iOSComponents
 
 /// for disk space
@@ -80,7 +446,7 @@ final class Device {
     
     static var homeFolder: String {
         return NSHomeDirectory() as String
-        //        return NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).last ?? ""
+//                return NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).last ?? ""
     }
     
     /// Available vs Free
