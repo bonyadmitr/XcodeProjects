07a702a2e0a8b317e3c15bbb5def9645fa230064
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-03-29 09:22:25 +0300

    created project Reachability

---
+++ Reachability/Reachability/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Reachability
+//
+//  Created by Bondar Yaroslav on 3/29/19.
+//  Copyright © 2019 Bondar Yaroslav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
