ba811af4b269e92b279aa6cb8bcf2a7e3a300e16
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-04-04 21:59:38 +0300

    started Router

--- Router/Router/AppDelegate.swift
+++ Router/Router/AppDelegate.swift
@@ -8,44 +8,98 @@
 
 import UIKit
 
+final class Router {
+    
+    private var window: UIWindow!
+    
+    func setup(appDelegate: AppDelegate) {
+        let window = UIWindow()
+        window.backgroundColor = UIColor.cyan
+//        window.rootViewController = StartController()
+        window.makeKeyAndVisible()
+        appDelegate.window = window
+        self.window = window
+    }
+    
+    func startWithMainApp() {
+        window.rootViewController = MainController()
+    }
+    
+    func startWithSplash() {
+        window.rootViewController = StartController()
+    }
+}
+
+import UIKit
+
+final class StartController: LabelController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        label.text = "Start"
+    }
+}
+
+
+import UIKit
+
+final class MainController: LabelController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        label.text = "Main"
+    }
+    
+}
+
+import UIKit
+
+class LabelController: UIViewController {
+    
+    let label = UILabel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(label)
+        
+        view.backgroundColor = .white
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        label.sizeToFit()
+        label.center = view.center
+    }
+}
+
+private let router = Router()
+
+//extension UIViewController {
+//    var router: Router {
+//        return globalRouter
+//    }
+//}
+
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+final class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        window = UIWindow()
-        window?.backgroundColor = UIColor.cyan
-        window?.rootViewController = UIViewController()
-        window?.makeKeyAndVisible()
+        
+        router.setup(appDelegate: self)
+        
+        let isLoggedIn = true
+        if isLoggedIn {
+            router.startWithMainApp()
+        } else {
+            router.startWithSplash()
+        }
 
         return true
     }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
 }
-
