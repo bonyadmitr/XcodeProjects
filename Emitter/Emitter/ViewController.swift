1650c1127b2dd641a3eb206d05dca8f7cc46d8be
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-04-08 07:28:14 +0300

    created Emitter project

---
+++ Emitter/Emitter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Emitter
+//
+//  Created by Bondar Yaroslav on 4/7/19.
+//  Copyright © 2019 Bondar Yaroslav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
