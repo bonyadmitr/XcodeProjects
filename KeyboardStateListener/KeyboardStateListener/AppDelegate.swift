d7c27932719bf3d428e132972725b9bf0502d9d7
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-04-17 11:57:52 +0300

    created project KeyboardStateListener

---
+++ KeyboardStateListener/KeyboardStateListener/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  KeyboardStateListener
+//
+//  Created by Bondar Yaroslav on 4/17/19.
+//  Copyright © 2019 Bondar Yaroslav. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        
+        
+        return true
+    }
+}
+
---
+++ KeyboardStateListener/KeyboardStateListener/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KeyboardStateListener
+//
+//  Created by Bondar Yaroslav on 4/17/19.
+//  Copyright © 2019 Bondar Yaroslav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
