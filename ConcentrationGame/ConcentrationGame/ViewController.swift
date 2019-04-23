2a044adb32704e98d394360e381f94286e35603c
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-04-23 12:01:23 +0300

    created project ConcentrationGame

---
+++ ConcentrationGame/ConcentrationGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ConcentrationGame
+//
+//  Created by Bondar Yaroslav on 4/23/19.
+//  Copyright © 2019 Bondar Yaroslav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
