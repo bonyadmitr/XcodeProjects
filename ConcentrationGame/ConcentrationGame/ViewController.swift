b49c75d23b8d42719389b0e38949704aa0ca1e98
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-04-23 12:01:50 +0300

    added temp logic to shoe emojis

--- ConcentrationGame/ConcentrationGame/ViewController.swift
+++ ConcentrationGame/ConcentrationGame/ViewController.swift
@@ -10,11 +10,161 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private weak var collectionView: UICollectionView! {
+        willSet {
+            let viewWidth = UIScreen.main.bounds.width
+            
+            let columns: CGFloat = CGFloat(numberOfCollumns)
+            let padding: CGFloat = 1
+            let itemWidth = floor((viewWidth - (columns - 1) * padding) / columns)
+            let itemSize = CGSize(width: itemWidth, height: itemWidth)
+            
+            if let layout = newValue.collectionViewLayout as? UICollectionViewFlowLayout {
+                layout.itemSize = itemSize
+                layout.minimumInteritemSpacing = padding
+                layout.minimumLineSpacing = padding
+            }
+            
+            
+            newValue.register(GameCell.self, forCellWithReuseIdentifier: "GameCell")
+            newValue.dataSource = self
+            newValue.delegate = self
+            
+            newValue.backgroundColor = UIColor.lightGray
+        }
+    }
+    
+    let q = "👻🎃😱👽💀🧟‍♀️🐲👹🤡☠️"
+    
+//    let themes = [
+//        "Halloween": "👻🎃😱👽💀🧟‍♀️🐲👹🤡☠️",
+//        "Animals": "😸🐶🐰🦊🐷🐥🐼🦋🐭🐠",
+//        "Numbers": "1️⃣2️⃣3️⃣4️⃣5️⃣6️⃣7️⃣8️⃣9️⃣🔟",
+//        "Letters": "АБВГДЕЖЗИКЛМНОПРСТУФХЦШЩЬЪЭЮЯ",
+//        "Cats": "😹😻😼😽🙀😿😾🐱😸😺"
+//    ]
+    
+    let numberOfRaws = 4
+    let numberOfCollumns = 4
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
 }
 
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return numberOfRaws * numberOfCollumns
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "GameCell", for: indexPath) as! GameCell
+        let w = UnicodeScalar("👻")
+        let next = UnicodeScalar(w.value + UInt32(indexPath.row))!
+        cell.label.text = String(next)
+        return cell
+    }
+}
+
+extension ViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+}
+
+final class GameCell: UICollectionViewCell {
+    
+    let label = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    private func setup() {
+        //label.font = UIFont.systemFont(ofSize: 130)
+        label.font = UIFont.systemFont(ofSize: 90)
+        label.textAlignment = .center
+        label.backgroundColor = UIColor.white
+        label.isOpaque = true
+        addSubview(label)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        label.frame = bounds
+    }
+}
+
+//extension ViewController: UICollectionViewDelegateFlowLayout {
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//
+//        (collectionView.bounds.width - CGFloat(numberOfCollumns - 1) * 1) / CGFloat(numberOfCollumns)
+//        return CGSize(width: 100, height: 100)
+//    }
+//}
+extension String {
+    subscript (i: Int) -> Character {
+        return self[index(startIndex, offsetBy: i)]
+    }
+    subscript (bounds: CountableRange<Int>) -> Substring {
+        let start = index(startIndex, offsetBy: bounds.lowerBound)
+        let end = index(startIndex, offsetBy: bounds.upperBound)
+        return self[start ..< end]
+    }
+    subscript (bounds: CountableClosedRange<Int>) -> Substring {
+        let start = index(startIndex, offsetBy: bounds.lowerBound)
+        let end = index(startIndex, offsetBy: bounds.upperBound)
+        return self[start ... end]
+    }
+    subscript (bounds: CountablePartialRangeFrom<Int>) -> Substring {
+        let start = index(startIndex, offsetBy: bounds.lowerBound)
+        let end = index(endIndex, offsetBy: -1)
+        return self[start ... end]
+    }
+    subscript (bounds: PartialRangeThrough<Int>) -> Substring {
+        let end = index(startIndex, offsetBy: bounds.upperBound)
+        return self[startIndex ... end]
+    }
+    subscript (bounds: PartialRangeUpTo<Int>) -> Substring {
+        let end = index(startIndex, offsetBy: bounds.upperBound)
+        return self[startIndex ..< end]
+    }
+}
+extension Substring {
+    subscript (i: Int) -> Character {
+        return self[index(startIndex, offsetBy: i)]
+    }
+    subscript (bounds: CountableRange<Int>) -> Substring {
+        let start = index(startIndex, offsetBy: bounds.lowerBound)
+        let end = index(startIndex, offsetBy: bounds.upperBound)
+        return self[start ..< end]
+    }
+    subscript (bounds: CountableClosedRange<Int>) -> Substring {
+        let start = index(startIndex, offsetBy: bounds.lowerBound)
+        let end = index(startIndex, offsetBy: bounds.upperBound)
+        return self[start ... end]
+    }
+    subscript (bounds: CountablePartialRangeFrom<Int>) -> Substring {
+        let start = index(startIndex, offsetBy: bounds.lowerBound)
+        let end = index(endIndex, offsetBy: -1)
+        return self[start ... end]
+    }
+    subscript (bounds: PartialRangeThrough<Int>) -> Substring {
+        let end = index(startIndex, offsetBy: bounds.upperBound)
+        return self[startIndex ... end]
+    }
+    subscript (bounds: PartialRangeUpTo<Int>) -> Substring {
+        let end = index(startIndex, offsetBy: bounds.upperBound)
+        return self[startIndex ..< end]
+    }
+}
