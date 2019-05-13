5d19a06930372fb5d9eef63db0049021a1fc6476
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-05-13 16:17:48 +0300

    created ChatView project

---
+++ ChatView/ChatView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatView
+//
+//  Created by Bondar Yaroslav on 5/13/19.
+//  Copyright © 2019 Bondar Yaroslav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
