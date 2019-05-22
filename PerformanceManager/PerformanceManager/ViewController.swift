2306868b924a2462d3d68eec81d5a1d3419e8d60
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-05-22 09:14:57 +0300

    created project PerformanceManager

---
+++ PerformanceManager/PerformanceManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PerformanceManager
+//
+//  Created by Bondar Yaroslav on 5/22/19.
+//  Copyright © 2019 Bondar Yaroslav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
