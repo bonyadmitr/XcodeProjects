ebf248bb84e6725a1a9c40ab046c59ef36d72287
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-05-27 11:28:26 +0300

    created project VideoPlayer

---
+++ _mac/VideoPlayer/VideoPlayer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  VideoPlayer
+//
+//  Created by Bondar Yaroslav on 5/27/19.
+//  Copyright © 2019 Bondar Yaroslav. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
