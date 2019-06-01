8c2423301e3722a4dca907390d0532f3c0aff8da
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-06-01 12:13:04 +0300

    * added mainWindowController * fixed window close

--- _mac/VideoPlayer/VideoPlayer/AppDelegate.swift
+++ _mac/VideoPlayer/VideoPlayer/AppDelegate.swift
@@ -11,16 +11,38 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        
+        let mainStoryboard = NSStoryboard(name: "Main", bundle: nil)
+        let windowIdentifier = NSStoryboard.SceneIdentifier("MainWindow")
+        
+        guard let mainWindowController = mainStoryboard.instantiateController(withIdentifier: windowIdentifier) as? NSWindowController else {
+            assertionFailure()
+            return
+        }
+        
+        /// instead of "nil" can be "self"
+        mainWindowController.showWindow(nil)
+        mainWindowController.window?.makeKeyAndOrderFront(nil)
+        
+        /// to fix frame of closed window
+        //mainWindow.window?.setFrame(NSRect(x: 0, y: 0, width: 100, height: 100), display: true)
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
 
-
+    /// to open app after close button click we only hide it
+    /// https://stackoverflow.com/a/43332520
+    func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
+        if !flag {
+            for window in sender.windows {
+                window.makeKeyAndOrderFront(self)
+            }
+        }
+        
+        return true
+    }
 }
 
