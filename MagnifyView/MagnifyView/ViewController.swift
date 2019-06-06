1d79ab1c3d64df4540307360db275c1b4d801631
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-06-06 16:56:38 +0300

    added MagnifyView

--- MagnifyView/MagnifyView/ViewController.swift
+++ MagnifyView/MagnifyView/ViewController.swift
@@ -8,13 +8,92 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    private let magnifyView = MagnifyView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        magnifyView.layer.borderColor = UIColor.lightGray.cgColor
+        magnifyView.layer.borderWidth = 3
+        magnifyView.viewToMagnify = self.view
+        magnifyView.zoom = 4.0
+        magnifyView.yOffset = 60
+        
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 10)
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.frame = view.bounds
+        label.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        label.text = "some test. some test. some test. some test. some test. some test. some test. some test. some test. some test. some test. some test. some test. some test. some test. some test. some test. some test. some test. some test. some test. some test."
+        view.addSubview(label)
     }
-
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let point = touches.first?.location(in: self.view)
+        
+        magnifyView.setTouchPoint(pt: point!)
+        view.window?.addSubview(magnifyView)
+        //self.view.addSubview(magnifyView)
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        magnifyView.removeFromSuperview()
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let point = touches.first?.location(in: self.view)
+        magnifyView.setTouchPoint(pt: point!)
+    }
 }
 
+
+import UIKit
+
+final class MagnifyView: UIView {
+    
+    var viewToMagnify: UIView!
+    private var touchPoint: CGPoint!
+    
+    var zoom: CGFloat = 2
+    var yOffset: CGFloat = 60
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initialInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initialInit()
+    }
+    
+    private func initialInit() {
+        layer.masksToBounds = true
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = bounds.height * 0.5
+    }
+    
+    func setTouchPoint(pt: CGPoint) {
+        touchPoint = pt
+        center = CGPoint(x: pt.x, y: pt.y - yOffset)
+        setNeedsDisplay()
+    }
+    
+    override func draw(_ rect: CGRect) {
+        guard let context = UIGraphicsGetCurrentContext() else {
+            assertionFailure()
+            return
+        }
+        context.translateBy(x: 1 * frame.width * 0.5, y: 1 * frame.height * 0.5)
+        context.scaleBy(x: zoom, y: zoom)
+        context.translateBy(x: -1 * touchPoint.x, y: -1 * touchPoint.y)
+        viewToMagnify.layer.render(in: context)
+    }
+    
+}
