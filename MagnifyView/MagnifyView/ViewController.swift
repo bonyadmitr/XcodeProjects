bafd8e567fbf51c854e71a424a9b4e0608adb3d0
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-06-06 18:03:50 +0300

    cleared code

--- MagnifyView/MagnifyView/ViewController.swift
+++ MagnifyView/MagnifyView/ViewController.swift
@@ -10,20 +10,26 @@ import UIKit
 
 final class ViewController: UIViewController {
 
-    private let magnifyView = MagnifyView(frame: .init(x: 0, y: 0, width: 100, height: 100))
+    private lazy var magnifyView: MagnifyView = {
+        /// use "MagnifyView(frame" or "frame.size" to setup size
+        //magnifyView.frame.size = .init(width: 100, height: 100)
+        let magnifyView = MagnifyView(frame: .init(x: 0, y: 0, width: 100, height: 100))
+        
+        magnifyView.layer.borderColor = UIColor.lightGray.cgColor
+        magnifyView.layer.borderWidth = 3
+        magnifyView.zoom = 4.0
+        magnifyView.yOffset = 60
+        magnifyView.viewToMagnify = view
+        return magnifyView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        magnifyView.layer.borderColor = UIColor.lightGray.cgColor
-        magnifyView.layer.borderWidth = 3
-        magnifyView.viewToMagnify = view
-        magnifyView.zoom = 4.0
-        magnifyView.yOffset = 60
-        
-        /// use "MagnifyView(frame" or "frame.size"
-        //magnifyView.frame.size = .init(width: 100, height: 100)
-        
+        addLabel()
+    }
+    
+    private func addLabel() {
         let label = UILabel()
         label.font = UIFont.systemFont(ofSize: 10)
         label.numberOfLines = 0
