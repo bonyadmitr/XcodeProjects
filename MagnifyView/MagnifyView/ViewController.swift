6e15cedd6d89605698bdf930cafbacd840b9810d
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-06-10 09:58:38 +0300

    added gesture instead of touches events

--- MagnifyView/MagnifyView/ViewController.swift
+++ MagnifyView/MagnifyView/ViewController.swift
@@ -19,6 +19,12 @@ final class ViewController: UIViewController {
         super.viewDidLoad()
         
         addLabel()
+        
+        /// UIPanGestureRecognizer + UITapGestureRecognizer not working for touch down event
+        /// https://stackoverflow.com/a/18061848/5893286
+        let longPressGesture = UILongPressGestureRecognizer(target: self, action: #selector(onMagnifyGesture))
+        longPressGesture.minimumPressDuration = 0
+        view.addGestureRecognizer(longPressGesture)
     }
     
     private func addLabel() {
@@ -31,44 +37,61 @@ final class ViewController: UIViewController {
         label.text = "some test. some test. some test. some test. some test. some test. some test. some test. some test. some test. some test. some test. some test. some test. some test. some test. some test. some test. some test. some test. some test. some test."
         view.addSubview(label)
     }
+    
+    @objc private func onMagnifyGesture(_ gesture: UIGestureRecognizer) {
+        let point = gesture.location(in: view)
+        
+        switch gesture.state {
+        case .began:
+            magnifyView.touchPoint = point
+            view.window?.addSubview(magnifyView)
+        case .changed:
+            magnifyView.touchPoint = point
+        case .ended, .cancelled, .failed:
+            magnifyView.removeFromSuperview()
+        default:
+            assertionFailure()
+            magnifyView.removeFromSuperview()
+        }
+    }
 }
 
 // MARK: - touches
 /// can be used UIPanGestureRecognizer
-extension ViewController {
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        super.touchesBegan(touches, with: event)
-        
-        guard let point = touches.first?.location(in: self.view) else {
-            assertionFailure()
-            return
-        }
-        
-        magnifyView.touchPoint = point
-        view.window?.addSubview(magnifyView)
-        //self.view.addSubview(magnifyView)
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        super.touchesMoved(touches, with: event)
-        
-        guard let point = touches.first?.location(in: self.view) else {
-            assertionFailure()
-            return
-        }
-        magnifyView.touchPoint = point
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        super.touchesEnded(touches, with: event)
-        
-        magnifyView.removeFromSuperview()
-    }
-    
-    public override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        super.touchesCancelled(touches, with: event)
-        
-        magnifyView.removeFromSuperview()
-    }
-}
+//extension ViewController {
+//
+//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        super.touchesBegan(touches, with: event)
+//
+//        guard let point = touches.first?.location(in: view) else {
+//            assertionFailure()
+//            return
+//        }
+//
+//        magnifyView.touchPoint = point
+//        view.window?.addSubview(magnifyView)
+//        //self.view.addSubview(magnifyView)
+//    }
+//
+//    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        super.touchesMoved(touches, with: event)
+//
+//        guard let point = touches.first?.location(in: view) else {
+//            assertionFailure()
+//            return
+//        }
+//        magnifyView.touchPoint = point
+//    }
+//
+//    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        super.touchesEnded(touches, with: event)
+//
+//        magnifyView.removeFromSuperview()
+//    }
+//
+//    public override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        super.touchesCancelled(touches, with: event)
+//
+//        magnifyView.removeFromSuperview()
+//    }
+//}
