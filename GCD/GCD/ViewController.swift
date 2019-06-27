4ac2d3792f1b0db86bb77703a98a4b51bc54e968
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-06-27 09:17:23 +0300

    cleared code

--- GCD/GCD/ViewController.swift
+++ GCD/GCD/ViewController.swift
@@ -34,25 +34,6 @@ final class DispatchOperation {
     
 }
 
-/// https://developer.apple.com/swift/blog/?id=4
-@available(OSX 10.12, iOS 10.0, tvOS 10.0, watchOS 3.0, *)
-func dispatchAssert(condition: @autoclosure () -> DispatchPredicate) {
-    //if #available(iOS 10.0, *) {}
-    #if DEBUG
-    dispatchPrecondition(condition: condition())
-    #endif
-}
-
-@available(OSX 10.12, iOS 10.0, tvOS 10.0, watchOS 3.0, *)
-func assertMainQueue() {
-    dispatchAssert(condition: .onQueue(.main))
-}
-
-@available(OSX 10.12, iOS 10.0, tvOS 10.0, watchOS 3.0, *)
-func assertBackgroundQueue() {
-    dispatchAssert(condition: .notOnQueue(.main))
-}
-
 enum Benchmark {
     static func one(block: () -> Void) -> TimeInterval {
         let startTime = CFAbsoluteTimeGetCurrent()
