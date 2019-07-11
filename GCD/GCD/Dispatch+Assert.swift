985802114f54c04fe1bf9e0b89d00b3f9c18e3b8
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-07-11 09:06:00 +0300

    changed dispatchAssert @available

--- GCD/GCD/Dispatch+Assert.swift
+++ GCD/GCD/Dispatch+Assert.swift
@@ -1,20 +1,38 @@
 import Foundation
 
 /// https://developer.apple.com/swift/blog/?id=4
-@available(OSX 10.12, iOS 10.0, tvOS 10.0, watchOS 3.0, *)
 func dispatchAssert(condition: @autoclosure () -> DispatchPredicate) {
-    //if #available(iOS 10.0, *) {}
     #if DEBUG
-    dispatchPrecondition(condition: condition())
+    if #available(iOS 10.0, *) {
+        dispatchPrecondition(condition: condition())
+    }
     #endif
 }
 
-@available(OSX 10.12, iOS 10.0, tvOS 10.0, watchOS 3.0, *)
 func assertMainQueue() {
     dispatchAssert(condition: .onQueue(.main))
 }
 
-@available(OSX 10.12, iOS 10.0, tvOS 10.0, watchOS 3.0, *)
 func assertBackgroundQueue() {
     dispatchAssert(condition: .notOnQueue(.main))
 }
+
+
+/// https://developer.apple.com/swift/blog/?id=4
+//@available(OSX 10.12, iOS 10.0, tvOS 10.0, watchOS 3.0, *)
+//func dispatchAssert(condition: @autoclosure () -> DispatchPredicate) {
+//    //if #available(iOS 10.0, *) {}
+//    #if DEBUG
+//    dispatchPrecondition(condition: condition())
+//    #endif
+//}
+//
+//@available(OSX 10.12, iOS 10.0, tvOS 10.0, watchOS 3.0, *)
+//func assertMainQueue() {
+//    dispatchAssert(condition: .onQueue(.main))
+//}
+//
+//@available(OSX 10.12, iOS 10.0, tvOS 10.0, watchOS 3.0, *)
+//func assertBackgroundQueue() {
+//    dispatchAssert(condition: .notOnQueue(.main))
+//}
