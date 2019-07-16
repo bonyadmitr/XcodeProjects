177fb20ff0e035707176ce83a29a79d06807d9eb
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-07-16 20:46:36 +0300

    added firebase

--- FirebaseTest/FirebaseTest/AppDelegate.swift
+++ FirebaseTest/FirebaseTest/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAnalytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +17,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        FirebaseApp.configure()
+        
+        
+        
         return true
     }
 
