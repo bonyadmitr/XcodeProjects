0bc1dd315a7d8ae04880076f4515bd7bbc439cc5
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-07-16 20:25:30 +0300

    created FirebaseTest project

---
+++ FirebaseTest/FirebaseTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseTest
+//
+//  Created by Yaroslav Bondar on 16/07/2019.
+//  Copyright © 2019 Yaroslav Bondar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
