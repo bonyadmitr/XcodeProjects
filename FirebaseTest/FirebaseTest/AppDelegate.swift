5020f61575b81ba2c72b0264fc0bbe1c00b5160d
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-07-19 15:59:13 +0300

    cleared code

--- FirebaseTest/FirebaseTest/AppDelegate.swift
+++ FirebaseTest/FirebaseTest/AppDelegate.swift
@@ -9,45 +9,9 @@
 import UIKit
 
 import FirebaseCore
-import FirebaseFirestore
 
 import Fabric
 import Crashlytics
-
-final class FirestoreService {
-    
-    /// https://firebase.google.com/docs/firestore/quickstart
-    let db = Firestore.firestore()
-    
-    func getUsers() {
-        db.collection("users").getDocuments() { (querySnapshot, err) in
-            if let err = err {
-                print("Error getting documents: \(err)")
-            } else if let querySnapshot = querySnapshot {
-                
-                let users = querySnapshot.documents.map { $0.data() }
-                print(users)
-                
-            } else {
-                assertionFailure()
-            }
-        }
-    }
-    
-    func createNewUser() {
-        var ref: DocumentReference? = nil
-        ref = db.collection("users").addDocument(data: [
-            "id": 2,
-            "name": "some name of user"
-        ]) { err in
-            if let err = err {
-                print("Error adding document: \(err)")
-            } else {
-                print("Document added with ID: \(ref!.documentID)")
-            }
-        }
-    }
-}
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
