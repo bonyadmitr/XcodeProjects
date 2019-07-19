fcdc4911b3f47f57da39681ed81bdde3a110ebe6
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-07-19 15:54:17 +0300

    created FIRDatabaseService

--- FirebaseTest/FirebaseTest/AppDelegate.swift
+++ FirebaseTest/FirebaseTest/AppDelegate.swift
@@ -14,6 +14,40 @@ import FirebaseFirestore
 import Fabric
 import Crashlytics
 
+
+import FirebaseDatabase
+
+// TODO: !!! it is not working !!!
+final class FIRDatabaseService {
+    
+    /// https://firebase.google.com/docs/database/ios/read-and-write
+    let ref = Database.database().reference()
+    
+    func getUsers() {
+
+//        let userID = "qwe"
+//        let newName = "some name"
+
+        /// rewrite all fields
+        //ref.child("users").child(userID).setValue(["username": newName])
+
+        /// update one field
+        //ref.child("users/\(userID)/name").setValue(newName)
+
+        ref.child("users").observeSingleEvent(of: .value, with: { (snapshot) in
+            // Get user value
+            let all = snapshot.value as? [String: Any]
+            print(all ?? "nil")
+//            let username = value?["username"] as? String ?? ""
+//            let user = User(username: username)
+
+            // ...
+        }) { (error) in
+            print(error.localizedDescription)
+        }
+    }
+}
+
 final class FirestoreService {
     
     /// https://firebase.google.com/docs/firestore/quickstart
@@ -32,34 +66,6 @@ final class FirestoreService {
                 assertionFailure()
             }
         }
-        
-        
-        /// https://firebase.google.com/docs/database/ios/read-and-write
-        //        var ref: DatabaseReference!
-        //
-        //        ref = Database.database().reference()
-        //
-        //        let userID = "qwe"
-        //        let newName = "some name"
-        //
-        //        /// rewrite all fields
-        //        //ref.child("users").child(userID).setValue(["username": newName])
-        //
-        //        /// update one field
-        //        //ref.child("users/\(userID)/name").setValue(newName)
-        //
-        //        ref.coll
-        //        ref.child("users").observeSingleEvent(of: .value, with: { (snapshot) in
-        //            // Get user value
-        //            let all = snapshot.value as? [String: Any]
-        //            print(all)
-        ////            let username = value?["username"] as? String ?? ""
-        ////            let user = User(username: username)
-        //
-        //            // ...
-        //        }) { (error) in
-        //            print(error.localizedDescription)
-        //        }
     }
     
     func createNewUser() {
