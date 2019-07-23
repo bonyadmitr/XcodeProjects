573a185b67c8526ea2bbbec678e5190baf43699e
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-07-23 18:18:54 +0300

    created UnitTestsAndPerformanceTests project

---
+++ UnitTestsAndPerformance/UnitTestsAndPerformance/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UnitTestsAndPerformance
+//
+//  Created by Yaroslav Bondar on 23/07/2019.
+//  Copyright © 2019 Yaroslav Bondar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
