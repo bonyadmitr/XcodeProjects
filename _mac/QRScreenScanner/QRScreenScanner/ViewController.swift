1cfa7ee75c242aae5e1079665790cbd46dfb6b9c
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-07-23 11:31:42 +0300

    created project QRScreenScanner

---
+++ _mac/QRScreenScanner/QRScreenScanner/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  QRScreenScanner
+//
+//  Created by Bondar Yaroslav on 7/23/19.
+//  Copyright © 2019 Bondar Yaroslav. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
