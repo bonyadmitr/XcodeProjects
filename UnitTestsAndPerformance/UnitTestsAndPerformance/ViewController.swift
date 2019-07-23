5f982278e5d46ebd4193559f9c23711a1e7612df
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-07-23 19:35:53 +0300

    updated vc for isRetained

--- UnitTestsAndPerformance/UnitTestsAndPerformance/ViewController.swift
+++ UnitTestsAndPerformance/UnitTestsAndPerformance/ViewController.swift
@@ -10,21 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var isRetained = false
     var handler: (() -> Void)?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        view.backgroundColor = .red
-        
-        handler = { [weak self] in
-            guard let self = self else {
-                return
+        if isRetained {
+            //handler = someFunc
+            handler = {
+                print("- handler", self)
             }
-            print("- handler", self)
+        } else {
+            handler = { [weak self] in
+                guard let self = self else {
+                    return
+                }
+                print("- handler", self)
+            }
         }
-        
-        //handler = someFunc
         
         handler?()
     }
@@ -32,56 +36,4 @@ class ViewController: UIViewController {
     func someFunc() {
         print("- handler", self)
     }
-}
-
-func assertDeallocation(_ testedViewController: @escaping () -> UIViewController) {
-    #if DEBUG
-    
-//    let rootViewController = UIViewController()
-    
-    
-    
-    // Make sure that the view is active and we can use it for presenting views.
-//    let window = UIWindow(frame: CGRect(x: 0, y: 0, width: 400, height: 400))
-//    let window = UIWindow()
-    let window = UIApplication.shared.delegate!.window!!
-    let rootViewController = window.rootViewController!
-//    window.rootViewController = rootViewController
-//    window.makeKeyAndVisible()
-    
-    DispatchQueue.global().async {
-        weak var weakReferenceViewController: UIViewController?
-        let autoreleasepoolSemaphore = DispatchSemaphore(value: 0)
-        
-        autoreleasepool {
-            
-            DispatchQueue.main.async {
-                /// Present and dismiss the view after which the view controller should be released.
-                rootViewController.present(testedViewController(), animated: false, completion: {
-                    weakReferenceViewController = rootViewController.presentedViewController
-                    assert(weakReferenceViewController != nil)
-                    
-                    rootViewController.dismiss(animated: false, completion: {
-                        autoreleasepoolSemaphore.signal()
-                    })
-                })
-            }
-
-        }
-        
-        let result = autoreleasepoolSemaphore.wait(timeout: .now() + 5)
-        print("-", weakReferenceViewController ?? "nil1")
-        
-        switch result {
-        case .success:
-            assert(weakReferenceViewController == nil)
-        case .timedOut:
-            assertionFailure()
-        }
-        
-        print("1")
-        print("-", weakReferenceViewController ?? "nil1")
-    }
-
-    #endif
 }
