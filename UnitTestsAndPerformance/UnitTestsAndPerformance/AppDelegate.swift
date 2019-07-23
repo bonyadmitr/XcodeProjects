040c1f8d0a3017d93e16dab224283e87a15639e0
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-07-23 19:36:09 +0300

    cleared code

--- UnitTestsAndPerformance/UnitTestsAndPerformance/AppDelegate.swift
+++ UnitTestsAndPerformance/UnitTestsAndPerformance/AppDelegate.swift
@@ -15,10 +15,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
-        assertDeallocation({ ViewController() })
-//        assertDeallocation(ViewController())
-        
         return true
     }
 
