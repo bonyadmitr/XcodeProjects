dd8250f9374327f814776ebde4a5f5388ce5a680
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-07-23 19:16:23 +0300

    added temp func assertDeallocation

--- UnitTestsAndPerformance/UnitTestsAndPerformance/AppDelegate.swift
+++ UnitTestsAndPerformance/UnitTestsAndPerformance/AppDelegate.swift
@@ -15,7 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        assertDeallocation({ ViewController() })
+//        assertDeallocation(ViewController())
+        
         return true
     }
 
