6e764b0c56d82a5782c3ea6d92105177be71d246
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-07-27 15:13:31 +0300

    created TableViewTests project

---
+++ _mac/TableViewTests/TableViewTests/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TableViewTests
+//
+//  Created by Bondar Yaroslav on 7/27/19.
+//  Copyright © 2019 Bondar Yaroslav. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ _mac/TableViewTests/TableViewTests/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TableViewTests
+//
+//  Created by Bondar Yaroslav on 7/27/19.
+//  Copyright © 2019 Bondar Yaroslav. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
