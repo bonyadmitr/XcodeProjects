e564bc239572eee9fe0e2ec58405812d8b1535ac
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-07-27 15:30:34 +0300

    added NSTableView in code

--- _mac/TableViewTests/TableViewTests/ViewController.swift
+++ _mac/TableViewTests/TableViewTests/ViewController.swift
@@ -13,7 +13,32 @@ class ViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        /// https://stackoverflow.com/a/27747282/5893286
+        let tableView = NSTableView(frame: view.bounds)
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        let tableContainer = NSScrollView(frame: view.bounds)
+        tableContainer.autoresizingMask = [.width, .height]
+        tableContainer.documentView = tableView
+        tableContainer.hasVerticalScroller = true
+        
+        
+        let column1 = NSTableColumn(identifier: NSUserInterfaceItemIdentifier(rawValue: "date"))
+        column1.width = 100
+        column1.title = "Date"
+        let column2 = NSTableColumn(identifier: NSUserInterfaceItemIdentifier(rawValue: "value"))
+        column2.width = view.frame.width - 100
+        column2.title = "Value"
+        
+        tableView.addTableColumn(column1)
+        tableView.addTableColumn(column2)
+        
+        view.addSubview(tableContainer)
+//        tableView
+        
+        
+        
     }
 
     override var representedObject: Any? {
@@ -25,3 +50,39 @@ class ViewController: NSViewController {
 
 }
 
+extension ViewController: NSTableViewDataSource {
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return 100
+    }
+    
+    /// NSTableView set content Mode
+    /// https://stackoverflow.com/q/19218807/5893286
+    func tableView(_ tableView: NSTableView, objectValueFor tableColumn: NSTableColumn?, row: Int) -> Any? {
+        guard let columnIdentifier = tableColumn?.identifier.rawValue else {
+            assertionFailure()
+            return nil
+        }
+        
+        switch columnIdentifier {
+        case "date" :
+            return "date \(row)"
+        case "value":
+            return "value \(row)"
+        default:
+            assertionFailure()
+            return nil
+        }
+    }
+    
+//    func tableView(_ tableView: NSTableView, dataCellFor tableColumn: NSTableColumn?, row: Int) -> NSCell? {
+//        <#code#>
+//    }
+    
+//    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+//        return nil
+//    }
+}
+
+extension ViewController: NSTableViewDelegate {
+    
+}
