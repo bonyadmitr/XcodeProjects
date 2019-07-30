ed56a163c28778b8ad02eff13ccb343078e76678
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-07-30 19:12:35 +0300

    created project UIKeyCommandTest

---
+++ UIKeyCommandTest/UIKeyCommandTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIKeyCommandTest
+//
+//  Created by Yaroslav Bondar on 30/07/2019.
+//  Copyright © 2019 Yaroslav Bondar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
