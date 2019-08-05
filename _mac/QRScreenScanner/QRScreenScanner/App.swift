b0971d6abc9e2ee8c63be12d133ffd63ffa77503
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-08-05 09:22:48 +0300

    cleared code file

---
+++ _mac/QRScreenScanner/QRScreenScanner/App.swift
@@ -0,0 +1,41 @@
+import Cocoa
+
+
+// TODO: https://stackoverflow.com/a/9220857/5893286
+// TODO: https://stackoverflow.com/a/50832237/5893286 + https://stackoverflow.com/a/4686782/5893286
+// TODO: https://developer.apple.com/library/archive/technotes/tn2083/_index.html
+
+final class App {
+    
+    static let shared = App()
+    
+    let statusItemManager = StatusItemManager()
+    let menuManager = MenuManager()
+    
+    private let window: NSWindow = {
+        let window = NSWindow(contentViewController: ViewController())
+        window.center()
+        return window
+    }()
+    
+    func start() {
+        //        ScreenManager.disableHardwareMirroring()
+        //        ScreenManager.allDisplayImages()
+        //        ScreenManager.toggleMirroring()
+        
+        //        let window = ScreenManager.compositedWindow(for: "Google Chrome")
+        //        let w = ScreenManager.compositedWindowsByName()
+        //let e = ScreenManager.windowsByName()
+        //ScreenManager.visibleWindowsImages()
+        
+        
+        menuManager.setup()
+        statusItemManager.setup()
+        showWindow()
+    }
+    
+    func showWindow() {
+        window.makeKeyAndOrderFront(nil)
+        NSApp.activate(ignoringOtherApps: true)
+    }
+}
