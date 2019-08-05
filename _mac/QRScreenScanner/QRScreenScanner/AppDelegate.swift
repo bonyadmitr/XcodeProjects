91ba0f2596f90e83c9ba07df5668c4a8e6679247
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-08-05 09:11:48 +0300

    added todos

--- _mac/QRScreenScanner/QRScreenScanner/AppDelegate.swift
+++ _mac/QRScreenScanner/QRScreenScanner/AppDelegate.swift
@@ -8,6 +8,10 @@
 
 import Cocoa
 
+// TODO: https://stackoverflow.com/a/9220857/5893286
+// TODO: https://stackoverflow.com/a/50832237/5893286 + https://stackoverflow.com/a/4686782/5893286
+// TODO: https://developer.apple.com/library/archive/technotes/tn2083/_index.html
+
 final class MenuManager {
     
     //static let shared = MenuManager()
