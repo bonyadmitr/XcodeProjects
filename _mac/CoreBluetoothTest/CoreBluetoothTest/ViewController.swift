486660ec1b4947337aa73fb29c1ff60279fb0e87
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-08-08 04:48:24 +0300

    created project CoreBluetoothTest

---
+++ _mac/CoreBluetoothTest/CoreBluetoothTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CoreBluetoothTest
+//
+//  Created by Yaroslav Bondar on 07/08/2019.
+//  Copyright © 2019 Yaroslav Bondar. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
