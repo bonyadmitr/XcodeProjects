a29c0bb157e6dc50c6e20052bb051b7e1a83a297
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-08-09 19:09:33 +0300

    fixed namespace

--- _mac/QRScreenScanner/QRScreenScanner/Servies/System.swift
+++ _mac/QRScreenScanner/QRScreenScanner/Servies/System.swift
@@ -15,10 +15,9 @@ import Foundation
 //    }
 //}
 //#endif
-struct System {
+enum System {
     
     static let osVersion: String = {
-        
         let os = ProcessInfo.processInfo.operatingSystemVersion
         return "\(os.majorVersion).\(os.minorVersion).\(os.patchVersion)"
     }()
@@ -46,78 +45,6 @@ struct System {
     
     static let memoryTotal = ProcessInfo.processInfo.physicalMemory
     
-    static func cpuUsage() -> Double {
-        var totalUsageOfCPU: Double = 0.0
-        var threadsList = UnsafeMutablePointer(mutating: [thread_act_t]())
-        var threadsCount = mach_msg_type_number_t(0)
-        let threadsResult = withUnsafeMutablePointer(to: &threadsList) {
-            return $0.withMemoryRebound(to: thread_act_array_t?.self, capacity: 1) {
-                task_threads(mach_task_self_, $0, &threadsCount)
-            }
-        }
-        
-        if threadsResult == KERN_SUCCESS {
-            for index in 0..<threadsCount {
-                var threadInfo = thread_basic_info()
-                var threadInfoCount = mach_msg_type_number_t(THREAD_INFO_MAX)
-                let infoResult = withUnsafeMutablePointer(to: &threadInfo) {
-                    $0.withMemoryRebound(to: integer_t.self, capacity: 1) {
-                        thread_info(threadsList[Int(index)], thread_flavor_t(THREAD_BASIC_INFO), $0, &threadInfoCount)
-                    }
-                }
-                
-                guard infoResult == KERN_SUCCESS else {
-                    break
-                }
-                
-                let threadBasicInfo = threadInfo as thread_basic_info
-                if threadBasicInfo.flags & TH_FLAGS_IDLE == 0 {
-                    totalUsageOfCPU = (totalUsageOfCPU + (Double(threadBasicInfo.cpu_usage) / Double(TH_USAGE_SCALE) * 100.0))
-                }
-            }
-        }
-        
-        vm_deallocate(mach_task_self_, vm_address_t(UInt(bitPattern: threadsList)), vm_size_t(Int(threadsCount) * MemoryLayout<thread_t>.stride))
-        return totalUsageOfCPU
-    }
-    
-    /// can be static
-    static func memoryUsage() -> UInt64 {
-        
-        
-        // TODO: check on device
-        /// memory usage not equal Xcode Debug Gauge
-        //        var taskInfo = mach_task_basic_info()
-        //        var count = mach_msg_type_number_t(MemoryLayout<mach_task_basic_info>.size)/4
-        //        let result: kern_return_t = withUnsafeMutablePointer(to: &taskInfo) {
-        //            $0.withMemoryRebound(to: integer_t.self, capacity: 1) {
-        //                task_info(mach_task_self_, task_flavor_t(MACH_TASK_BASIC_INFO), $0, &count)
-        //            }
-        //        }
-        //
-        //        var used: UInt64 = 0
-        //        if result == KERN_SUCCESS {
-        //            used = UInt64(taskInfo.resident_size)
-        //        }
-        
-        /// resident_size does not get accurate memory, and the correct way is to use phys_footprint, which can be proved from the source codes of WebKit and XNU.
-        /// https://github.com/WebKit/webkit/blob/master/Source/WTF/wtf/cocoa/MemoryFootprintCocoa.cpp
-        var taskInfo = task_vm_info_data_t()
-        var count = mach_msg_type_number_t(MemoryLayout<task_vm_info>.size) / 4
-        let result: kern_return_t = withUnsafeMutablePointer(to: &taskInfo) {
-            $0.withMemoryRebound(to: integer_t.self, capacity: 1) {
-                task_info(mach_task_self_, task_flavor_t(TASK_VM_INFO), $0, &count)
-            }
-        }
-        
-        var used: UInt64 = 0
-        if result == KERN_SUCCESS {
-            used = UInt64(taskInfo.phys_footprint)
-        }
-        
-        return used
-    }
-    
     /// doc https://developer.apple.com/documentation/coreservices/launch_services
     static func defalutBrowserBundleId() -> String {
         return (LSCopyDefaultHandlerForURLScheme("https" as CFString)?.takeRetainedValue() as String?)
@@ -130,6 +57,81 @@ struct System {
         //return (LSCopyAllRoleHandlersForContentType(kUTTypeHTML, .viewer)?.takeRetainedValue() as? [String]).assert(or: [])
         /// addition "com.rockysandstudio.MKPlayer"
         return (LSCopyAllHandlersForURLScheme("https" as CFString)?.takeRetainedValue() as? [String]).assert(or: [])
+    }
+    
+    enum App {
+        
+        static func cpuUsage() -> Double {
+            var totalUsageOfCPU: Double = 0.0
+            var threadsList = UnsafeMutablePointer(mutating: [thread_act_t]())
+            var threadsCount = mach_msg_type_number_t(0)
+            let threadsResult = withUnsafeMutablePointer(to: &threadsList) {
+                return $0.withMemoryRebound(to: thread_act_array_t?.self, capacity: 1) {
+                    task_threads(mach_task_self_, $0, &threadsCount)
+                }
+            }
+            
+            if threadsResult == KERN_SUCCESS {
+                for index in 0..<threadsCount {
+                    var threadInfo = thread_basic_info()
+                    var threadInfoCount = mach_msg_type_number_t(THREAD_INFO_MAX)
+                    let infoResult = withUnsafeMutablePointer(to: &threadInfo) {
+                        $0.withMemoryRebound(to: integer_t.self, capacity: 1) {
+                            thread_info(threadsList[Int(index)], thread_flavor_t(THREAD_BASIC_INFO), $0, &threadInfoCount)
+                        }
+                    }
+                    
+                    guard infoResult == KERN_SUCCESS else {
+                        break
+                    }
+                    
+                    let threadBasicInfo = threadInfo as thread_basic_info
+                    if threadBasicInfo.flags & TH_FLAGS_IDLE == 0 {
+                        totalUsageOfCPU = (totalUsageOfCPU + (Double(threadBasicInfo.cpu_usage) / Double(TH_USAGE_SCALE) * 100.0))
+                    }
+                }
+            }
+            
+            vm_deallocate(mach_task_self_, vm_address_t(UInt(bitPattern: threadsList)), vm_size_t(Int(threadsCount) * MemoryLayout<thread_t>.stride))
+            return totalUsageOfCPU
+        }
+        
+        /// can be static
+        static func memoryUsage() -> UInt64 {
+            
+            
+            // TODO: check on device
+            /// memory usage not equal Xcode Debug Gauge
+            //        var taskInfo = mach_task_basic_info()
+            //        var count = mach_msg_type_number_t(MemoryLayout<mach_task_basic_info>.size)/4
+            //        let result: kern_return_t = withUnsafeMutablePointer(to: &taskInfo) {
+            //            $0.withMemoryRebound(to: integer_t.self, capacity: 1) {
+            //                task_info(mach_task_self_, task_flavor_t(MACH_TASK_BASIC_INFO), $0, &count)
+            //            }
+            //        }
+            //
+            //        var used: UInt64 = 0
+            //        if result == KERN_SUCCESS {
+            //            used = UInt64(taskInfo.resident_size)
+            //        }
+            
+            /// resident_size does not get accurate memory, and the correct way is to use phys_footprint, which can be proved from the source codes of WebKit and XNU.
+            /// https://github.com/WebKit/webkit/blob/master/Source/WTF/wtf/cocoa/MemoryFootprintCocoa.cpp
+            var taskInfo = task_vm_info_data_t()
+            var count = mach_msg_type_number_t(MemoryLayout<task_vm_info>.size) / 4
+            let result: kern_return_t = withUnsafeMutablePointer(to: &taskInfo) {
+                $0.withMemoryRebound(to: integer_t.self, capacity: 1) {
+                    task_info(mach_task_self_, task_flavor_t(TASK_VM_INFO), $0, &count)
+                }
+            }
+            
+            var used: UInt64 = 0
+            if result == KERN_SUCCESS {
+                used = UInt64(taskInfo.phys_footprint)
+            }
+            
+            return used
+        }
     }
 }
 
@@ -144,3 +146,4 @@ extension Optional {
         }
     }
 }
+
