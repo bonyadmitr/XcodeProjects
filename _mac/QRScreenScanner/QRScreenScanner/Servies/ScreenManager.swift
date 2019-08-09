1137efbdded4a5e5629594b779a8a66f8ceb29a5
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-08-09 18:56:34 +0300

    created DisplayConfigurator

--- _mac/QRScreenScanner/QRScreenScanner/Servies/ScreenManager.swift
+++ _mac/QRScreenScanner/QRScreenScanner/Servies/ScreenManager.swift
@@ -153,67 +153,6 @@ final class ScreenManager {
         
         return (0..<allocatedDisplayCount).compactMap { CGDisplayCreateImage(displaysIds[$0]) }
     }
-    
-    //    static func postError(_ error : CGError) {
-    //        assert(error == .success, "failed: \(error)")
-    ////        if error != .success {
-    ////            print("failed: \(error)")
-    ////        }
-    //    }
-    
-    /// https://stackoverflow.com/a/41585973/5893286
-    static func toggleMirroring() {
-        isDisplayedMirrored() ? disableHardwareMirroring() : enableHardwareMirroring()
-    }
-    
-    // determine if mirroring is active (only relevant for software mirroring)
-    // hack to convert from boolean_t (aka UInt32) to swift's bool
-    static func isDisplayedMirrored() -> Bool {
-        return CGDisplayIsInMirrorSet(CGMainDisplayID()) > 0
-    }
-    
-    /// designed for hardware mirroring with > 1 display
-    /// should be no penalty for running with only 1 display, using either hardware or software mirroring drivers
-    /// but not tested
-    /// https://stackoverflow.com/a/41585973/5893286
-    static func disableHardwareMirroring() {
-        configureDisplay { displayConfig in
-            // only interested in the main display
-            // kCGNullDirectDisplay parameter disables hardware mirroring
-            CGConfigureDisplayMirrorOfDisplay(displayConfig, CGMainDisplayID(), kCGNullDirectDisplay).handleError()
-        }
-    }
-    
-    static func enableHardwareMirroring() {
-        let displayCount = displayCount2()
-        assert(displayCount > 1)
-        //assert(isDisplayedMirrored() == false)
-        
-        let mainDisplayId = CGMainDisplayID()
-        
-        configureDisplay { displayConfig in
-            displayIds2(for: displayCount)
-                .filter { $0 != mainDisplayId }
-                .forEach { CGConfigureDisplayMirrorOfDisplay(displayConfig, $0, mainDisplayId).handleError() }
-        }
-    }
-    
-    static func configureDisplay(handler: (_ displayConfig: CGDisplayConfigRef?) -> Void) {
-        //let config = UnsafeMutablePointer<CGDisplayConfigRef?>.allocate(capacity: 1)
-        //config.pointee
-        var displayConfig: CGDisplayConfigRef?
-        CGBeginDisplayConfiguration(&displayConfig).handleError()
-        assert(displayConfig != nil)
-        handler(displayConfig)
-        
-        // The first entry in the list of active displays is the main display. In case of mirroring, the first entry is the largest drawable display or, if all are the same size, the display with the greatest pixel depth.
-        // The "Permanently" option might not survive reboot when run from playground, but does when run in an application
-        // may not be permanent between boots using Playgroud, but is in an application
-        if CGCompleteDisplayConfiguration(displayConfig, .permanently).require() != .success {
-            CGCancelDisplayConfiguration(displayConfig)
-        }
-    }
-    
 }
 
 //@discardableResult
