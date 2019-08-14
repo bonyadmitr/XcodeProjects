e6e5230fdd113b1243d88f64a218dac95ddb8dfb
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-08-14 18:29:23 +0300

    created project ScreenRecorder

---
+++ _mac/ScreenRecorder/ScreenRecorder/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ScreenRecorder
+//
+//  Created by Bondar Yaroslav on 8/14/19.
+//  Copyright © 2019 Bondar Yaroslav. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
