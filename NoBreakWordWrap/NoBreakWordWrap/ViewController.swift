90b91e72a5fcccf1c19175c3d704fb05366d6fd1
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-08-16 17:21:51 +0300

    created project NoBreakWordWrap

---
+++ NoBreakWordWrap/NoBreakWordWrap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NoBreakWordWrap
+//
+//  Created by Bondar Yaroslav on 8/16/19.
+//  Copyright © 2019 Bondar Yaroslav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
