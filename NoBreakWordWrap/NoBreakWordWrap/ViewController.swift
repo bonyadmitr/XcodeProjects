0ca58a6f87a02cd9ec51871fa63b3231bda21878
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-08-16 18:59:15 +0300

    added NSLocalizedString samples

--- NoBreakWordWrap/NoBreakWordWrap/ViewController.swift
+++ NoBreakWordWrap/NoBreakWordWrap/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     @IBOutlet private weak var label3: UILabel!
     @IBOutlet private weak var label4: UILabel!
     @IBOutlet private weak var label5: UILabel!
+    @IBOutlet private weak var label6: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,8 +23,13 @@ class ViewController: UIViewController {
         label1.text = "sometext\u{00a0}Wi\u{2011}Fi sometext"
         label2.text = "sometext Wi\u{2011}Fi sometext"
         label3.text = "sometext Wi‑Fi sometext"
-        label4.text = String(format: "sometext Wi%@Fi sometext", "\u{2011}")
-        label5.text = String(format: "sometext Wi%@Fi sometext", "‑")
+        
+        label4.text = String(format: NSLocalizedString("sometext_wifi_param", comment: ""), "\u{2011}")
+        
+        /// not working \u{2011} in Localizable.strings
+        label5.text = NSLocalizedString("sometext_wifi_sold1", comment: "")
+        
+        label6.text = NSLocalizedString("sometext_wifi_sold2", comment: "")
     }
 
 
