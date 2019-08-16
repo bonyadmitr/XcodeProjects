6bf4e967a92c300a40f363e4ca721ddafdb8e959
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-08-16 18:53:09 +0300

    * added labels * added strings

--- NoBreakWordWrap/NoBreakWordWrap/ViewController.swift
+++ NoBreakWordWrap/NoBreakWordWrap/ViewController.swift
@@ -9,10 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet private weak var label1: UILabel!
+    @IBOutlet private weak var label2: UILabel!
+    @IBOutlet private weak var label3: UILabel!
+    @IBOutlet private weak var label4: UILabel!
+    @IBOutlet private weak var label5: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        label1.text = "sometext\u{00a0}Wi\u{2011}Fi sometext"
+        label2.text = "sometext Wi\u{2011}Fi sometext"
+        label3.text = "sometext Wi‑Fi sometext"
+        label4.text = String(format: "sometext Wi%@Fi sometext", "\u{2011}")
+        label5.text = String(format: "sometext Wi%@Fi sometext", "‑")
     }
 
 
