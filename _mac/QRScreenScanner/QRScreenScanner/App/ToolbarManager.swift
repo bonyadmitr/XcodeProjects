e991ba69c2540e71b584a1f2d5c7be895178a36e
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-08-17 16:24:26 +0300

    added sound extension

--- _mac/QRScreenScanner/QRScreenScanner/App/ToolbarManager.swift
+++ _mac/QRScreenScanner/QRScreenScanner/App/ToolbarManager.swift
@@ -138,3 +138,21 @@ extension ToolbarManager: NSToolbarDelegate {
         return nil
     }
 }
+
+/// https://stackoverflow.com/a/51601171/5893286
+extension NSSound.Name {
+    static let basso     = NSSound.Name("Basso")
+    static let blow      = NSSound.Name("Blow")
+    static let bottle    = NSSound.Name("Bottle")
+    static let frog      = NSSound.Name("Frog")
+    static let funk      = NSSound.Name("Funk")
+    static let glass     = NSSound.Name("Glass")
+    static let hero      = NSSound.Name("Hero")
+    static let morse     = NSSound.Name("Morse")
+    static let ping      = NSSound.Name("Ping")
+    static let pop       = NSSound.Name("Pop")
+    static let purr      = NSSound.Name("Purr")
+    static let sosumi    = NSSound.Name("Sosumi")
+    static let submarine = NSSound.Name("Submarine")
+    static let tink      = NSSound.Name("Tink")
+}
