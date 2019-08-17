766c8832c17a6f193c1a049c043437124773d206
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-08-18 00:01:11 +0300

    created project MuteMic

---
+++ _mac/MuteMic/MuteMic/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MuteMic
+//
+//  Created by Bondar Yaroslav on 8/17/19.
+//  Copyright © 2019 Bondar Yaroslav. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
