c651288975879b5bab8d374518f21042b13b4f71
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-08-22 10:30:32 +0300

    created project PerformanceTests

---
+++ PerformanceTests/PerformanceTests/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PerformanceTests
+//
+//  Created by Bondar Yaroslav on 8/21/19.
+//  Copyright © 2019 Bondar Yaroslav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
