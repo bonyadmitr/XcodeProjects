a3c908654f765b9f059edd38fe347b8f38040c29
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-08-22 08:31:15 +0300

    cleared code

--- _mac/MuteMic/MuteMic/AppDelegate.swift
+++ _mac/MuteMic/MuteMic/AppDelegate.swift
@@ -30,43 +30,5 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
             }
         }
     }
-
-    func applicationWillTerminate(_ aNotification: Notification) {}
-}
-
-
-extension Optional {
-    func assert(or defaultValue: Wrapped) -> Wrapped {
-        switch self {
-        case .none:
-            assertionFailure()
-            return defaultValue
-        case .some(let value):
-            return value
-        }
-    }
     
-    /**
-     it is nonescaping, so there is no perfermance issue
-     
-         text.assertExecute { print($0) }
-     
-    vs
-     
-         if let text = text {
-             print(text)
-         } else {
-             assertionFailure()
-         }
-    
-    */
-    // TODO: check with and without "rethrows"
-    func assertExecute(_ action: (Wrapped) throws -> Void) rethrows {
-        switch self {
-        case .none:
-            assertionFailure()
-        case .some(let value):
-            try action(value)
-        }
-    }
 }
