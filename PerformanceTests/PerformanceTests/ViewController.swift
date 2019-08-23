b1c15aa962bd4e01878bca6ea376b2f3508c67c3
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-08-23 10:58:20 +0300

    cleared project

--- PerformanceTests/PerformanceTests/ViewController.swift
+++ PerformanceTests/PerformanceTests/ViewController.swift
@@ -1,20 +1,11 @@
-//
-//  ViewController.swift
-//  PerformanceTests
-//
-//  Created by Bondar Yaroslav on 8/21/19.
-//  Copyright © 2019 Bondar Yaroslav. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .lightGray
     }
-
-
+    
 }
 
