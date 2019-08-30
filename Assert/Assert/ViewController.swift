be05fc5f120b9ca2d0dec8e6d382c3ffd538b762
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-08-30 15:24:32 +0300

    added extension Optional assert

--- Assert/Assert/ViewController.swift
+++ Assert/Assert/ViewController.swift
@@ -14,7 +14,7 @@ final class ViewController: UIViewController {
         super.viewDidLoad()
         
         testDispatchAssert()
-        
+        testOptionalAssert()
         
         
         view.backgroundColor = .lightGray
@@ -36,4 +36,48 @@ final class ViewController: UIViewController {
         }
     }
     
+    private func testOptionalAssert() {
+        let text: String? = nil//"some"
+        
+        let unwrapedText = text.assert(or: "")
+        print(unwrapedText)
+        
+        text.assertExecute { print($0) }
+    }
+}
+
+extension Optional {
+    func assert(or defaultValue: Wrapped, file: String = #file, line: Int = #line) -> Wrapped {
+        switch self {
+        case .none:
+            assertionFailure("\((file as NSString).lastPathComponent):\(line) is nil")
+            return defaultValue
+        case .some(let value):
+            return value
+        }
+    }
+    
+    /**
+     it is nonescaping, so there is no perfermance issue
+     
+     text.assertExecute { print($0) }
+     
+     vs
+     
+     if let text = text {
+     print(text)
+     } else {
+     assertionFailure()
+     }
+     
+     */
+    // TODO: check with and without "rethrows"
+    func assertExecute(file: String = #file, line: Int = #line, _ action: (Wrapped) throws -> Void) rethrows {
+        switch self {
+        case .none:
+            assertionFailure("\((file as NSString).lastPathComponent):\(line) is nil")
+        case .some(let value):
+            try action(value)
+        }
+    }
 }
