baa62ee2d7e19b57822d25dbb62814a9b2256772
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-08-30 15:11:04 +0300

    created Assert project

---
+++ Assert/Assert/Dispatch+Assert.swift
@@ -0,0 +1,9 @@
+//
+//  Dispatch+Assert.swift
+//  Assert
+//
+//  Created by Bondar Yaroslav on 8/30/19.
+//  Copyright © 2019 Bondar Yaroslav. All rights reserved.
+//
+
+import Foundation
---
+++ Assert/Assert/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assert
+//
+//  Created by Bondar Yaroslav on 8/30/19.
+//  Copyright © 2019 Bondar Yaroslav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
