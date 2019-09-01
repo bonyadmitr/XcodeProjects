c49458533d565861bfe367ce56131fc03549920c
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-09-01 15:13:45 +0300

    added asserts

--- _mac/UndoManagerMac/UndoManagerMac/ViewController.swift
+++ _mac/UndoManagerMac/UndoManagerMac/ViewController.swift
@@ -12,12 +12,19 @@ class ViewController: NSViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        /// window is not shown yet
+        assert(NSApp.keyWindow == nil && undoManager == nil)
+        
+        /// undoManager will not register action
+        //setObject(value: "1")
     }
     
     override func viewWillAppear() {
         super.viewWillAppear()
-
+        
+        /// window is shown
+        assert(NSApp.keyWindow != nil && undoManager != nil)
         setObject(value: "1")
     }
     
