c3adca5479389e53f11d9a8abcf3e6cb213557d9
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-09-02 10:23:55 +0300

    updated main.swift

--- _mac/QRScreenScanner/QRScreenScanner/App/main.swift
+++ _mac/QRScreenScanner/QRScreenScanner/App/main.swift
@@ -1,5 +1,20 @@
 import Cocoa
 
-let delegate = AppDelegate()
-NSApplication.shared.delegate = delegate
-NSApplication.shared.run()
+/// old simple
+//let delegate = AppDelegate()
+//NSApplication.shared.delegate = delegate
+//NSApplication.shared.run()
+
+autoreleasepool {
+    let delegate = AppDelegate()
+    /// code https://github.com/lapcat/NiblessMenu/blob/master/NiblessMenu/main.swift
+    /// NSApplication delegate is a weak reference,
+    /// so we have to make sure it's not deallocated.
+    /// In Objective-C you would use NS_VALID_UNTIL_END_OF_SCOPE
+    withExtendedLifetime(delegate, {
+        let application = NSApplication.shared
+        application.delegate = delegate
+        application.run()
+        application.delegate = nil
+    })
+}
