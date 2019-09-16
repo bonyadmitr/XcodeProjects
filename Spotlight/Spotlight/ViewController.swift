d5c43a01b73e36005b616ae309e1ecf02bc37315
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-09-16 10:35:32 +0300

    created Spotlight project

---
+++ Spotlight/Spotlight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Spotlight
+//
+//  Created by Bondar Yaroslav on 9/16/19.
+//  Copyright © 2019 Bondar Yaroslav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
