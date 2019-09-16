9d7b606645f941d1c2a16e82316875c9d4479dbe
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-09-16 10:42:46 +0300

    added comment

--- Spotlight/Spotlight/AppDelegate.swift
+++ Spotlight/Spotlight/AppDelegate.swift
@@ -11,6 +11,7 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
+    /// needs for iOS 12 and less
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
