343bc45400fbfd04ae98afb40ecaffb0cb2375d8
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-09-17 08:23:16 +0300

    created TouchBarTest project

---
+++ TouchBarTest/TouchBarTest/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  TouchBarTest
+//
+//  Created by Yaroslav Bondar on 17/09/2019.
+//  Copyright © 2019 Yaroslav Bondar. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
