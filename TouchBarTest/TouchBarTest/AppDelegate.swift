c2f5105d9adb66b56c8a41939b22406d5d7e65b1
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-09-18 07:23:37 +0300

    * added TouchBar.h * setup project for private api

--- TouchBarTest/TouchBarTest/AppDelegate.swift
+++ TouchBarTest/TouchBarTest/AppDelegate.swift
@@ -15,13 +15,98 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        
+        TouchBarManager.shared.setup()
+        
     }
-
+    
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
-
 }
 
+final class Window: NSWindow {
+    
+    @available(OSX 10.12.2, *)
+    override func makeTouchBar() -> NSTouchBar? {
+        return TouchBarManager.shared.touchBar
+    }
+}
+
+
+import Cocoa
+
+@available(OSX 10.12.2, *)
+private extension NSTouchBarItem.Identifier {
+    static let button = NSTouchBarItem.Identifier("button")
+    static let segment = NSTouchBarItem.Identifier("segment")
+}
+
+@available(OSX 10.12.2, *)
+private extension NSTouchBar.CustomizationIdentifier {
+    static let main = "Main"
+}
+
+
+
+@available(OSX 10.12.2, *)
+final class TouchBarManager: NSObject, NSTouchBarProvider {
+    
+    static let shared = TouchBarManager()
+    
+    let touchBar: NSTouchBar? = {
+        let touchBar = NSTouchBar()
+        touchBar.customizationIdentifier = .main
+        touchBar.defaultItemIdentifiers = [.button, .segment]
+        touchBar.customizationAllowedItemIdentifiers = [.button, .segment, .fixedSpaceSmall, .fixedSpaceLarge, .flexibleSpace, .otherItemsProxy]
+        return touchBar
+    }()
+    
+    func setup() {
+        touchBar?.delegate = self
+        NSApplication.shared.isAutomaticCustomizeTouchBarMenuItemEnabled = true
+        //            NSApp.touchBar
+    }
+    
+}
+
+@available(OSX 10.12.2, *)
+extension TouchBarManager: NSTouchBarDelegate {
+    
+    func touchBar(_ touchBar: NSTouchBar, makeItemForIdentifier identifier: NSTouchBarItem.Identifier) -> NSTouchBarItem? {
+        
+        /// not good solution bcz there are other NSTouchBarItems like NSPopoverTouchBarItem
+        let item = NSCustomTouchBarItem(identifier: identifier)
+        
+        switch identifier {
+        case .segment:
+            item.customizationLabel = "Segment label"
+            
+            item.view = NSSegmentedControl(labels: ["1", "2", "3"],
+                                           trackingMode: .momentary,
+                                           target: self,
+                                           action: #selector(segmentAction))
+            
+        case .button:
+            item.customizationLabel = "Button label"
+            
+            item.view = NSButton(title: "Button",
+                                 target: self,
+                                 action: #selector(buttonAction))
+            
+        default:
+            break
+            //            assertionFailure("unknown id: \(identifier.rawValue)")
+        }
+        
+        return item
+    }
+    
+    @objc private func segmentAction(_ sender: NSSegmentedControl) {
+        print(sender.selectedSegment)
+    }
+    
+    @objc private func buttonAction() {
+        print("button")
+    }
+}
