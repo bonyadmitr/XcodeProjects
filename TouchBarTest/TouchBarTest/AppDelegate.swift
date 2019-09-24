8b1808b762f2cdc0679469ffa0ca7673bd967398
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-09-24 19:36:15 +0300

    added MultitouchSupport framework but it is not working

--- TouchBarTest/TouchBarTest/AppDelegate.swift
+++ TouchBarTest/TouchBarTest/AppDelegate.swift
@@ -142,9 +142,104 @@ extension TouchBarManager: NSTouchBarDelegate {
     
     @objc private func segmentAction(_ sender: NSSegmentedControl) {
         print(sender.selectedSegment)
+        
+        switch sender.selectedSegment {
+        case 0:
+            HapticFeedback.shared.tap(strong: 1)
+        case 1:
+            HapticFeedback.shared.tap(strong: 2)
+        case 2:
+            HapticFeedback.shared.tap(strong: 3)
+        default:
+            assertionFailure()
+        }
     }
     
     @objc private func buttonAction() {
+        HapticFeedback.shared.tap(strong: 2)
         print("button")
+    }
+}
+
+import IOKit
+
+/// source https://github.com/Toxblh/MTMR/blob/master/MTMR/HapticFeedback.swift
+class HapticFeedback {
+    static let shared = HapticFeedback()
+    
+    // Here we have list of possible IDs for Haptic Generator Device. They are not constant
+    // To find deviceID, you will need IORegistryExplorer app from Additional Tools for Xcode dmg
+    // which you can download from https://developer.apple.com/download/more/?=Additional%20Tools
+    // Open IORegistryExplorer app, search for AppleMultitouchDevice and get "Multitouch ID"
+    // There should be programmatic way to get it but I can't find, no docs for macOS :(
+    private let possibleDeviceIDs: [UInt64] = [
+        0x200_0000_0100_0000, // MacBook Pro 2016/2017
+        0x300000080500000 // MacBook Pro 2019 (possibly 2018 as well)
+    ]
+    private var correctDeviceID: UInt64?
+    private var actuatorRef: CFTypeRef?
+    
+    init() {
+        recreateDevice()
+    }
+    
+    // Don't know how to do strong is enum one of
+    // 1 like back Click
+    // 2 like Click
+    // 3 week
+    // 4 medium
+    // 5 week medium
+    // 6 strong
+    // 15 nothing
+    // 16 nothing
+    // you can get a plist `otool -s __TEXT __tpad_act_plist /System/Library/PrivateFrameworks/MultitouchSupport.framework/Versions/Current/MultitouchSupport|tail -n +3|awk -F'\t' '{print $2}'|xxd -r -p`
+    
+    func tap(strong: Int32) {
+        guard correctDeviceID != nil, actuatorRef != nil else {
+            print("guard actuatorRef == nil (no haptic device found?)")
+            return
+        }
+        
+        var result: IOReturn
+        
+        result = MTActuatorOpen(actuatorRef!)
+        guard result == kIOReturnSuccess else {
+            print("guard MTActuatorOpen")
+            recreateDevice()
+            return
+        }
+        
+        result = MTActuatorActuate(actuatorRef!, strong, 0, 0, 0)
+        guard result == kIOReturnSuccess else {
+            print("guard MTActuatorActuate")
+            return
+        }
+        
+        result = MTActuatorClose(actuatorRef!)
+        guard result == kIOReturnSuccess else {
+            print("guard MTActuatorClose")
+            return
+        }
+    }
+    
+    private func recreateDevice() {
+        if let actuatorRef = actuatorRef {
+            MTActuatorClose(actuatorRef)
+            self.actuatorRef = nil // just in case %)
+        }
+        
+        if let correctDeviceID = correctDeviceID {
+            actuatorRef = MTActuatorCreateFromDeviceID(correctDeviceID).takeRetainedValue()
+        } else {
+            // Let's find our Haptic device
+            possibleDeviceIDs.forEach {(deviceID) in
+                guard correctDeviceID == nil else {return}
+                actuatorRef = MTActuatorCreateFromDeviceID(deviceID).takeRetainedValue()
+                
+                if actuatorRef != nil {
+                    correctDeviceID = deviceID
+                }
+            }
+        }
     }
 }
