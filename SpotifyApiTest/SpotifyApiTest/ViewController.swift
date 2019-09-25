31384ad9b8e25d7895493686d059dfbfe6b245fe
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-09-25 15:44:03 +0300

    created SpotifySDKManager

--- SpotifyApiTest/SpotifyApiTest/ViewController.swift
+++ SpotifyApiTest/SpotifyApiTest/ViewController.swift
@@ -18,3 +18,50 @@ class ViewController: UIViewController {
 
 }
 
+final class SpotifySDKManager: NSObject {
+    
+    static let shared = SpotifySDKManager()
+    
+    /// get from https://developer.spotify.com/dashboard/applications
+    private let clientID = "8ea6ec9161534d84be983e780390a6a7"
+    
+    private let redirectUrl: URL = {
+        if let url = URL(string: "spotifyTest://spotify-login-callback") {
+            return url
+        } else {
+            assertionFailure()
+            return URL(fileURLWithPath: "")
+        }
+    }()
+    
+    private lazy var sessionManager: SPTSessionManager = {
+        let configuration = SPTConfiguration(clientID: clientID, redirectURL: redirectUrl)
+        return SPTSessionManager(configuration: configuration, delegate: self)
+    }()
+    
+    func connectToSporify() {
+        sessionManager.isSpotifyAppInstalled ? connectToSpotifyWithSDK() : ()//showSpotifyAuthWebViewController()
+    }
+    
+    private func connectToSpotifyWithSDK() {
+        let scope: SPTScope = [.playlistReadPrivate, .userLibraryRead]
+        
+        if #available(iOS 11, *) {
+            sessionManager.initiateSession(with: scope, options: .clientOnly)
+        } else if let controller = UIApplication.shared.keyWindow?.rootViewController {
+            sessionManager.initiateSession(with: scope, options: .clientOnly, presenting: controller)
+        } else {
+            assertionFailure("never should be called")
+        }
+    }
+}
+
+extension SpotifySDKManager: SPTSessionManagerDelegate {
+    func sessionManager(manager: SPTSessionManager, didInitiate session: SPTSession) {
+        /// nothing to do
+    }
+    
+    func sessionManager(manager: SPTSessionManager, didFailWith error: Error) {
+        //showSpotifyAuthWebViewController()
+    }
+}
