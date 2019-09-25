b3ce594db5aebaf415bee2dca351be5f5318f9ac
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-09-25 14:46:31 +0300

    created SpotifyApiTest project

---
+++ SpotifyApiTest/SpotifyApiTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpotifyApiTest
+//
+//  Created by Bondar Yaroslav on 9/25/19.
+//  Copyright © 2019 Bondar Yaroslav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
