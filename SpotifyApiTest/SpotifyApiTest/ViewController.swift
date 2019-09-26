55199d2cf8319e158b662cecc964ced99cff9bcc
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-09-26 10:40:52 +0300

    added SpotifyWebViewAuthController presenting

--- SpotifyApiTest/SpotifyApiTest/ViewController.swift
+++ SpotifyApiTest/SpotifyApiTest/ViewController.swift
@@ -12,8 +12,32 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let vc = SpotifyWebViewAuthController()
+        vc.delegate = self
+        present(vc, animated: true, completion: nil)
+    }
 
+}
+
+extension ViewController: SpotifyWebViewAuthControllerDelegate {
+    func spotifyAuthSuccess(with spotifyCode: String) {
+        print("success with code: \(spotifyCode)")
+        
+        /// close SpotifyWebViewAuthController
+        assert(presentedViewController is SpotifyWebViewAuthController)
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func spotifyAuthCancel() {
+        
+    }
+    
 }
