3fa3947f7fd1a6f5da62356afa77967a24829b34
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-09-28 10:25:48 +0300

    cleared code

--- InputView/InputView/ViewController.swift
+++ InputView/InputView/ViewController.swift
@@ -18,40 +18,6 @@ class ViewController: UIViewController {
 
 }
 
-/// can be used UIControl
-class InputView: UIView {
-    
-    private var customInputView: UIView?
-    private var customInputAccessoryView: UIView?
-    
-    override var inputAccessoryView: UIView? {
-        return customInputAccessoryView
-    }
-    
-    override var inputView: UIView? {
-        return customInputView
-    }
-    
-    override var canBecomeFirstResponder: Bool {
-        return inputView != nil
-    }
-    
-    func set(inputView: UIView?, inputAccessoryView: UIView?) {
-        customInputView = inputView
-        customInputAccessoryView = inputAccessoryView
-    }
-    
-    func addTapGestureToOpenInputView() {
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(openInputView))
-        addGestureRecognizer(tapGesture)
-    }
-    
-    @objc func openInputView() {
-        assert(window != nil, "Never call becomeFirstResponder() on a view that is not part of an active view hierarchy")
-        becomeFirstResponder()
-    }
-}
-
 final class DatePickerView: InputView {
     
     override init(frame: CGRect) {
