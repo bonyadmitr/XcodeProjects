a4c7aef052ca739512d0e5688d7c7400da4820cc
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-10-02 10:10:52 +0300

    created project CacheManager

---
+++ CacheManager/CacheManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CacheManager
+//
+//  Created by Bondar Yaroslav on 10/2/19.
+//  Copyright © 2019 Bondar Yaroslav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
