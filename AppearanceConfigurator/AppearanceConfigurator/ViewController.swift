9473b2eff69289ee39708043abd04eb58b293119
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-10-03 17:03:38 +0300

    created AppearanceConfigurator project

---
+++ AppearanceConfigurator/AppearanceConfigurator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppearanceConfigurator
+//
+//  Created by Bondar Yaroslav on 10/3/19.
+//  Copyright © 2019 Bondar Yaroslav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
