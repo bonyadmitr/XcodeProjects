bafa1a44a171226d7b70c713671cfbc4aa170af0
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-11-26 14:35:38 +0300

    cleared code

---
+++ ProductsList/ProductsList/ProductDetailView.swift
@@ -0,0 +1,74 @@
+import UIKit
+
+final class ProductDetailView: UIView {
+    
+    typealias Item = ProductItemDB
+    
+    @IBOutlet private weak var imageView: UIImageView! {
+        willSet {
+            /// newValue used for simplifying copying same settings
+            
+            newValue.contentMode = .scaleAspectFit
+            newValue.isOpaque = true
+            
+            /// used color to show empty state bcz UIActivityIndicatorView is too expensive for performance
+            newValue.backgroundColor = .systemBackground
+            
+            //newValue.clipsToBounds = true
+            newValue.kf.indicatorType = .activity
+        }
+    }
+    
+    @IBOutlet private weak var nameLabel: UILabel! {
+        willSet {
+            /// newValue used for simplifying copying same settings
+            newValue.font = UIFont.preferredFont(forTextStyle: .headline)
+            //newValue.textAlignment = .center
+            newValue.backgroundColor = .systemBackground
+            newValue.textColor = .label
+            newValue.numberOfLines = 1
+        }
+    }
+    
+    @IBOutlet private weak var priceLabel: UILabel! {
+        willSet {
+            /// newValue used for simplifying copying same settings
+            newValue.font = UIFont.preferredFont(forTextStyle: .body)
+            //newValue.textAlignment = .center
+            newValue.backgroundColor = .systemBackground
+            newValue.textColor = .label
+            newValue.numberOfLines = 1
+        }
+    }
+    
+    @IBOutlet private weak var descriptionLabel: UILabel! {
+        willSet {
+            /// newValue used for simplifying copying same settings
+            newValue.font = UIFont.preferredFont(forTextStyle: .body)
+            //newValue.textAlignment = .left
+            newValue.backgroundColor = .systemBackground
+            newValue.textColor = .label
+            newValue.numberOfLines = 0
+            newValue.text = ""
+            newValue.isHidden = true
+        }
+    }
+    
+    func setup(for item: Item) {
+        nameLabel.text = item.name
+        priceLabel.text = "Price: \(item.price)"
+        
+        if let description = item.detail {
+            descriptionLabel.isHidden = false
+            descriptionLabel.text = description
+        }
+        
+        imageView.kf.cancelDownloadTask()
+        imageView.kf.setImage(with: item.imageUrl, placeholder: UIImage(systemName: "photo"))
+    }
+    
+    func setupDetail(from detailedItem: Product.DetailItem) {
+        descriptionLabel.isHidden = false
+        descriptionLabel.text = detailedItem.description
+    }
+}
