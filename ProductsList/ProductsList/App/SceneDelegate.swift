bb418496042f4101dae27a933500fe1815a8c2f5
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-11-28 00:01:50 +0300

    added basic version of state restoration

--- ProductsList/ProductsList/App/SceneDelegate.swift
+++ ProductsList/ProductsList/App/SceneDelegate.swift
@@ -5,12 +5,15 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
         /// SceneDelegate Without Storyboard (short and simple) https://samwize.com/2019/08/05/setup-scenedelegate-without-storyboard/
         /// UIScene programmatic https://medium.com/@ZkHaider/apples-new-uiscene-api-a-programmatic-approach-52d05e382cf2
         guard let windowScene = scene as? UIWindowScene else {
             assertionFailure()
             return
         }
+        // "Each key and value must be of the following types: NSArray, NSData, NSDate, NSDictionary, NSNull, NSNumber, NSSet, NSString, NSURL, or NSUUID"
+//        scene.userActivity = connectionOptions.userActivities.first ?? session.stateRestorationActivity ?? NSUserActivity(activityType: "restoration")
         
         /// or #1
         //let window = UIWindow(frame: windowScene.coordinateSpace.bounds)
@@ -27,7 +30,35 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window.rootViewController = navVC
         window.makeKeyAndVisible()
         
+        // my strategy: each v.c. must have a restorationInfo property...
+        // ...and must be handed this userInfo as it is created...
+        // ...before its viewDidLoad has a chance to run
+        // but then it must destroy it when it no longer needs it!
+//        if let rvc = window?.rootViewController as? RootViewController {
+//            rvc.restorationInfo = scene.userActivity?.userInfo
+//        }
+        
+        //navVC.restoreUserActivityState(<#T##activity: NSUserActivity##NSUserActivity#>)
+        
+        // Do we have an activity to restore?
+        if let userActivity = connectionOptions.userActivities.first ?? session.stateRestorationActivity {
+            // Setup the detail view controller with it's restoration activity.
+            if !configure(window: window, with: userActivity) {
+                print("Failed to restore DetailViewController from \(userActivity)")
+            }
+        }
+        
         self.window = window
+    }
+    
+    private func configure(window: UIWindow?, with activity: NSUserActivity) -> Bool {
+        let detailViewController = ProductDetailController()
+        if let navigationController = window?.rootViewController as? UINavigationController {
+            navigationController.pushViewController(detailViewController, animated: false)
+            detailViewController.restoreUserActivityState(activity)
+            return true
+        }
+        return false
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -42,11 +73,6 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
     }
 
-    func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
-    }
-
     func sceneWillEnterForeground(_ scene: UIScene) {
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
@@ -57,7 +83,24 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
+    
+    // Called when the scene will move from an active state to an inactive state.
+    // This may occur due to temporary interruptions (ex. an incoming phone call).
+    func sceneWillResignActive(_ scene: UIScene) {
+        if let navController = window!.rootViewController as? UINavigationController {
+            if let detailViewController = navController.viewControllers.last as? ProductDetailController {
+                // Fetch the user activity from our detail view controller so restore for later.
+                scene.userActivity = detailViewController.detailUserActivity
+            }
+        }
+    }
 
-
+        
+    // MARK: State Restoration
+    
+    // This is the NSUserActivity that will be used to restore state when the scene reconnects.
+    func stateRestorationActivity(for scene: UIScene) -> NSUserActivity? {
+        return scene.userActivity
+    }
 }
 
--- ProductsList/ProductsList/UI/ProductDetailController/ProductDetailController.swift
+++ ProductsList/ProductsList/UI/ProductDetailController/ProductDetailController.swift
@@ -18,23 +18,30 @@ final class ProductDetailController: UIViewController, ErrorPresenter {
         }
     }()
     
-    private let item: Item
+    private var item: Item?
     
     init(item: Item) {
         self.item = item
         super.init(nibName: nil, bundle: nil)
     }
     
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        self.item = nil
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
     required init?(coder: NSCoder) {
-        assertionFailure("init from code only")
-        self.item = Item()
+        self.item = nil
+        //assertionFailure("init from code only")
         super.init(coder: coder)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setup(for: item)
+        if let item = item {
+            setup(for: item)
+        }
     }
     
     private func setup(for item: Item) {
@@ -78,4 +85,209 @@ final class ProductDetailController: UIViewController, ErrorPresenter {
         }
     }
     
+}
+
+
+
+
+
+
+
+
+
+
+
+// MARK: - NSUserActivity Support
+
+extension ProductDetailController {
+
+    // Encode and decode keys for saving the NSUserActivity to the restoration archive (non-scene based).
+    static let restoreActivityKey = "RestoreActivity"
+    static let activityTitleKey = "title"
+//    static let activityNotesKey = "notes"
+    static let activityIdentifierKey = "identifier"
+//    static let activityEditStateKey = "editState"
+    
+    /** Create the user activity type.
+        Note: The activityType string loaded below must be included in your Info.plist file under the `NSUserActivityTypes` array.
+            More info: https://developer.apple.com/documentation/foundation/nsuseractivity
+    */
+    class var activityType: String {
+        let activityType = ""
+        
+        // Load our activity type from our Info.plist.
+        if let activityTypes = Bundle.main.infoDictionary?["NSUserActivityTypes"] {
+            if let activityArray = activityTypes as? [String] {
+                return activityArray[0]
+            }
+        }
+        
+        return activityType
+    }
+
+    func applyUserActivityEntries(_ activity: NSUserActivity) {
+        guard let item = item else {
+            assertionFailure()
+            return
+        }
+        
+        let itemTitle: [String: String] = [ProductDetailController.activityTitleKey: title!]
+        activity.addUserInfoEntries(from: itemTitle)
+//
+//        let itemNotes: [String: String] = [ProductDetailController.activityNotesKey: detailNotes.text!]
+//        activity.addUserInfoEntries(from: itemNotes)
+//
+//        // We remember the item's identifier for unsaved changes.
+        let itemIdentifier: [String: Any] = [ProductDetailController.activityIdentifierKey: item.objectID.uriRepresentation()]
+        activity.addUserInfoEntries(from: itemIdentifier)
+//
+//        // Remember the edit mode state to restore next time (we compare the orignal note with the unsaved note).
+//        let originalItem = DataSource.shared().itemFromIdentifier(detailItem!.identifier)
+//        let nowEditing = originalItem.title != detailName.text || originalItem.notes != detailNotes.text
+//        let nowEditingSaveState: [String: Bool] = [DetailViewController.activityEditStateKey: nowEditing]
+//        activity.addUserInfoEntries(from: nowEditingSaveState)
+    }
+    
+    // Used to construct an NSUserActivity instance for state restoration.
+    var detailUserActivity: NSUserActivity {
+        let userActivity = NSUserActivity(activityType: ProductDetailController.activityType)
+        userActivity.title = "Restore Item"
+        applyUserActivityEntries(userActivity)
+        return userActivity
+    }
+
+    func restoreItemInterface(_ activityUserInfo: [AnyHashable: Any]) {
+        let itemTitle = activityUserInfo[ProductDetailController.activityTitleKey] as? String
+        title = itemTitle
+        
+        let context = CoreDataStack.shared.viewContext
+//        CoreDataStack.shared.performBackgroundTask { context in
+            if let itemID = activityUserInfo[ProductDetailController.activityIdentifierKey] as? URL,
+                let id = CoreDataStack.shared.managedObjectID(for: itemID),
+                let item = context.object(with: id) as? Item
+            {
+                self.item = item
+                self.setup(for: item)
+            }
+            
+//        }
+        
+        
+        
+        
+        
+        
+        
+//        let itemNotes = activityUserInfo[DetailViewController.activityNotesKey] as? String
+//        let itemIdentifier = activityUserInfo[DetailViewController.activityIdentifierKey] as? String
+//
+//        detailItem = Item(title: itemTitle!, notes: itemNotes!, identifier: itemIdentifier)
+//
+//        if let editingState = activityUserInfo[DetailViewController.activityEditStateKey] as? Bool {
+//            restoredEditState = editingState
+//        }
+    }
+}
+
+// MARK: - UIUserActivityRestoring
+
+extension ProductDetailController {
+    
+    override func updateUserActivityState(_ activity: NSUserActivity) {
+        super.updateUserActivityState(activity)
+        applyUserActivityEntries(activity)
+    }
+
+    override func restoreUserActivityState(_ activity: NSUserActivity) {
+         super.restoreUserActivityState(activity)
+        
+        // Check if the activity is of our type.
+        if activity.activityType == ProductDetailController.activityType {
+            // Get the user activity data.
+            if let activityUserInfo = activity.userInfo {
+                restoreItemInterface(activityUserInfo)
+            }
+        }
+    }
+
+}
+
+// MARK: - State Restoration (UIStateRestoring)
+
+extension ProductDetailController {
+    
+/// - Tag: encodeRestorableState
+    override func encodeRestorableState(with coder: NSCoder) {
+        super.encodeRestorableState(with: coder)
+
+        let encodedActivity = NSUserActivityEncoder(detailUserActivity)
+        coder.encode(encodedActivity, forKey: ProductDetailController.restoreActivityKey)
+    }
+   
+/// - Tag: decodeRestorableState
+    override func decodeRestorableState(with coder: NSCoder) {
+        super.decodeRestorableState(with: coder)
+        
+        if coder.containsValue(forKey: ProductDetailController.restoreActivityKey) {
+            if let decodedActivity = coder.decodeObject(forKey: ProductDetailController.restoreActivityKey) as? NSUserActivityEncoder {
+                if let activityUserInfo = decodedActivity.userActivity.userInfo {
+                    restoreItemInterface(activityUserInfo)
+                }
+            }
+        }
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import Foundation
+
+// MARK: - NSUserActivity Encoding-Decoding Support
+
+/** NSUserActivity does not conform to NSCoding protocol, so we have a convenience wrapper class
+    to be used to store this user activity as an archive for non-scene state restoration.
+*/
+class NSUserActivityEncoder: NSObject, NSCoding {
+    private let activityTypeKey = "activityType"
+    private let activityTitleKey = "activityTitle"
+    private let activityUserInfoKey = "activityUserInfo"
+    
+    private (set) var userActivity: NSUserActivity
+
+    init(_ userActivity: NSUserActivity) {
+        self.userActivity = userActivity
+    }
+
+    required init?(coder: NSCoder) {
+        if let activityType = coder.decodeObject(forKey: activityTypeKey) as? String {
+            userActivity = NSUserActivity(activityType: activityType)
+            if let title = coder.decodeObject(forKey: activityTitleKey) as? String {
+                userActivity.title = title
+            }
+            if let userInfo = coder.decodeObject(forKey: activityUserInfoKey) as? [AnyHashable: Any] {
+                userActivity.userInfo = userInfo
+            }
+        } else {
+            return nil
+        }
+    }
+
+    func encode(with coder: NSCoder) {
+        coder.encode(userActivity.activityType, forKey: activityTypeKey)
+        coder.encode(userActivity.title, forKey: activityTitleKey)
+        coder.encode(userActivity.userInfo, forKey: activityUserInfoKey)
+    }
 }
