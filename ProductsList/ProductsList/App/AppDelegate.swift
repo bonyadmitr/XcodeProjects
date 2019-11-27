d81c56d4a05307bbef1f645c353eff0275046e16
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-11-27 10:03:49 +0300

    * cleared code * added comment

--- ProductsList/ProductsList/App/AppDelegate.swift
+++ ProductsList/ProductsList/App/AppDelegate.swift
@@ -1,17 +1,10 @@
-//
-//  AppDelegate.swift
-//  ProductsList
-//
-//  Created by Bondar Yaroslav on 11/15/19.
-//  Copyright © 2019 Bondar Yaroslav. All rights reserved.
-//
-
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+final class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    /// need for ios < 13
+    //var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
