edadcc13cc3c0084248b97537ccb9555f5d3bc2b
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-12-11 21:57:37 +0300

    created normal notes project

---
+++ Notes/Notes/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  Notes
+//
+//  Created by Yaroslav Bondar on 11.12.2019.
+//  Copyright © 2019 Yaroslav Bondar. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.timestamp!.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureView()
+    }
+
+    var detailItem: Event? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
