2dafa66333b783a6ae00378da48296626addf140
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-12-11 20:10:16 +0300

    created swiftui notes project

---
+++ _swiftui/Notes/Notes/ContentView.swift
@@ -0,0 +1,81 @@
+//
+//  ContentView.swift
+//  Notes
+//
+//  Created by Yaroslav Bondar on 11.12.2019.
+//  Copyright © 2019 Yaroslav Bondar. All rights reserved.
+//
+
+import SwiftUI
+
+private let dateFormatter: DateFormatter = {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateStyle = .medium
+    dateFormatter.timeStyle = .medium
+    return dateFormatter
+}()
+
+struct ContentView: View {
+    @Environment(\.managedObjectContext)
+    var viewContext   
+ 
+    var body: some View {
+        NavigationView {
+            MasterView()
+                .navigationBarTitle(Text("Master"))
+                .navigationBarItems(
+                    leading: EditButton(),
+                    trailing: Button(
+                        action: {
+                            withAnimation { Event.create(in: self.viewContext) }
+                        }
+                    ) { 
+                        Image(systemName: "plus")
+                    }
+                )
+            Text("Detail view content goes here")
+                .navigationBarTitle(Text("Detail"))
+        }.navigationViewStyle(DoubleColumnNavigationViewStyle())
+    }
+}
+
+struct MasterView: View {
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \Event.timestamp, ascending: true)], 
+        animation: .default)
+    var events: FetchedResults<Event>
+
+    @Environment(\.managedObjectContext)
+    var viewContext
+
+    var body: some View {
+        List {
+            ForEach(events, id: \.self) { event in
+                NavigationLink(
+                    destination: DetailView(event: event)
+                ) {
+                    Text("\(event.timestamp!, formatter: dateFormatter)")
+                }
+            }.onDelete { indices in
+                self.events.delete(at: indices, from: self.viewContext)
+            }
+        }
+    }
+}
+
+struct DetailView: View {
+    @ObservedObject var event: Event
+
+    var body: some View {
+        Text("\(event.timestamp!, formatter: dateFormatter)")
+            .navigationBarTitle(Text("Detail"))
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        return ContentView().environment(\.managedObjectContext, context)
+    }
+}
---
+++ _swiftui/Notes/Notes/Event.swift
@@ -0,0 +1,41 @@
+//
+//  Event.swift
+//  Notes
+//
+//  Created by Yaroslav Bondar on 11.12.2019.
+//  Copyright © 2019 Yaroslav Bondar. All rights reserved.
+//
+
+import SwiftUI
+import CoreData
+
+extension Event {
+    static func create(in managedObjectContext: NSManagedObjectContext){
+        let newEvent = self.init(context: managedObjectContext)
+        newEvent.timestamp = Date()
+        
+        do {
+            try  managedObjectContext.save()
+        } catch {
+            // Replace this implementation with code to handle the error appropriately.
+            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            let nserror = error as NSError
+            fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+        }
+    }   
+}
+
+extension Collection where Element == Event, Index == Int {
+    func delete(at indices: IndexSet, from managedObjectContext: NSManagedObjectContext) {
+        indices.forEach { managedObjectContext.delete(self[$0]) }       
+ 
+        do {
+            try managedObjectContext.save()
+        } catch {
+            // Replace this implementation with code to handle the error appropriately.
+            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            let nserror = error as NSError
+            fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+        }
+    }
+}
