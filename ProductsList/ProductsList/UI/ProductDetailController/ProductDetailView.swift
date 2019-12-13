22ad5fb601c8257c5ebcf74eee537ad06e4452cb
Yaroslav Bondar <bonyadmitr@gmail.com>
2019-12-13 10:00:13 +0300

    cleared code

--- ProductsList/ProductsList/UI/ProductDetailController/ProductDetailView.swift
+++ ProductsList/ProductsList/UI/ProductDetailController/ProductDetailView.swift
@@ -6,7 +6,7 @@ protocol Stylable {
     associatedtype StyleType
     
     /// used different func style instead of  @discardableResult
-    func setup(_ style: StyleType)
+    func setStyle(_ style: StyleType)
 }
 //extension Stylable where Self: UILabel {
 //    func setup() {
@@ -20,7 +20,7 @@ protocol Stylable {
 
 extension Stylable {
     func style(_ style: StyleType) -> Self {
-        setup(style)
+        setStyle(style)
         return self
     }
 }
@@ -33,7 +33,7 @@ extension UILabel: Stylable {
         case subtitle
     }
     
-    func setup(_ style: Style) {
+    func setStyle(_ style: Style) {
         switch style {
         case .title:
             font = UIFont.preferredFont(forTextStyle: .headline)
@@ -46,19 +46,6 @@ extension UILabel: Stylable {
         textColor = .label
         numberOfLines = 1
     }
-    
-//    func setup() {
-//        font = UIFont.preferredFont(forTextStyle: .headline)
-//        textAlignment = .center
-//        backgroundColor = .systemBackground
-//        textColor = .label
-//        numberOfLines = 1
-//    }
-//
-//    func style(_ color: UIColor) -> Self {
-//        textColor = color
-//        return self
-//    }
 }
 
 extension UIImageView: Stylable {
@@ -69,7 +56,7 @@ extension UIImageView: Stylable {
         case common
     }
     
-    func setup(_ style: StyleType) {
+    func setStyle(_ style: StyleType) {
         switch style {
         case .kf:
             kf.indicatorType = .activity
@@ -86,13 +73,13 @@ extension UIImageView: Stylable {
     }
 }
 
+/// @Style(style: .subtitle)
+/// { willSet { newValue.setStyle(.title) } }
 @propertyWrapper
 struct Style<T: Stylable> {
     
     var wrappedValue: T? {
-        willSet {
-            newValue?.setup(style)
-        }
+        willSet { newValue?.setStyle(style) }
     }
     
     private let style: T.StyleType
@@ -107,87 +94,33 @@ struct Style<T: Stylable> {
     }
 }
 
+/// @Style2(style: .title)
+/// private let label2 = UILabel().style(.title)
 @propertyWrapper
 struct Style2<T: Stylable> {
     let wrappedValue: T
     
     init(wrappedValue: T, style: T.StyleType) {
-        wrappedValue.setup(style)
+        wrappedValue.setStyle(style)
         self.wrappedValue = wrappedValue
     }
 }
-
 
 final class ProductDetailView: UIView {
     
     typealias Item = ProductItemDB
     
-    //@Style(style: .title)
-//    @Style2(style: .title)
-    private var label1 = UILabel()
-//
-//    private let label2 = UILabel().style(.title)
-    
     @Style(style: .kf)
     @IBOutlet private var imageView: UIImageView!
-//        {
-//        willSet {
-//            /// newValue used for simplifying copying same settings
-//
-//            newValue.contentMode = .scaleAspectFit
-//            newValue.isOpaque = true
-//
-//            /// used color to show empty state bcz UIActivityIndicatorView is too expensive for performance
-//            newValue.backgroundColor = .systemBackground
-//
-//            //newValue.clipsToBounds = true
-//            newValue.kf.indicatorType = .activity
-//        }
-//    }
     
     @Style(style: .title)
     @IBOutlet private var nameLabel: UILabel!
-//        {
-//        willSet { newValue.setup(.title) }
-//    }
-    
-//        {
-//        willSet {
-//            /// newValue used for simplifying copying same settings
-//            newValue.font = UIFont.preferredFont(forTextStyle: .headline)
-//            newValue.textAlignment = .center
-//            newValue.backgroundColor = .systemBackground
-//            newValue.textColor = .label
-//            newValue.numberOfLines = 1
-//        }
-//    }
     
     @Style(style: .subtitle)
     @IBOutlet private var priceLabel: UILabel!
-//        {
-//        willSet {
-//            /// newValue used for simplifying copying same settings
-//            newValue.font = UIFont.preferredFont(forTextStyle: .body)
-//            newValue.textAlignment = .center
-//            newValue.backgroundColor = .systemBackground
-//            newValue.textColor = .label
-//            newValue.numberOfLines = 1
-//        }
-//    }
+    
     @Style(style: .subtitle)
     @IBOutlet private var descriptionLabel: UILabel!
-//        {
-//        willSet {
-//            /// newValue used for simplifying copying same settings
-//            newValue.font = UIFont.preferredFont(forTextStyle: .body)
-//            //newValue.textAlignment = .left
-//            newValue.backgroundColor = .systemBackground
-//            newValue.textColor = .label
-//            newValue.numberOfLines = 0
-//            newValue.text = ""
-//            newValue.isHidden = true
-//        }
-//    }
     
     func setup(for item: Item) {
         nameLabel.text = item.name
