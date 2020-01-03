b6cb67279fad8f2c8c914e361783f50c90e70472
Yaroslav Bondar <bonyadmitr@gmail.com>
2020-01-04 00:54:54 +0300

    started deallocation tests

--- ProductsList/ProductsListTests/ProductsListTests.swift
+++ ProductsList/ProductsListTests/ProductsListTests.swift
@@ -7,6 +7,8 @@
 //
 
 import XCTest
+@testable import ProductsList
+import CoreData
 
 class ProductsListTests: XCTestCase {
 
@@ -18,18 +20,10 @@ class ProductsListTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func test_deallocation_ProductsListController() {
+        assertDeallocationPresentedVC { ProductsListController() }
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+    
 }
 
 // MARK: - XCTestCase+Deallocation
