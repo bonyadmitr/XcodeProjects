e7480d31fada25c76a0689d38165e1a5aace3b5c
Yaroslav Bondar <bonyadmitr@gmail.com>
2020-01-06 19:52:52 +0300

    added protocol NibInit

--- ProductsList/ProductsListTests/ProductsListTests.swift
+++ ProductsList/ProductsListTests/ProductsListTests.swift
@@ -10,6 +10,23 @@ import XCTest
 @testable import ProductsList
 import CoreData
 
+protocol NibInit {}
+extension NibInit where Self: UIView {
+    static func initFromNib() -> Self {
+        let nibName = String(describing: Self.self)
+        let nib = UINib(nibName: nibName, bundle: nil)
+        return nib.instantiate(withOwner: nil, options: nil)[0] as! Self
+    }
+}
+
+extension NibInit where Self: UIViewController {
+    static func initFromNib() -> Self {
+        let nibName = String(describing: Self.self)
+        return self.init(nibName: nibName, bundle: nil)
+    }
+}
+
+
 class ProductsListTests: XCTestCase {
     
     typealias Item = ProductItemDB
