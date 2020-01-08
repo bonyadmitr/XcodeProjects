63f9b9bf5a9cd2c4dccde2af053cee4f83463404
Yaroslav Bondar <bonyadmitr@gmail.com>
2020-01-08 11:10:23 +0300

    added test_previewDismiss

--- ProductsList/ProductsListUITests/ProductsListUITests.swift
+++ ProductsList/ProductsListUITests/ProductsListUITests.swift
@@ -10,34 +10,64 @@ import XCTest
 
 class ProductsListUITests: XCTestCase {
 
+    var app: XCUIApplication!
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
+                
         // In UI tests it is usually best to stop immediately when a failure occurs.
+        //
+        // Since UI tests are more expensive to run, it's usually a good idea
+        // to exit if a failure was encountered
         continueAfterFailure = false
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        app = XCUIApplication()
+
+        // We send a command line argument to our app,
+        // to enable it to reset its state
+        //app.launchArguments.append("--uitesting")
+        app.launchArguments.append("DISABLE_ANIMATIONS")
+        
+        app.launch()
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func test_previewDismiss() {
+        /// wait cells appear after network request
+        //let cell = app.cells["IMG_0352.JPG"].firstMatch
+        let cell = app.collectionViews.cells.firstMatch
+        //let cell = app.collectionViews.cells.firstMatch.children(matching: .other).element
+        XCTAssertTrue(cell.waitForExistence(timeout: 2))
+        
+        /// open popup
+        cell.press(forDuration: 1.0)
+        
+        /// check popup
+        let isPopupExists = app.scrollViews.otherElements.buttons["Share"].exists
+        XCTAssertTrue(isPopupExists)
+        
+        /// close popup
+        app.swipeDown()
+        //app.tap()
+        
+        /// wait popup close
+        sleep(1)
+        
+        /// to handle possible crash
+        /// can be put into "override func tearDown()"
+        XCTAssertTrue(app.isEnabled)
     }
 
-    func testLaunchPerformance() {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
+//    func testLaunchPerformance() {
+//        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
+//            // This measures how long it takes to launch your application.
+//            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
+//                XCUIApplication().launch()
+//            }
+//        }
+//    }
 }
