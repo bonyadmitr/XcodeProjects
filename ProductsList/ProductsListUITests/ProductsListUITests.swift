af8ace07b614104b5acb5ae04495a62186d9a0f4
Yaroslav Bondar <bonyadmitr@gmail.com>
2020-01-09 09:04:28 +0300

    added func swipeBack() for tests

--- ProductsList/ProductsListUITests/ProductsListUITests.swift
+++ ProductsList/ProductsListUITests/ProductsListUITests.swift
@@ -76,16 +76,19 @@ class ProductsListUITests: XCTestCase {
         XCTContext.runActivity(named: "cell tap") { _ in
             let cell = collectionView.cells.element(boundBy: 4)
             cell.tap()
+            sleep(1)
         }
         
         /// back / pop
         XCTContext.runActivity(named: "pop") { _ in
-            app.navigationBars.buttons.firstMatch.tap()
-                    
-            //        app.windows.element(boundBy:0).swipeLeft()
-
-            //        let element = app.scrollViews.children(matching: .other).element(boundBy: 0)
-            //        element.swipeRight()
+//            app.swipeBack()
+            
+            /// working
+//            isHittable
+            let backButton = app.navigationBars.buttons.firstMatch
+            if backButton.isHittable {
+                backButton.tap()
+            }
         }
         
         XCTContext.runActivity(named: "app.isEnabled") { _ in
@@ -107,4 +110,22 @@ class ProductsListUITests: XCTestCase {
 //            }
 //        }
 //    }
+}
+
+extension XCUIApplication {
+    
+    /// objc source https://stackoverflow.com/a/34816101/5893286
+    func swipeBack() {
+        let topLeftCoordinate = self.coordinate(withNormalizedOffset: .init(dx: 0.01, dy: 0.15))
+        let topLeftOffsetCoordinate = topLeftCoordinate.withOffset(.init(dx: 50, dy: 0))
+        topLeftCoordinate.press(forDuration: 0.5, thenDragTo: topLeftOffsetCoordinate)
+        
+        /// not working
+        //app.windows.element(boundBy:0).swipeLeft()
+        
+        /// not working
+        //let element = app.scrollViews.children(matching: .other).element(boundBy: 0)
+        //element.swipeRight()
+    }
+    
 }
