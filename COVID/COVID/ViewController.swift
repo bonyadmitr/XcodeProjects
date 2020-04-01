6a79a0030696b1a6830bf93c51e2a3b8db30cb0c
Yaroslav Bondar <bonyadmitr@gmail.com>
2020-04-01 23:15:27 +0300

    added temp extensions

--- COVID/COVID/ViewController.swift
+++ COVID/COVID/ViewController.swift
@@ -95,6 +95,40 @@ extension Error {
     }
 }
 
+//extension Data {
+//    var stringValue: String {
+//        return String(data: self, encoding: .utf8) ?? String(describing: self)
+//    }
+//}
+
+//extension Collection where Indices.Iterator.Element == Index {
+//
+//    /// article https://medium.com/flawless-app-stories/say-goodbye-to-index-out-of-range-swift-eca7c4c7b6ca
+//    subscript(safe index: Index) -> Iterator.Element? {
+//        return (startIndex <= index && index < endIndex) ? self[index] : nil
+//    }
+//}
+
+//extension JSONDecoder {
+//
+//    /// JSONDecoder keypath
+//    /// another solution https://github.com/dgrzeszczak/KeyedCodable/
+//    /// another solution https://gist.github.com/aunnnn/9a6b4608ae49fe1594dbcabd9e607834
+//    /// another solution https://github.com/aunnnn/NestedDecodable
+//    ///
+//    /// code https://gist.github.com/sgr-ksmt/d3b79ed1504768f2058c5ea06dc93698
+//    func decode<T: Decodable>(_ type: T.Type, from data: Data, keyPath: String) throws -> T {
+//        let toplevel = try JSONSerialization.jsonObject(with: data)
+//        if let nestedJson = (toplevel as AnyObject).value(forKeyPath: keyPath) {
+//            let nestedJsonData = try JSONSerialization.data(withJSONObject: nestedJson)
+//            return try decode(type, from: nestedJsonData)
+//        } else {
+//            throw DecodingError.dataCorrupted(.init(codingPath: [], debugDescription: "Nested json not found for key path \"\(keyPath)\""))
+//        }
+//    }
+//}
+
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
