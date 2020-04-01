b6abeb1e3cc839bda6f6e7509d5c7cf4f62e66ad
Yaroslav Bondar <bonyadmitr@gmail.com>
2020-04-01 20:54:30 +0300

    created new project COVID

---
+++ COVID/COVID/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  COVID
+//
+//  Created by Bondar Yaroslav on 4/1/20.
+//  Copyright © 2020 Bondar Yaroslav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
