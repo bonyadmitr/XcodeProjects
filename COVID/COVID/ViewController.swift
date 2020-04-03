8f88101a2fdfa7ba0b1f55cc6a43cee2cd799073
Yaroslav Bondar <bonyadmitr@gmail.com>
2020-04-03 08:40:43 +0300

    added CountyDetailController

--- COVID/COVID/ViewController.swift
+++ COVID/COVID/ViewController.swift
@@ -259,6 +259,30 @@ extension CountiesController: UITableViewDelegate {
     }
 }
 
+class CountyDetailController: UIViewController {
+
+    let textLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+        
+        view.addSubview(textLabel)
+        textLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            textLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            textLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            textLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16)
+        ])
+    }
+    
+}
+
 class ViewController: UIViewController {
     
     private let globalInfoLabel: UILabel = {
