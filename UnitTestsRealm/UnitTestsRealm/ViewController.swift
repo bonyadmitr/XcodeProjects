23c373fadfa9821d8c0322c4d195aa31d252ccf2
Yaroslav Bondar <bonyadmitr@gmail.com>
2020-04-10 14:05:21 +0300

    created project UnitTestsRealm

---
+++ UnitTestsRealm/UnitTestsRealm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UnitTestsRealm
+//
+//  Created by Bondar Yaroslav on 4/10/20.
+//  Copyright © 2020 Bondar Yaroslav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
