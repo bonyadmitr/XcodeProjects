a3e988c5b0d8b38b569f21b7352516b23d7aa647
Yaroslav Bondar <bonyadmitr@gmail.com>
2020-04-15 16:02:19 +0300

    created DocumentScanner

--- OCRManager/OCRManager/ViewController.swift
+++ OCRManager/OCRManager/ViewController.swift
@@ -10,11 +10,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let documentScanner = DocumentScanner()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        
+        documentScanner.open(in: self) { images in
+            for image in images {
+            }
+        }
+    }
 
 }
 
+import VisionKit
+
+final class DocumentScanner: NSObject {
+    
+    private var handler: (([UIImage]) -> Void)?
+    
+    func open(in vc: UIViewController, handler: @escaping ([UIImage]) -> Void) {
+        #if targetEnvironment(simulator)
+        /// crash 'NSGenericException', reason: 'Document camera is not available'
+        print("- DocumentScanner is not available for simulator")
+        #else
+        
+        self.handler = handler
+        let scanVC = VNDocumentCameraViewController()
+        scanVC.delegate = self
+        vc.present(scanVC, animated: true)
+        #endif
+    }
+}
+
+extension DocumentScanner: VNDocumentCameraViewControllerDelegate {
+    func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFinishWith scan: VNDocumentCameraScan) {
+        print("- scan.title:", scan.title)
+        let images = (0 ..< scan.pageCount).map { scan.imageOfPage(at: $0) }
+        handler?(images)
+        controller.dismiss(animated: true)
+    }
+    
+//    func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFailWithError error: Error) {
+//        //Handle properly error
+//        controller.dismiss(animated: true)
+//    }
+//
+//    func documentCameraViewControllerDidCancel(_ controller: VNDocumentCameraViewController) {
+//        controller.dismiss(animated: true)
+//    }
+}
