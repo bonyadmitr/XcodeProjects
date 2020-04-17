0c7a957c171131393b9800db285e7eb87e74e843
Yaroslav Bondar <bonyadmitr@gmail.com>
2020-04-17 10:42:07 +0300

    added Decibel links

--- AudioComponents/AudioComponents/ViewController.swift
+++ AudioComponents/AudioComponents/ViewController.swift
@@ -8,6 +8,12 @@
 
 import UIKit
 
+/// Decibel
+/// https://github.com/segmentio/decibel
+/// http://www.mikitamanko.com/blog/2017/04/15/swift-how-to-get-decibels/
+/// https://stackoverflow.com/a/38342286/5893286
+/// https://stackoverflow.com/a/9812267/5893286
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
