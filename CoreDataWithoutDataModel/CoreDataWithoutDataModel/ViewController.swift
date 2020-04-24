2a6c0469c30f3808174f909972c4b57b8c834df1
Yaroslav Bondar <bonyadmitr@gmail.com>
2020-04-24 13:42:04 +0300

    added helpers

--- CoreDataWithoutDataModel/CoreDataWithoutDataModel/ViewController.swift
+++ CoreDataWithoutDataModel/CoreDataWithoutDataModel/ViewController.swift
@@ -13,8 +13,51 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+import CoreData
+
+extension NSEntityDescription {
+    convenience init(from classType: AnyClass) {
+        let className = NSStringFromClass(classType)
+        self.init()
+        self.name = className
+        self.managedObjectClassName = className
     }
-
-
+    
+    func addProperty(_ property: NSPropertyDescription) {
+        self.properties.append(property)
+    }
 }
 
+extension NSAttributeDescription {
+    convenience init(name: String, ofType: NSAttributeType, isOptional: Bool = false) {
+        self.init()
+        self.name = name
+        self.attributeType = ofType
+        self.isOptional = isOptional
+    }
+}
+
+extension NSManagedObject {
+    class func fetchRequest<T: NSManagedObject>() -> NSFetchRequest<T> {
+        let className = NSStringFromClass(self)//String(describing: self)
+        return NSFetchRequest<T>(entityName: className)
+    }
+}
+
+//protocol ClassNameable {
+//    static func className() -> String
+//    func className() -> String
+//}
+//
+//extension ClassNameable {
+//    static func className() -> String {
+//        //return NSStringFromClass(self)
+//        return String(describing: self)
+//    }
+//
+//    func className() -> String {
+//        return String(describing: type(of: self))
+//    }
+//}
+//
+//extension NSObject: ClassNameable {}
