014a6ee786b957abb05d600ac9d239553f2ad22d
Yaroslav Bondar <bonyadmitr@gmail.com>
2020-04-28 14:42:56 +0300

    created project EmptyProjectSceneDelegate

---
+++ EmptyProjectSceneDelegate/EmptyProject/AppDelegate.swift
@@ -0,0 +1,70 @@
+//
+//  AppDelegate.swift
+//  EmptyProject
+//
+//  Created by Yaroslav Bondar on 29/11/2018.
+//  Copyright © 2018 Yaroslav Bondar. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        window = UIWindow()
+        window?.backgroundColor = UIColor.cyan
+        window?.rootViewController = UIViewController()
+        window?.makeKeyAndVisible()
+
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    @available(iOS 13.0, *)
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        let sceneConfig = UISceneConfiguration()
+        sceneConfig.delegateClass = SceneDelegate.self
+        return sceneConfig
+        
+        //return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+    
+    @available(iOS 13.0, *)
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+}
+
+import UIKit
+
+@available(iOS 13.0, *)
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
+        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
+        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        guard let scene = scene as? UIWindowScene else {
+            assertionFailure()
+            return
+        }
+        
+        window = UIWindow(windowScene: scene)
+        window?.backgroundColor = UIColor.cyan
+        window?.rootViewController = UIViewController()
+        window?.makeKeyAndVisible()
+    }
+}
