7db0ba3fc5bfa32cd5618a9e34758a90bd4f3728
Yaroslav Bondar <bonyadmitr@gmail.com>
2020-05-04 16:10:51 +0300

    created project DynamicFont

---
+++ DynamicFont/DynamicFont/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DynamicFont
+//
+//  Created by Bondar Yaroslav on 5/4/20.
+//  Copyright © 2020 Bondar Yaroslav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
