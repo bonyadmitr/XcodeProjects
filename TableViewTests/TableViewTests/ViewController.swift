8a596f0330767ac448492e7f37b827391780951d
Yaroslav Bondar <bonyadmitr@gmail.com>
2020-05-13 00:51:56 +0300

    created TableViewTests

---
+++ TableViewTests/TableViewTests/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewTests
+//
+//  Created by Bondar Yaroslav on 5/12/20.
+//  Copyright © 2020 Bondar Yaroslav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
