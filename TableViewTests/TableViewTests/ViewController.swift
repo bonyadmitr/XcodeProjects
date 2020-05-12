1c7399fe12063acec897bf70df471250eab6741e
Yaroslav Bondar <bonyadmitr@gmail.com>
2020-05-13 00:52:48 +0300

    added table logic

--- TableViewTests/TableViewTests/ViewController.swift
+++ TableViewTests/TableViewTests/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private weak var tableView: UITableView! {
+        willSet {
+            newValue.dataSource = self
+            newValue.delegate = self
+            newValue.prefetchDataSource = self
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -18,3 +26,78 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1000
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        print("- cellForRowAt: \(indexPath.row)")
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TableCell", for: indexPath) as! TableCell
+        print("- 0", cell.textLabel!.bounds.height)
+        
+        cell.indexPath = indexPath
+        cell.textLabel?.numberOfLines = 0
+        //cell.textLabel?.text = "Row \(indexPath.row + 1) \n\n\n\n\n\n\n---Row \(indexPath.row + 1)"
+        print("- 1", cell.textLabel!.bounds.height)
+        
+        /// insert big tet to test UILable memory cost
+//        cell.textLabel?.text = """
+//        """
+        
+        return cell
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+//        print("*  willDisplay: \(indexPath.row)")
+        
+//        cell.textLabel?.numberOfLines = 0
+//        cell.textLabel?.text = "Row \(indexPath.row + 1) \n\n\n\n\n\n\n---Row \(indexPath.row + 1)"
+        
+        print("- 2", cell.textLabel!.bounds.height)
+        
+//        cell.textLabel?.text = "Row \(indexPath.row + 1)"
+    }
+    
+    func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        cell.textLabel?.text = nil
+    }
+    
+    /// called twice
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return 44
+        //print("% heightForRowAt: \(indexPath.row)")
+        return UITableView.automaticDimension
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
+extension ViewController: UITableViewDataSourcePrefetching {
+    
+    func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
+        //print("# prefetchRowsAt: \(indexPaths.map{$0.row})")
+    }
+    
+}
+
+final class TableCell: UITableViewCell {
+    
+    var indexPath: IndexPath?
+    
+    override func layoutSubviews() {
+        print("- 4", textLabel!.bounds.height)
+        super.layoutSubviews()
+        
+        
+        print("- 5", textLabel!.bounds.height)
+        
+        print("- layoutSubviews \(indexPath!.row)")
+    }
+    
+}
