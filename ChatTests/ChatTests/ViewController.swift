766bc0a61e2c33144569a6dac37069ae219f79c3
Yaroslav Bondar <bonyadmitr@gmail.com>
2020-05-20 00:05:42 +0300

    created ChatViewController by MessageKit

--- ChatTests/ChatTests/ViewController.swift
+++ ChatTests/ChatTests/ViewController.swift
@@ -7,14 +7,328 @@
 //
 
 import UIKit
+import MessageKit
+import InputBarAccessoryView
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
 
 
 }
 
+struct User: SenderType {
+    let senderId: String
+    let displayName: String
+}
+
+struct MockMessage: MessageType {
+    
+    let messageId: String
+    let sentDate: Date
+    let kind: MessageKind
+    let user: User
+    
+    var sender: SenderType {
+        return user
+    }
+    
+    private init(kind: MessageKind, user: User, messageId: String, date: Date) {
+        self.kind = kind
+        self.user = user
+        self.messageId = messageId
+        self.sentDate = date
+    }
+    
+    init(text: String, user: User, messageId: String, date: Date) {
+        self.init(kind: .text(text), user: user, messageId: messageId, date: date)
+    }
+    
+}
+
+
+extension UIColor {
+    static let primaryColor = UIColor.magenta
+}
+
+final class DataProvider {
+    
+    static let shared = DataProvider()
+    
+    let meUser = User(senderId: "0", displayName: "Me")
+    let managerUser = User(senderId: "b1", displayName: "Manager")
+    
+    lazy var messages = [
+        MockMessage(text: "wqiuehqwiueh\nweqe", user: meUser, messageId: UUID().uuidString, date: Date()),
+        MockMessage(text: "12312", user: meUser, messageId: UUID().uuidString, date: Date().addingTimeInterval(3600)),
+        MockMessage(text: "uiui", user: managerUser, messageId: UUID().uuidString, date: Date()),
+        MockMessage(text: "wqiuehqwiueh\nweqe", user: managerUser, messageId: UUID().uuidString, date: Date()),
+        MockMessage(text: "12312", user: meUser, messageId: UUID().uuidString, date: Date()),
+    ]
+    
+}
+
+import UIKit
+import MessageKit
+import InputBarAccessoryView
+
+/// A base class
+/// lib https://github.com/MessageKit/MessageKit
+/// basic example https://github.com/MessageKit/MessageKit/blob/master/Example/Sources/View%20Controllers/ChatViewController.swift
+/// there is no start from bottom https://github.com/MessageKit/MessageKit/issues?q=is%3Aissue+CGAffineTransform
+class ChatViewController: MessagesViewController, MessagesDataSource {
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    /// The `BasicAudioController` controll the AVAudioPlayer state (play, pause, stop) and udpate audio cell UI accordingly.
+    //open lazy var audioController = BasicAudioController(messageCollectionView: messagesCollectionView)
+    
+    var messageList: [MockMessage] = []
+    
+    let refreshControl = UIRefreshControl()
+    
+    let formatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        return formatter
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureMessageCollectionView()
+        configureMessageInputBar()
+        loadFirstMessages()
+        title = "MessageKit"
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+//        MockSocket.shared.connect(with: [SampleData.shared.nathan, SampleData.shared.wu])
+//            .onNewMessage { [weak self] message in
+//                self?.insertMessage(message)
+//        }
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+//        MockSocket.shared.disconnect()
+//        audioController.stopAnyOngoingPlaying()
+    }
+    
+    func loadFirstMessages() {
+        DispatchQueue.global(qos: .userInitiated).async {
+            let messages = DataProvider.shared.messages
+            
+            DispatchQueue.main.async {
+                self.messageList = messages
+                self.messagesCollectionView.reloadData()
+                self.messagesCollectionView.scrollToLastItem()
+//                self.messagesCollectionView.scrollToBottom()
+            }
+        }
+    }
+    
+    @objc
+    func loadMoreMessages() {
+        DispatchQueue.global(qos: .userInitiated).asyncAfter(deadline: .now() + 1) {
+            let messages = DataProvider.shared.messages
+            
+            DispatchQueue.main.async {
+                self.messageList.insert(contentsOf: messages, at: 0)
+                self.messagesCollectionView.reloadDataAndKeepOffset()
+                self.refreshControl.endRefreshing()
+            }
+        }
+    }
+    
+    func configureMessageCollectionView() {
+        
+        messagesCollectionView.messagesDataSource = self
+        messagesCollectionView.messagesLayoutDelegate = self
+        messagesCollectionView.messagesDisplayDelegate = self
+        messagesCollectionView.messageCellDelegate = self
+        
+        scrollsToBottomOnKeyboardBeginsEditing = true // default false
+        maintainPositionOnKeyboardFrameChanged = true // default false
+        
+        messagesCollectionView.addSubview(refreshControl)
+        refreshControl.addTarget(self, action: #selector(loadMoreMessages), for: .valueChanged)
+        
+        // Hide the outgoing avatar and adjust the label alignment to line up with the messages
+        if let layout = messagesCollectionView.collectionViewLayout as? MessagesCollectionViewFlowLayout {
+            //layout.textMessageSizeCalculator.outgoingAvatarSize = .zero
+            //layout.textMessageSizeCalculator.incomingAvatarSize = .zero
+            
+            ///or
+            layout.setMessageOutgoingAvatarSize(.zero)
+            layout.setMessageIncomingAvatarSize(.zero)
+        }
+    }
+    
+    func configureMessageInputBar() {
+        
+        messageInputBar.setRightStackViewWidthConstant(to: 36, animated: false)
+        messageInputBar.sendButton.imageView?.backgroundColor = UIColor(white: 0.85, alpha: 1)
+        messageInputBar.sendButton.contentEdgeInsets = UIEdgeInsets(top: 2, left: 2, bottom: 2, right: 2)
+        messageInputBar.sendButton.setSize(CGSize(width: 36, height: 36), animated: false)
+        
+        //messageInputBar.sendButton.image = UIImage(systemName: "arrow.up")
+//        messageInputBar.sendButton.title = nil
+        
+        messageInputBar.sendButton.title = "↑"
+        messageInputBar.sendButton.titleLabel?.font = UIFont.systemFont(ofSize: 30)
+        
+        messageInputBar.sendButton.imageView?.layer.cornerRadius = 16
+        messageInputBar.middleContentViewPadding.right = -38
+        
+        messageInputBar.delegate = self
+        
+//        messageInputBar.inputTextView.tintColor = .primaryColor
+//        messageInputBar.sendButton.setTitleColor(.primaryColor, for: .normal)
+//        messageInputBar.sendButton.setTitleColor(
+//            UIColor.primaryColor.withAlphaComponent(0.3),
+//            for: .highlighted
+//        )
+    }
+    
+    // MARK: - Helpers
+    
+    func insertMessage(_ message: MockMessage) {
+        messageList.append(message)
+        // Reload last section to update header/footer labels and insert a new one
+        messagesCollectionView.performBatchUpdates({
+            messagesCollectionView.insertSections([messageList.count - 1])
+            if messageList.count >= 2 {
+                messagesCollectionView.reloadSections([messageList.count - 2])
+            }
+        }, completion: { [weak self] _ in
+            if self?.isLastSectionVisible() == true {
+                self?.messagesCollectionView.scrollToBottom(animated: true)
+            }
+        })
+    }
+    
+    func isLastSectionVisible() -> Bool {
+        guard !messageList.isEmpty else { return false }
+        let lastIndexPath = IndexPath(item: 0, section: messageList.count - 1)
+        return messagesCollectionView.indexPathsForVisibleItems.contains(lastIndexPath)
+    }
+    
+    // MARK: - MessagesDataSource
+    
+    func currentSender() -> SenderType {
+        return DataProvider.shared.meUser
+    }
+    
+    func numberOfSections(in messagesCollectionView: MessagesCollectionView) -> Int {
+        return messageList.count
+    }
+    
+    func messageForItem(at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> MessageType {
+        return messageList[indexPath.section]
+    }
+}
+
+/// The MessagesLayoutDelegate and MessagesDisplayDelegate don't require you to implement any methods as they have default implementations for everything. You just need to make your MessagesViewController subclass conform to these two protocols and set them in the MessagesCollectionView object
+extension ChatViewController: MessagesDisplayDelegate, MessagesLayoutDelegate {
+    
+    func messageStyle(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> MessageStyle {
+        let tail: MessageStyle.TailCorner = isFromCurrentSender(message: message) ? .bottomRight : .bottomLeft
+        return .bubbleTail(tail, .curved)
+    }
+    
+    /// or:
+    ///layout?.setMessageOutgoingAvatarSize(.zero)
+    ///layout?.setMessageIncomingAvatarSize(.zero)
+//    func configureAvatarView(_ avatarView: AvatarView, for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) {
+//        avatarView.isHidden = true
+//    }
+    
+//    func textColor(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> UIColor {
+//        return isFromCurrentSender(message: message) ? .white : .darkText
+//    }
+    
+//    func backgroundColor(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> UIColor {
+//        return isFromCurrentSender(message: message) ? .primaryColor : UIColor(red: 230/255, green: 230/255, blue: 230/255, alpha: 1)
+//    }
+    
+//    func configureAvatarView(_ avatarView: AvatarView, for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) {
+//
+//        let sender = message.sender
+//
+//        let firstName = sender.displayName.components(separatedBy: " ").first
+//        let lastName = sender.displayName.components(separatedBy: " ").first
+//        let initials = "\(firstName?.first ?? "A")\(lastName?.first ?? "A")"
+//
+//        let avatar = Avatar(image: nil, initials: initials)
+//        avatarView.set(avatar: avatar)
+//    }
+}
+
+// MARK: - MessageInputBarDelegate
+extension ChatViewController: InputBarAccessoryViewDelegate {
+
+    func inputBar(_ inputBar: InputBarAccessoryView, didPressSendButtonWith text: String) {
+
+        // Here we can parse for which substrings were autocompleted
+        let attributedText = messageInputBar.inputTextView.attributedText!
+        let range = NSRange(location: 0, length: attributedText.length)
+        attributedText.enumerateAttribute(.autocompleted, in: range, options: []) { (_, range, _) in
+
+            let substring = attributedText.attributedSubstring(from: range)
+            let context = substring.attribute(.autocompletedContext, at: 0, effectiveRange: nil)
+            print("Autocompleted: `", substring, "` with context: ", context ?? [])
+        }
+
+        let components = inputBar.inputTextView.components
+        messageInputBar.inputTextView.text = String()
+        messageInputBar.invalidatePlugins()
+
+        // Send button activity animation
+        messageInputBar.sendButton.startAnimating()
+        messageInputBar.inputTextView.placeholder = "Sending..."
+        DispatchQueue.global(qos: .default).async {
+            // fake send request task
+            sleep(1)
+            DispatchQueue.main.async { [weak self] in
+                self?.messageInputBar.sendButton.stopAnimating()
+                self?.messageInputBar.inputTextView.placeholder = "Aa"
+                self?.insertMessages(components)
+                self?.messagesCollectionView.scrollToBottom(animated: true)
+            }
+        }
+    }
+
+    private func insertMessages(_ data: [Any]) {
+        for component in data {
+            let user = DataProvider.shared.meUser
+            if let str = component as? String {
+                let message = MockMessage(text: str, user: user, messageId: UUID().uuidString, date: Date())
+                insertMessage(message)
+            }
+//            else if let img = component as? UIImage {
+//                let message = MockMessage(image: img, user: user, messageId: UUID().uuidString, date: Date())
+//                insertMessage(message)
+//            }
+        }
+    }
+}
+
+extension ChatViewController: MessageCellDelegate {
+    
+    func didTapMessage(in cell: MessageCollectionViewCell) {
+        guard let indexPath = messagesCollectionView.indexPath(for: cell) else { return }
+        guard let messagesDataSource = messagesCollectionView.messagesDataSource else { return }
+        let message = messagesDataSource.messageForItem(at: indexPath, in: messagesCollectionView)
+        print("did tap on: \(message.kind)")
+    }
+    
+}
