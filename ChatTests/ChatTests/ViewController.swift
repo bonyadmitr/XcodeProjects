744336876a79bb637994d7983038b183d22defd8
Yaroslav Bondar <bonyadmitr@gmail.com>
2020-05-20 09:29:19 +0300

    added MSGChatController

--- ChatTests/ChatTests/ViewController.swift
+++ ChatTests/ChatTests/ViewController.swift
@@ -2,6 +2,130 @@ import UIKit
 import MessageKit
 import InputBarAccessoryView
 
+import MessengerKit
+
+struct User2: MSGUser {
+    var displayName: String
+    var avatar: UIImage?
+    var avatarUrl: URL?
+    var isSender: Bool
+}
+
+
+class MSGChatController: MSGMessengerViewController {
+
+    
+    // Users in the chat
+    
+    let steve = User2(displayName: "Steve", avatar: nil, avatarUrl: nil, isSender: true)
+    
+    let tim = User2(displayName: "Tim", avatar: nil, avatarUrl: nil, isSender: false)
+    
+    // Messages
+    
+    lazy var messages: [[MSGMessage]] = {
+        return [
+            [
+                MSGMessage(id: 1, body: .emoji("🐙💦🔫"), user: tim, sentAt: Date()),
+            ],
+            [
+                MSGMessage(id: 2, body: .text("Yeah sure, gimme 5"), user: steve, sentAt: Date()),
+                MSGMessage(id: 3, body: .text("Okay ready when you are"), user: steve, sentAt: Date())
+            ]
+        ]
+    }()
+    
+    // Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        dataSource = self
+    }
+
+    override func inputViewPrimaryActionTriggered(inputView: MSGInputView) {
+        let message = MSGMessage(id: 10, body: .text(inputView.message), user: steve, sentAt: Date())
+        insert(message)
+    }
+    
+    override func insert(_ message: MSGMessage) {
+        
+        collectionView.performBatchUpdates({
+            if let lastSection = self.messages.last, let lastMessage = lastSection.last, lastMessage.user.displayName == message.user.displayName {
+                self.messages[self.messages.count - 1].append(message)
+                
+                let sectionIndex = self.messages.count - 1
+                let itemIndex = self.messages[sectionIndex].count - 1
+                self.collectionView.insertItems(at: [IndexPath(item: itemIndex, section: sectionIndex)])
+                
+            } else {
+                self.messages.append([message])
+                let sectionIndex = self.messages.count - 1
+                self.collectionView.insertSections([sectionIndex])
+            }
+        }, completion: { (_) in
+            self.collectionView.scrollToBottom(animated: true)
+            self.collectionView.layoutTypingLabelIfNeeded()
+        })
+        
+    }
+    
+    override func insert(_ messages: [MSGMessage], callback: (() -> Void)? = nil) {
+        
+        collectionView.performBatchUpdates({
+            for message in messages {
+                if let lastSection = self.messages.last, let lastMessage = lastSection.last, lastMessage.user.displayName == message.user.displayName {
+                    self.messages[self.messages.count - 1].append(message)
+                    
+                    let sectionIndex = self.messages.count - 1
+                    let itemIndex = self.messages[sectionIndex].count - 1
+                    self.collectionView.insertItems(at: [IndexPath(item: itemIndex, section: sectionIndex)])
+                    
+                } else {
+                    self.messages.append([message])
+                    let sectionIndex = self.messages.count - 1
+                    self.collectionView.insertSections([sectionIndex])
+                }
+            }
+        }, completion: { (_) in
+            self.collectionView.scrollToBottom(animated: false)
+            self.collectionView.layoutTypingLabelIfNeeded()
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+                callback?()
+            }
+        })
+        
+    }
+    
+}
+
+// MARK: - MSGDataSource
+
+extension MSGChatController: MSGDataSource {
+    
+    func numberOfSections() -> Int {
+        return messages.count
+    }
+    
+    func numberOfMessages(in section: Int) -> Int {
+        return messages[section].count
+    }
+    
+    func message(for indexPath: IndexPath) -> MSGMessage {
+        return messages[indexPath.section][indexPath.item]
+    }
+    
+    func footerTitle(for section: Int) -> String? {
+        return "Just now"
+    }
+    
+    func headerTitle(for section: Int) -> String? {
+        return messages[section].first?.user.displayName
+    }
+
+}
+
+
+
 // TODO: base on ChatViewController
 class ViewController: UIViewController {
 
