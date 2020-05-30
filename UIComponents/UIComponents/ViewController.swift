9adc418c3f18ecd38d7dac31b75b9435228826dc
Yaroslav Bondar <bonyadmitr@gmail.com>
2020-05-31 01:13:30 +0300

    added func roundCorners

--- UIComponents/UIComponents/ViewController.swift
+++ UIComponents/UIComponents/ViewController.swift
@@ -59,4 +59,29 @@ extension UIView {
         layer.addSublayer(borderLayer)
     }
 
+    /// source https://gist.github.com/shaildyp/6cd1de39498ff7b3c22fa758f005f7cd
+    func roundCorners(_ corners: CACornerMask, radius: CGFloat) {
+        if #available(iOS 11, *) {
+            self.layer.cornerRadius = radius
+            self.layer.maskedCorners = corners
+        } else {
+            var cornerMask = UIRectCorner()
+            if(corners.contains(.layerMinXMinYCorner)){
+                cornerMask.insert(.topLeft)
+            }
+            if(corners.contains(.layerMaxXMinYCorner)){
+                cornerMask.insert(.topRight)
+            }
+            if(corners.contains(.layerMinXMaxYCorner)){
+                cornerMask.insert(.bottomLeft)
+            }
+            if(corners.contains(.layerMaxXMaxYCorner)){
+                cornerMask.insert(.bottomRight)
+            }
+            let path = UIBezierPath(roundedRect: self.bounds, byRoundingCorners: cornerMask, cornerRadii: CGSize(width: radius, height: radius))
+            let mask = CAShapeLayer()
+            mask.path = path.cgPath
+            self.layer.mask = mask
+        }
+    }
 }
