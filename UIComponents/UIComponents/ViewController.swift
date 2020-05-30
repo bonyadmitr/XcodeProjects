68432e92080a01953ef675beb39cc4347ad4d97c
Yaroslav Bondar <bonyadmitr@gmail.com>
2020-05-31 01:12:52 +0300

    added extension UIView

--- UIComponents/UIComponents/ViewController.swift
+++ UIComponents/UIComponents/ViewController.swift
@@ -27,3 +27,36 @@ class RoundedProgressView: UIProgressView {
         layer.masksToBounds = true
     }
 }
+
+extension UIView {
+    
+    @discardableResult func rounded() -> CAShapeLayer {
+        return round(corners: .allCorners, radius: bounds.midY)
+    }
+    
+    func round(corners: UIRectCorner, radius: CGFloat, borderColor: UIColor, borderWidth: CGFloat) {
+        let mask = round(corners: corners, radius: radius)
+        addBorder(mask: mask, borderColor: borderColor, borderWidth: borderWidth)
+    }
+
+    /// source https://stackoverflow.com/a/35621736/5893286
+    @discardableResult func round(corners: UIRectCorner, radius: CGFloat) -> CAShapeLayer {
+        let path = UIBezierPath(roundedRect: bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        let mask = CAShapeLayer()
+        mask.path = path.cgPath
+        self.layer.mask = mask
+        return mask
+    }
+
+    func addBorder(mask: CAShapeLayer?, borderColor: UIColor, borderWidth: CGFloat) {
+        //layer.sublayers
+        let borderLayer = CAShapeLayer()
+        borderLayer.path = mask?.path
+        borderLayer.fillColor = UIColor.clear.cgColor
+        borderLayer.strokeColor = borderColor.cgColor
+        borderLayer.lineWidth = borderWidth// * UIScreen.main.scale
+        borderLayer.frame = bounds
+        layer.addSublayer(borderLayer)
+    }
+
+}
