fd3fd031263e61f1e3b34edfff474f9237072183
Yaroslav Bondar <bonyadmitr@gmail.com>
2020-05-30 11:01:42 +0300

    created NavigationBasic project

---
+++ NavigationBasic/NavigationBasic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NavigationBasic
+//
+//  Created by Bondar Yaroslav on 5/29/20.
+//  Copyright © 2020 Bondar Yaroslav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
