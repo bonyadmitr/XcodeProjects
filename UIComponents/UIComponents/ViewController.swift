4fe02b3c0f48ea6193d98a35113a2d9dfd2a9e84
Yaroslav Bondar <bonyadmitr@gmail.com>
2020-05-31 01:09:37 +0300

    created project UIComponents

---
+++ UIComponents/UIComponents/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIComponents
+//
+//  Created by Bondar Yaroslav on 5/31/20.
+//  Copyright © 2020 Bondar Yaroslav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
