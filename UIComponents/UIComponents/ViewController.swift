ca2182aa08f48f2924e1fec9744af61ed2f6e466
Yaroslav Bondar <bonyadmitr@gmail.com>
2020-06-02 08:30:34 +0300

    added GhostButton2

--- UIComponents/UIComponents/ViewController.swift
+++ UIComponents/UIComponents/ViewController.swift
@@ -710,3 +710,65 @@ class GhostButton: MultiLineButton, Rounded {
     }
     
 }
+
+/// set normalBackgroundColor != UIColor.clear
+final class GhostButton2: HighlightButton, Rounded {
+    
+    override var tintColor: UIColor! {
+        didSet {
+            highlightedBackgroundColor = tintColor
+            layer.borderColor = tintColor.cgColor
+            setTitleColor(tintColor, for: .normal)
+        }
+    }
+    
+    override var normalBackgroundColor: UIColor {
+        didSet {
+            //let fixedColor = (normalBackgroundColor == UIColor.clear) ? Colors.background : normalBackgroundColor
+            setTitleColor(normalBackgroundColor, for: .highlighted)
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+    
+    //override func didMoveToSuperview() {
+    //    super.didMoveToSuperview()
+    //
+    //    /// not working with UIStackView. it's backgroundColor == nil
+    //    let superviewBackgroundColor = superview?.backgroundColor ?? .clear
+    //    normalBackgroundColor = superviewBackgroundColor
+    //}
+    
+    private func setup() -> Void {
+        heightAnchor.constraint(greaterThanOrEqualToConstant: 44).isActive = true
+        tintColor = Colors.text
+        
+        normalBackgroundColor = Colors.background
+        
+        setDynamicFont(Fonts.button)
+        
+        layer.borderWidth = 2
+        
+        initSetupRounded()
+    }
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        
+        /// not working COLOR.resolvedColor(with: traitCollection).cgColor https://stackoverflow.com/a/57177411/5893286
+        /// cgColor update https://stackoverflow.com/a/58312205/5893286
+        if #available(iOS 13.0, *), traitCollection.hasDifferentColorAppearance(comparedTo: previousTraitCollection) {
+            layer.borderColor = tintColor.cgColor
+        }
+
+    }
+    
+}
