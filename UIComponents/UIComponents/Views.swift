e2c77313017696f5bf8fc5088f58e9fc324ad5db
Yaroslav Bondar <bonyadmitr@gmail.com>
2020-06-06 00:31:44 +0300

    added InsetsButton

--- UIComponents/UIComponents/Views.swift
+++ UIComponents/UIComponents/Views.swift
@@ -103,3 +103,59 @@ class InsetsLabel: UILabel {
         return size
     }
 }
+
+class InsetsButton: UIButton {
+    
+    var insets: UIEdgeInsets = .zero {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    
+    @IBInspectable var topBottom: CGSize = .zero {
+        didSet {
+            insets.bottom = topBottom.width
+            insets.top = topBottom.height
+            setNeedsDisplay()
+        }
+    }
+    
+    @IBInspectable var leftRight: CGSize = .zero {
+        didSet {
+            insets.left = leftRight.width
+            insets.right = leftRight.height
+            setNeedsDisplay()
+        }
+    }
+    
+    override func titleRect(forContentRect contentRect: CGRect) -> CGRect {
+        return super.titleRect(forContentRect: contentRect.inset(by: insets))
+    }
+    
+    /// can be used
+    //override func draw(_ rect: CGRect) {
+    //    super.draw(UIEdgeInsetsInsetRect(rect, insets))
+    //}
+    
+    override var intrinsicContentSize: CGSize {
+        var size = super.intrinsicContentSize
+        if size != .zero {
+            size.width += insets.left + insets.right
+            size.height += insets.top + insets.bottom
+        }
+
+        size.width = ceil(size.width)
+        size.height = ceil(size.height)
+
+        return size
+    }
+    
+    
+    /// another
+//    override var intrinsicContentSize: CGSize {
+//        let size = super.intrinsicContentSize
+//
+//        return CGSize(width: size.width + self.titleEdgeInsets.left + self.titleEdgeInsets.right,
+//                      height: size.height + self.titleEdgeInsets.top + self.titleEdgeInsets.bottom)
+//    }
+}
