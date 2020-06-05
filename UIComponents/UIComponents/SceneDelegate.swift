61ddf8c50bdc31601a233667f1bbcd9f2f479e5f
Yaroslav Bondar <bonyadmitr@gmail.com>
2020-06-06 00:18:50 +0300

    added overrideUserInterfaceStyle

--- UIComponents/UIComponents/SceneDelegate.swift
+++ UIComponents/UIComponents/SceneDelegate.swift
@@ -14,6 +14,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        /// apple video https://developer.apple.com/videos/play/wwdc2019/214/
+        window?.overrideUserInterfaceStyle = .unspecified
+        //UIApplication.shared.windows.forEach { window in
+        //    window.overrideUserInterfaceStyle = .unspecified
+        //}
+        
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
