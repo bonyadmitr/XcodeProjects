d6810c339334feb02d2e7c14cbe6bb59d868ca4c
Yaroslav Bondar <bonyadmitr@gmail.com>
2020-06-06 00:26:13 +0300

    added InsetsLabel

--- UIComponents/UIComponents/PlaceholderTextView.swift
+++ UIComponents/UIComponents/PlaceholderTextView.swift
@@ -347,3 +347,40 @@ final class TextInsetsLabel: UILabel {
         return textRect.inset(by: invertedInsets)
     }
 }
+class InsetsLabel: UILabel {
+    
+    var insets: UIEdgeInsets = .zero {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+    
+    @IBInspectable var topBottom: CGSize = .zero {
+        didSet {
+            insets.bottom = topBottom.width
+            insets.top = topBottom.height
+            setNeedsDisplay()
+        }
+    }
+    
+    @IBInspectable var leftRight: CGSize = .zero {
+        didSet {
+            insets.left = leftRight.width
+            insets.right = leftRight.height
+            setNeedsDisplay()
+        }
+    }
+    
+    override func drawText(in rect: CGRect) {
+        super.drawText(in: rect.inset(by: insets))
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        var size = super.intrinsicContentSize
+        if size != .zero {
+            size.width += insets.left + insets.right
+            size.height += insets.top + insets.bottom
+        }
+        return size
+    }
+}
