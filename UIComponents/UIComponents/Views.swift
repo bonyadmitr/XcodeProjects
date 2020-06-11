29eca261f6d9a97850d2a443e1da46bf6d5747eb
Yaroslav Bondar <bonyadmitr@gmail.com>
2020-06-11 17:29:38 +0300

    added SpringLabel

--- UIComponents/UIComponents/Views.swift
+++ UIComponents/UIComponents/Views.swift
@@ -294,3 +294,21 @@ extension UILabel {
     }
 }
 
+
+
+final class SpringLabel: UILabel {
+    
+    var duration = 0.1
+    
+    override var text: String? {
+        didSet {
+            UIView.animate(withDuration: duration) {
+                self.transform = CGAffineTransform(scaleX: 1.2, y: 1.2)
+            }
+            UIView.animate(withDuration: duration, delay: duration, options: [], animations: {
+                self.transform = .identity
+            }, completion: nil)
+        }
+    }
+    
+}
