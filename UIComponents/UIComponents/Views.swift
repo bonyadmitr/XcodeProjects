80e2ad0ab1e843725de8dc4ac6654b02cccd68ac
Yaroslav Bondar <bonyadmitr@gmail.com>
2020-06-18 08:54:20 +0300

    added extension UIBarButtonItem

--- UIComponents/UIComponents/Views.swift
+++ UIComponents/UIComponents/Views.swift
@@ -530,4 +530,43 @@ extension UITextField {
         inputAccessoryView = keyboardToolbar
     }
 }
+
+import UIKit
+
+extension UIBarButtonItem {
+
+    convenience init(title: String,
+                     font: UIFont? = nil,
+                     tintColor: UIColor,
+                     accessibilityLabel: String? = nil,
+                     style: UIBarButtonItem.Style = .plain,
+                     target: Any?,
+                     selector: Selector?) {
+
+        self.init(title: title, style: style, target: target, action: selector)
+        self.tintColor = tintColor
+        self.accessibilityLabel = accessibilityLabel
+
+        if let font = font {
+            /// not working setTitleTextAttributes([.font : font], for: [.normal, .highlighted])
+            setTitleTextAttributes([.font : font], for: .normal)
+            setTitleTextAttributes([.font : font], for: .highlighted)
+            setTitleTextAttributes([.font : font], for: .disabled)
+            setTitleTextAttributes([.font : font], for: .selected)
+        }
+    }
+    
+    /// usage:
+    //let button = isEdit ? readyButton : editButton
+    //button.fixEnabledState()
+    //navigationItem.setRightBarButton(button, animated: true)
+    //
+    /// if you use the properties for the buttons there is a bug only on ios 11 with the replacement of buttons by clicking on them
+    /// https://forums.developer.apple.com/thread/75521
+    func fixEnabledState() {
+        isEnabled = false
+        isEnabled = true
+    }
+    
+}
 }
