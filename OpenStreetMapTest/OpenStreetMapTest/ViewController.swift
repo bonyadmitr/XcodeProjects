fddee14a4b583e866b77dc65cc0e410b5141ced3
Yaroslav Bondar <bonyadmitr@gmail.com>
2020-06-18 08:53:10 +0300

    created OSMMapView

--- OpenStreetMapTest/OpenStreetMapTest/ViewController.swift
+++ OpenStreetMapTest/OpenStreetMapTest/ViewController.swift
@@ -18,3 +18,40 @@ class ViewController: UIViewController {
 
 }
 
+final class OSMMapView: MKMapView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    private func setup() {
+        
+        let template = "https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+        let overlay = MKTileOverlay(urlTemplate: template)
+        overlay.canReplaceMapContent = true
+        addOverlay(overlay, level: .aboveLabels)
+        //let tileRenderer = MKTileOverlayRenderer(tileOverlay: overlay)
+        delegate = self
+        
+    }
+    
+}
+
+extension OSMMapView: MKMapViewDelegate {
+    
+    /// called once
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        if overlay.isKind(of: MKTileOverlay.self) {
+            return MKTileOverlayRenderer(overlay: overlay)
+        } else {
+            assertionFailure()
+            return MKOverlayRenderer()
+        }
+    }
+}
