842e48246f1567087bcc9da2eacd5a1dd98ac86e
Yaroslav Bondar <bonyadmitr@gmail.com>
2020-06-25 09:10:38 +0300

    created LanguageProcessor

--- LanguageProcessor/LanguageProcessor/ViewController.swift
+++ LanguageProcessor/LanguageProcessor/ViewController.swift
@@ -18,3 +18,57 @@ class ViewController: UIViewController {
 
 }
 
+import NaturalLanguage
+
+final class LanguageProcessor {
+    
+    func getVerbs(from stringToRecognize: String, verbs: ([String]) -> Void) {
+        
+        var verbsArray: [String] = []
+        let range = stringToRecognize.startIndex ..< stringToRecognize.endIndex
+        
+        let scheme = NLTagScheme.lexicalClass
+        let tagger = NLTagger(tagSchemes: [scheme])
+        
+        tagger.string = stringToRecognize
+        tagger.enumerateTags(in: range,
+                             unit: .word,
+                             scheme: scheme,
+                             options: [.omitPunctuation, .omitWhitespace, .joinNames]) { tag, range in
+                                
+                                if let tag = tag {
+                                    print("Word \"\(stringToRecognize[range])\" - \(tag.rawValue)")
+                                }
+                                
+                                
+                                if tag?.rawValue == "Verb" {
+                                    verbsArray.append(String(stringToRecognize[range]))
+                                }
+                                
+                                return true
+        }
+        
+        verbs(verbsArray)
+    }
+
+    func getSentimentScore(from stringToRecognize: String, sentimentScore: (Double) -> Void) {
+        
+        let range = stringToRecognize.startIndex ..< stringToRecognize.endIndex
+        
+        let scheme = NLTagScheme.sentimentScore
+        let tagger = NLTagger(tagSchemes: [scheme])
+        
+        tagger.string = stringToRecognize
+        tagger.enumerateTags(in: range,
+                             unit: .paragraph,
+                             scheme: scheme,
+                             options: [.omitWhitespace]) { tag, range in
+                                
+                                let score = Double(tag?.rawValue ?? "") ?? 0
+                                sentimentScore(score)
+                                return true
+        }
+        
+    }
+
+}
