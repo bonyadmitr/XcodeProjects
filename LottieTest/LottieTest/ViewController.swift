7d9d7c5d7984962dea8e04ec25848a521051ce06
Yaroslav Bondar <bonyadmitr@gmail.com>
2020-06-30 13:48:01 +0300

    created SplashController

--- LottieTest/LottieTest/ViewController.swift
+++ LottieTest/LottieTest/ViewController.swift
@@ -6,6 +6,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let animationView = UILabel()
+        animationView.text = "Main screen"
+        animationView.frame = view.bounds
+        animationView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        view.addSubview(animationView)
+        
+    }
+
+
+}
+
+final class SplashController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
         print("- viewDidLoad")
         
         /// doc https://airbnb.io/lottie/#/ios
@@ -20,18 +36,35 @@ class ViewController: UIViewController {
         animationView.loopMode = .loop
         animationView.backgroundBehavior = .pauseAndRestore
         
+        animationView.frame = view.bounds
+        animationView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        view.addSubview(animationView)
+        
         //starAnimationView.play()
         animationView.play { finished in
             /// not called for 'loopMode = .loop' and 'backgroundBehavior = .pauseAndRestore'
             print("- finished: \(finished)")
         }
         
-        animationView.frame = view.bounds
-        animationView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
-        view.addSubview(animationView)
-        
+        openMain()
     }
-
-
+    
+    /// working in background
+    private func openMain() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            guard let window = UIApplication.shared.keyWindow else {
+                assertionFailure()
+                return
+            }
+            window.rootViewController = ViewController()
+            self.animateReload(for: window)
+        }
+    }
+    
+    private func animateReload(for window: UIWindow) {
+        /// you can set any animations https://github.com/malcommac/UIWindowTransitions
+        UIView.transition(with: window, duration: 0.5, options: .transitionFlipFromLeft, animations: {
+        }, completion: nil)
+    }
+    
 }
-
