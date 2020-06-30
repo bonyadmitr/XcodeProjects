a7278eeb4d1fe765e4c115122b7355ffff0efa7e
Yaroslav Bondar <bonyadmitr@gmail.com>
2020-06-30 14:03:24 +0300

    created PasteboardHandler project

---
+++ PasteboardHandler/PasteboardHandler/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PasteboardHandler
+//
+//  Created by Bondar Yaroslav on 6/30/20.
+//  Copyright © 2020 Bondar Yaroslav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
