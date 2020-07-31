750b8d7b6341dec55d6320810f75ec6aeeb9e7ea
Yaroslav Bondar <bonyadmitr@gmail.com>
2020-07-31 07:07:03 +0300

    created ViewController2

--- CollapseExpandSectionTableView/CollapseExpandSectionTableView/ViewController.swift
+++ CollapseExpandSectionTableView/CollapseExpandSectionTableView/ViewController.swift
@@ -135,3 +135,82 @@ extension ViewController: UITableViewDelegate {
     }
     
 }
+
+
+class ViewController2: UIViewController {
+    
+    private let sectionsDataSource = SectionsDataSource()
+    
+    private lazy var tableView: UITableView = {
+        let newValue = UITableView()
+        newValue.dataSource = self
+        newValue.delegate = self
+        newValue.frame = self.view.bounds
+        newValue.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        newValue.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        newValue.register(UINib(nibName: "TitleHeaderView", bundle: nil), forHeaderFooterViewReuseIdentifier: "TitleHeaderView")
+        //newValue.rowHeight = 44
+        newValue.sectionHeaderHeight = 60
+        newValue.sectionFooterHeight = 2 /// separator
+        return newValue
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(tableView)
+    }
+    
+    private func hideSection(_ section: Int) {
+        sectionsDataSource.sections[section].isExpanded.toggle()
+        let indexPathsForSection = (0..<sectionsDataSource.sections[section].rows.count).map { IndexPath(row: $0, section: section) }
+        
+        tableView.beginUpdates()
+        tableView.reloadRows(at: indexPathsForSection, with: .automatic)
+        tableView.endUpdates()
+    }
+}
+
+extension ViewController2: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sectionsDataSource.sections.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sectionsDataSource.sections[section].rows.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.backgroundColor = .white
+        cell.textLabel?.text = sectionsDataSource.sections[indexPath.section].rows[indexPath.row]
+        return cell
+    }
+    
+}
+
+extension ViewController2: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return sectionsDataSource.sections[indexPath.section].isExpanded ? 44 : 0
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        guard let view = tableView.dequeueReusableHeaderFooterView(withIdentifier: "TitleHeaderView") as? TitleHeaderView else {
+            assertionFailure()
+            return nil
+        }
+        view.setup(title: "Section: \(section)", section: section) { [weak self] section in
+            self?.hideSection(section)
+        }
+        return view
+    }
+    
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        let view = UIView()
+        view.backgroundColor = .white
+        return view
+    }
+    
+}
