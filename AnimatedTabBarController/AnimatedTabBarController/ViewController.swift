4af88597ecb596e3a4dd750f305c7a4caa77eafa
Yaroslav Bondar <bonyadmitr@gmail.com>
2020-08-09 14:55:00 +0300

    added changeTab

--- AnimatedTabBarController/AnimatedTabBarController/ViewController.swift
+++ AnimatedTabBarController/AnimatedTabBarController/ViewController.swift
@@ -12,8 +12,35 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let button1 = UIButton(type: .system)
+        button1.setTitle("Tab", for: .normal)
+        button1.addTarget(self, action: #selector(changeTab), for: .touchUpInside)
+        
+        let stackView = UIStackView(arrangedSubviews: [button1])
+        stackView.axis = .vertical
+        view.addSubview(stackView)
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            //stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
+        
     }
-
+    
+    @objc private func changeTab() {
+        guard let tabBarController = tabBarController else {
+            return
+        }
+        
+        if tabBarController.selectedIndex + 1 == tabBarController.viewControllers?.count {
+            tabBarController.selectedIndex = 0
+        } else {
+            tabBarController.selectedIndex += 1
+        }
+    }
 
 }
