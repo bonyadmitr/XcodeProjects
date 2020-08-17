52ed4a61408d0c25ccc22981716844e5341169b5
Yaroslav Bondar <bonyadmitr@gmail.com>
2020-08-17 09:22:49 +0300

    added MainTabBar

--- RaisedTabBar/RaisedTabBar/ViewController.swift
+++ RaisedTabBar/RaisedTabBar/ViewController.swift
@@ -18,3 +18,44 @@ class ViewController: UIViewController {
 
 }
 
+import UIKit
+
+class MainTabBar: UITabBar {
+    
+    private var middleButton = UIButton()
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupMiddleButton()
+    }
+    
+    override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+        if self.isHidden {
+            return super.hitTest(point, with: event)
+        }
+        
+        let from = point
+        let to = middleButton.center
+        
+        return sqrt((from.x - to.x) * (from.x - to.x) + (from.y - to.y) * (from.y - to.y)) <= 39 ? middleButton : super.hitTest(point, with: event)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        middleButton.center = CGPoint(x: UIScreen.main.bounds.width / 2, y: 0)
+    }
+    
+    func setupMiddleButton() {
+        middleButton.frame.size = CGSize(width: 70, height: 70)
+        middleButton.backgroundColor = .blue
+        middleButton.layer.cornerRadius = 35
+        middleButton.layer.masksToBounds = true
+        middleButton.center = CGPoint(x: UIScreen.main.bounds.width / 2, y: 0)
+        middleButton.addTarget(self, action: #selector(test), for: .touchUpInside)
+        addSubview(middleButton)
+    }
+    
+    @objc func test() {
+        print("my name is jeff")
+    }
+}
