725eee7a57e7ca603a805f963c4705be10ea13f2
Yaroslav Bondar <bonyadmitr@gmail.com>
2020-08-20 10:35:10 +0300

    created RaisedTabBarController

--- RaisedTabBar/RaisedTabBar/ViewController.swift
+++ RaisedTabBar/RaisedTabBar/ViewController.swift
@@ -33,15 +33,99 @@ final class RaisedTabBarController: UITabBarController {
         super.init(coder: coder)
         
         tabBar.items = super.tabBar.items
+class RaisedTabBarController: UITabBarController {
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
     }
     
     private func setup() {
-        raisedTabBar.onRaisedButtonHandler = {
-            print("hi")
+        setupMiddleButton()
+        updateItemsOffset()
+        
+        onRaisedButtonHandler = { [weak self] in
+            if let vc = self?.selectedViewController as? RaisedTabBarHandler {
+                vc.onRaisedButton()
+            } else {
+                print("default onRaisedButtonHandler")
+            }
+        }
+        
+    }
+    
+    var onRaisedButtonHandler: () -> Void = {}
+    private let raisedButton = UIButton()
+    
+    override var viewControllers: [UIViewController]? {
+        didSet {
+            updateItemsOffset()
         }
     }
     
-    override var tabBar: UITabBar { raisedTabBar }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        raisedButton.center = CGPoint(x: tabBar.center.x, y: 0)
+        tabBar.bringSubviewToFront(raisedButton)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        if tabBar.isHidden {
+            super.touchesBegan(touches, with: event)
+            return
+        }
+        
+        guard let touchLocation = touches.first?.location(in: view) else {
+            assertionFailure("touch will be always inside self")
+            return
+        }
+        
+        let onButton = view.convert(touchLocation, to: raisedButton)
+        
+        //if raisedButton.point(inside: onButton, with: event) {
+        if raisedButton.bounds.contains(onButton) {
+            onRaisedButtonHandler()
+        } else {
+            super.touchesBegan(touches, with: event)
+        }
+    }
+    
+    private func setupMiddleButton() {
+        raisedButton.frame.size = CGSize(width: 70, height: 70)
+        raisedButton.backgroundColor = .blue
+        raisedButton.layer.cornerRadius = raisedButton.bounds.midX
+        raisedButton.layer.masksToBounds = true
+        raisedButton.center = CGPoint(x: tabBar.center.x, y: 0)
+        raisedButton.addTarget(self, action: #selector(onRaisedButton), for: .touchUpInside)
+        tabBar.addSubview(raisedButton)
+    }
+    
+    @objc private func onRaisedButton() {
+        onRaisedButtonHandler()
+    }
+    
+    private func updateItemsOffset() {
+        guard let tabItems = tabBar.items else {
+            return
+        }
+        if tabItems.count == 2 {
+            tabItems[0].titlePositionAdjustment = UIOffset(horizontal: -15, vertical: 0)
+            tabItems[1].titlePositionAdjustment = UIOffset(horizontal: 15, vertical: 0)
+            // TODO: check for 4
+        } else if tabItems.count == 4 {
+            tabItems[0].titlePositionAdjustment = UIOffset(horizontal: -15, vertical: 0)
+            tabItems[1].titlePositionAdjustment = UIOffset(horizontal: -7, vertical: 0)
+            tabItems[3].titlePositionAdjustment = UIOffset(horizontal: 7, vertical: 0)
+            tabItems[4].titlePositionAdjustment = UIOffset(horizontal: 15, vertical: 0)
+        }
+    }
     
 }
 
