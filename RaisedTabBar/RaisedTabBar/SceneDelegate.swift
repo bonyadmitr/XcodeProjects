c44baf969936fb3fc30d18a6cc15a84bad7df771
Yaroslav Bondar <bonyadmitr@gmail.com>
2020-08-22 22:52:22 +0300

    added temp test code

--- RaisedTabBar/RaisedTabBar/SceneDelegate.swift
+++ RaisedTabBar/RaisedTabBar/SceneDelegate.swift
@@ -18,6 +18,19 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        
+//        let window = UIWindow(windowScene: scene as! UIWindowScene)
+//        let tabVC = RaisedTabBarController()
+//        
+//        let vc1 = UIViewController()
+//        vc1.title = "vc 1"
+//        
+//        let vc2 = UIViewController()
+//        vc2.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 0)
+//        
+//        tabVC.viewControllers = [vc1, vc2]
+//        window.rootViewController = tabVC
+//        self.window = window
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
