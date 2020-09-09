52a4b9dfa49549933bb2821d62632c9e3e62127a
Yaroslav Bondar <bonyadmitr@gmail.com>
2020-09-09 21:34:03 +0300

    created project NotificationPush

---
+++ NotificationPush/NotificationPush/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NotificationPush
+//
+//  Created by Yaroslav Bondr on 09.09.2020.
+//  Copyright © 2020 Yaroslav Bondr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
