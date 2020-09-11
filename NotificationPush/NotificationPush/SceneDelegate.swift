aaff181e9dba89511a13508c58418203bfa4175a
Yaroslav Bondar <bonyadmitr@gmail.com>
2020-09-11 09:23:39 +0300

    UIApplication.shared.applicationIconBadgeNumber

--- NotificationPush/NotificationPush/SceneDelegate.swift
+++ NotificationPush/NotificationPush/SceneDelegate.swift
@@ -30,6 +30,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+        UIApplication.shared.applicationIconBadgeNumber = 0
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
