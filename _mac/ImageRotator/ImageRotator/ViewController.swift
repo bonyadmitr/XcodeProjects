7e6b3109fed553325bccec98c7d6284c46785309
Yaroslav Bondar <bonyadmitr@gmail.com>
2020-10-09 11:56:35 +0300

    cleared code

--- _mac/ImageRotator/ImageRotator/ViewController.swift
+++ _mac/ImageRotator/ImageRotator/ViewController.swift
@@ -1,10 +1,3 @@
-//
-//  ViewController.swift
-//  ImageRotator
-//
-//  Created by Yaroslav Bondr on 08.10.2020.
-//
-
 import Cocoa
 
 class ViewController: NSViewController {
@@ -12,15 +5,8 @@ class ViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
-    }
-
-
+    
 }
 
