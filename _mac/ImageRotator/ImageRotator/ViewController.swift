7378c7454e1a359d6f36e6c36e8fd5107af8e9e1
Yaroslav Bondar <bonyadmitr@gmail.com>
2020-10-11 21:31:57 +0300

    added temp code

--- _mac/ImageRotator/ImageRotator/ViewController.swift
+++ _mac/ImageRotator/ImageRotator/ViewController.swift
@@ -4,6 +4,16 @@ class ViewController: NSViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+//        let url = URL(fileURLWithPath: "/Users/yaroslav/Downloads/Металл КП-2/IMG_20200423_173459.jpg")
+//        url.image()
+//        url.qqq()
+//        print(url.sizeOfImage)
+//        print()
+//
+        
+        
+//        exit(0)
+    }
 
         
     }
