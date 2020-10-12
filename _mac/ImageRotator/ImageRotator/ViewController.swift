c37a7ba4b25672d72932fedf364e0a0f4b567a06
Yaroslav Bondar <bonyadmitr@gmail.com>
2020-10-12 09:11:30 +0300

    addDropView

--- _mac/ImageRotator/ImageRotator/ViewController.swift
+++ _mac/ImageRotator/ImageRotator/ViewController.swift
@@ -4,6 +4,9 @@ class ViewController: NSViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        addDropView()
+        
 //        let url = URL(fileURLWithPath: "/Users/yaroslav/Downloads/Металл КП-2/IMG_20200423_173459.jpg")
 //        url.image()
 //        url.qqq()
@@ -14,6 +17,25 @@ class ViewController: NSViewController {
         
 //        exit(0)
     }
+    
+    /// call the last to add view on the top
+    private func addDropView() {
+        let dropView = DropView(frame: view.bounds)
+        dropView.setup(isSubview: true, fileTypes: NSImage.imageTypes) { filePaths, images in
+            
+            autoreleasepool {
+                filePaths
+                    .map { URL(fileURLWithPath: $0) }
+//                    .forEach { $0.image() }
+                    .forEach { $0.changeMetaDataWithCompress() }
+            }
+        }
+        dropView.autoresizingMask = [.width, .height]
+        view.addSubview(dropView)
+    }
+    
+}
+
 
         
     }
