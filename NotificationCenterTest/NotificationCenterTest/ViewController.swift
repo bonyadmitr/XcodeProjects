172e1d53e745fe4416acfb79495a515e16b2e603
Yaroslav Bondar <bonyadmitr@gmail.com>
2020-11-09 12:10:55 +0300

    TokenRemover

--- NotificationCenterTest/NotificationCenterTest/ViewController.swift
+++ NotificationCenterTest/NotificationCenterTest/ViewController.swift
@@ -17,3 +17,18 @@ class ViewController: UIViewController {
 extension NSNotification.Name {
     static let custom = NSNotification.Name(rawValue: "custom")
 }
+final class TokenRemover {
+    
+    private let token: NSObjectProtocol /// can be `Any`
+    private let notificationCenter: NotificationCenter
+    
+    init(token: NSObjectProtocol, notificationCenter: NotificationCenter) {
+        self.token = token
+        self.notificationCenter = notificationCenter
+    }
+    
+    deinit {
+        notificationCenter.removeObserver(token)
+    }
+    
+}
