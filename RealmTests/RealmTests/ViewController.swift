161f3339d8f23b295c41ae836f6afcfa76c33673
Yaroslav Bondar <bonyadmitr@gmail.com>
2020-11-20 15:13:28 +0300

    added project RealmTests

---
+++ RealmTests/RealmTests/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealmTests
+//
+//  Created by Yaroslav Bondr on 20.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
