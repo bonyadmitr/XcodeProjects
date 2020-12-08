f00384d1ff0066ee38524027d2d682b00ab7f0a2
Yaroslav Bondar <bonyadmitr@gmail.com>
2020-12-09 01:26:30 +0300

    added KeyboardMultiWindow project

---
+++ KeyboardMultiWindow/KeyboardMultiWindow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KeyboardMultiWindow
+//
+//  Created by Yaroslav Bondr on 08.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
