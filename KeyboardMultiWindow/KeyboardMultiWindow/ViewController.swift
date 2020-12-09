ee8da7392e9b176b11824a76f69d547957f1ea6d
Yaroslav Bondar <bonyadmitr@gmail.com>
2020-12-09 09:10:48 +0300

    added ui

--- KeyboardMultiWindow/KeyboardMultiWindow/ViewController.swift
+++ KeyboardMultiWindow/KeyboardMultiWindow/ViewController.swift
@@ -9,9 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let textField = UITextField()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        textField.borderStyle = .roundedRect
+        
+        let button1 = UIButton(type: .system)
+        button1.addTarget(self, action: #selector(onButtonWindow), for: .touchUpInside)
+        button1.setTitle("window", for: .normal)
+        
+        let button2 = UIButton(type: .system)
+        button2.addTarget(self, action: #selector(onButton), for: .touchUpInside)
+        button2.setTitle("Hide", for: .normal)
     }
 
 
