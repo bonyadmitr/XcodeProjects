b4248689588911ddec417571b9a6b96aa366daf7
Yaroslav Bondar <bonyadmitr@gmail.com>
2021-01-28 14:43:33 +0300

    created EmailClient project

---
+++ EmailClient/EmailClient/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmailClient
+//
+//  Created by Yaroslav Bondr on 28.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
