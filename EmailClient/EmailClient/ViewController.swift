511ca271ac590b45f98059b97a17d2f34f473f63
Yaroslav Bondar <bonyadmitr@gmail.com>
2021-01-30 12:21:10 +0300

    link

--- EmailClient/EmailClient/ViewController.swift
+++ EmailClient/EmailClient/ViewController.swift
@@ -16,6 +16,8 @@ import UIKit
  yandex auth problem "This user does not have access rights to this service" https://prnt.sc/uz30rb and https://searchengines.guru/ru/forum/1037543
  yandex lite client https://mail.yandex.ru/lite/inbox
  
+ MailCore 2 lib
+ http://libmailcore.com/api/objc/index.html
  */
 
 class ViewController: UIViewController {
