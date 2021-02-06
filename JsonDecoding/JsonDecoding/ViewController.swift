ea813794f538c08550d9e526dfb236a26860b2d9
Yaroslav Bondar <bonyadmitr@gmail.com>
2021-02-06 08:48:42 +0300

    extension Mirror

--- JsonDecoding/JsonDecoding/ViewController.swift
+++ JsonDecoding/JsonDecoding/ViewController.swift
@@ -17,3 +17,22 @@ class ViewController: UIViewController {
 
 }
 
+extension Mirror {
+    
+    /// The children of the mirror and its superclasses.
+    var allChildren: [Mirror.Child] {
+        var children = [Mirror.Child]()// = Array(self.children)
+        
+        var superclassMirror = self.superclassMirror
+        
+        while let mirror = superclassMirror {
+            children.append(contentsOf: mirror.children)
+            superclassMirror = mirror.superclassMirror
+        }
+        /// to display parant class properties first
+        children.append(contentsOf: self.children)
+        
+        return children
+    }
+    
+}
