c3608ec85a39c76a7751ab4d831eee07c284e8a3
Yaroslav Bondar <bonyadmitr@gmail.com>
2021-02-09 11:19:22 +0300

    created Debugger project

---
+++ Debugger/Debugger/CallStackParser.swift
@@ -0,0 +1,8 @@
+//
+//  CallStackParser.swift
+//  Debugger
+//
+//  Created by Yaroslav Bondr on 08.02.2021.
+//
+
+import Foundation
---
+++ Debugger/Debugger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Debugger
+//
+//  Created by Yaroslav Bondr on 08.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
