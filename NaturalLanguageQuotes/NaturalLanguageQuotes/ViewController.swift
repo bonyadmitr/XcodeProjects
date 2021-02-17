e57a3c2e45e918816c3b55cf7f5d76bced60b69d
Yaroslav Bondar <bonyadmitr@gmail.com>
2021-02-17 08:09:16 +0300

    links

--- NaturalLanguageQuotes/NaturalLanguageQuotes/ViewController.swift
+++ NaturalLanguageQuotes/NaturalLanguageQuotes/ViewController.swift
@@ -11,6 +11,8 @@
  https://developer.apple.com/documentation/naturallanguage
  https://www.appcoda.com/natural-language-processing-swift/
  https://www.analyticsvidhya.com/blog/2019/12/create-nlp-apps-ios-using-apples-core-ml-3/
+ https://heartbeat.fritz.ai/exploring-word-embeddings-and-text-catalogs-with-apples-natural-language-framework-in-ios-b4d9164f5a3b
+ Big Json https://type.fit/api/quotes
  
  */
 import UIKit
