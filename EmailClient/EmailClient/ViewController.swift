e41ac2e99f04ca30bc4ff7b3a1467208e4392fe1
Yaroslav Bondar <bonyadmitr@gmail.com>
2021-03-25 21:32:42 +0300

    link

--- EmailClient/EmailClient/ViewController.swift
+++ EmailClient/EmailClient/ViewController.swift
@@ -18,7 +18,7 @@ import UIKit
  yandex auth problem "Error: authentication failed: This user does not have access rights to this service" https://prnt.sc/uz30rb and https://searchengines.guru/ru/forum/1037543
  yandex lite client https://mail.yandex.ru/lite/inbox
  
- MailCore 2 lib
+ MailCore2 lib
  inside MailCore https://github.com/dinhvh/libetpan
  classes http://libmailcore.com/api/objc/index.html
  source https://github.com/MailCore/mailcore2
@@ -29,8 +29,7 @@ import UIKit
  andoid lib
  https://eclipse-ee4j.github.io/mail/
  
- 
- https://stackoverflow.com/a/31540230/5893286
+ send basic example https://stackoverflow.com/a/31540230/5893286
  
  В клиентах перемещение реализовано через копирование+удаление
  https://habr.com/ru/company/mailru/blog/151001/
