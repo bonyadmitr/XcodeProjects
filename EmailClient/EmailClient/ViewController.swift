81940c68c8eb1a3c5011e70a7f5b5e5e51b98d63
Yaroslav Bondar <bonyadmitr@gmail.com>
2021-03-26 11:01:46 +0300

    func chunked(into size: Int)

--- EmailClient/EmailClient/ViewController.swift
+++ EmailClient/EmailClient/ViewController.swift
@@ -116,6 +116,15 @@ final class DataSource {
 
 
 
+extension Array {
+    func chunked(into size: Int) -> [[Element]] {
+        return stride(from: 0, to: count, by: size).map {
+            Array(self[$0 ..< Swift.min($0 + size, count)])
+        }
+    }
+}
+
+
 class ViewController: UIViewController {
     
     override func viewDidLoad() {
