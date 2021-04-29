a9aa184dae12266c5380f834928a893fe9429669
Yaroslav Bondar <bonyadmitr@gmail.com>
2021-04-29 18:45:33 +0300

    links

--- Formatters/Formatters/ViewController.swift
+++ Formatters/Formatters/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // https://developer.apple.com/library/archive/documentation/Cocoa/Conceptual/DatesAndTimes/DatesAndTimes.html
+        // Data Formatting https://developer.apple.com/library/archive/documentation/Cocoa/Conceptual/DataFormatting/DataFormatting.html
+        // Date Formatters https://developer.apple.com/library/archive/documentation/Cocoa/Conceptual/DataFormatting/Articles/dfDateFormatting10_4.html
         let locale = Locale.current
         let dateFormat = DateFormatter.dateFormat(fromTemplate: "j", options:0, locale:locale)!
         let is12hoursDeviceFormat = dateFormat.last == "a"//safe: dateFormat.contains("a")
