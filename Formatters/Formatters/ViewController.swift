d47dc9a5d752e3bab7c5f7e9653d6c85ae025ef4
Yaroslav Bondar <bonyadmitr@gmail.com>
2021-04-29 18:45:46 +0300

    links

--- Formatters/Formatters/ViewController.swift
+++ Formatters/Formatters/ViewController.swift
@@ -15,6 +15,10 @@ class ViewController: UIViewController {
         // https://developer.apple.com/library/archive/documentation/Cocoa/Conceptual/DatesAndTimes/DatesAndTimes.html
         // Data Formatting https://developer.apple.com/library/archive/documentation/Cocoa/Conceptual/DataFormatting/DataFormatting.html
         // Date Formatters https://developer.apple.com/library/archive/documentation/Cocoa/Conceptual/DataFormatting/Articles/dfDateFormatting10_4.html
+        // TODO: en_US vs en_US_POSIX
+        // https://developer.apple.com/library/archive/qa/qa1480/_index.html
+        // https://stackoverflow.com/questions/6613110/what-is-the-best-way-to-deal-with-the-nsdateformatter-locale-feechur
+        // https://developer.apple.com/library/archive/documentation/Cocoa/Conceptual/DataFormatting/Articles/dfDateFormatting10_4.html#//apple_ref/doc/uid/TP40002369-SW7
         let locale = Locale.current
         let dateFormat = DateFormatter.dateFormat(fromTemplate: "j", options:0, locale:locale)!
         let is12hoursDeviceFormat = dateFormat.last == "a"//safe: dateFormat.contains("a")
