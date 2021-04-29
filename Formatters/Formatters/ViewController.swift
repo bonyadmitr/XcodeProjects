d792babfe2e94870c314d4c6c96a5d2054ce14e9
Yaroslav Bondar <bonyadmitr@gmail.com>
2021-04-29 18:45:25 +0300

    is12hoursDeviceFormat

--- Formatters/Formatters/ViewController.swift
+++ Formatters/Formatters/ViewController.swift
@@ -12,6 +12,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let locale = Locale.current
+        let dateFormat = DateFormatter.dateFormat(fromTemplate: "j", options:0, locale:locale)!
+        let is12hoursDeviceFormat = dateFormat.last == "a"//safe: dateFormat.contains("a")
+        print("12 hours?", is12hoursDeviceFormat)
     }
 
 
