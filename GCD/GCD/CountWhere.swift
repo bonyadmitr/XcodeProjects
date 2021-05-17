b0bd3bf54dcbc787bf12773c45fe3d9015c62aa9
Yaroslav Bondar <bonyadmitr@gmail.com>
2021-05-17 10:54:12 +0300

    func globalTest()

--- GCD/GCD/CountWhere.swift
+++ GCD/GCD/CountWhere.swift
@@ -68,3 +68,90 @@ func globalTestCountWhere() {
     let count3 = array.countConcurrent(where: { $0 > 50_000 })
     print("finish countConcurrent", count3, -date3.timeIntervalSinceNow)
 }
+
+func globalTest() {
+    
+    /// https://www.hackingwithswift.com/example-code/language/how-to-pass-the-fizz-buzz-test
+    func fizzbuzz(number: Int) -> String {
+        switch (number % 3 == 0, number % 5 == 0) {
+        case (true, false):
+            return "Fizz"
+        case (false, true):
+            return "Buzz"
+        case (true, true):
+            return "FizzBuzz"
+        case (false, false):
+            return String(number)
+        }
+    }
+    
+    
+    let limit = 1_000_000_000
+    print(
+        "time:",
+        Benchmark.one {
+//            DispatchQueue.concurrentPerform(iterations: limit) { i in
+//                _ = fizzbuzz(number: i)
+//            }
+            
+            for i in 1...limit {
+                //print(fizzbuzz(number: i))
+                _ = fizzbuzz(number: i)
+            }
+            
+            /// swift github search https://github.com/search?l=swift&q=FizzBuzz&type=Repositories
+            /// all lang https://rosettacode.org/wiki/FizzBuzz#Swift
+            /// perfermance C https://habr.com/ru/post/540136/
+            
+            /// https://github.com/rsha256/shortest-fizzbuzz/blob/master/Swift.swift
+//            func fizzBuzz(_ numberOfTurns: Int) {
+//                guard numberOfTurns >= 1 else {
+//                    print("Number of turns must be >= 1")
+//                    return
+//                }
+//
+//                for i in 1...numberOfTurns {
+//                    switch (i.isMultiple(of: 3), i.isMultiple(of: 5)) {
+//                    case (false, false):
+//                        print("\(i)")
+//                    case (true, false):
+//                        print("Fizz")
+//                    case (false, true):
+//                        print("Buzz")
+//                    case (true, true):
+//                        print("Fizz Buzz")
+//                    }
+//                }
+//            }
+            
+//
+//                switch (i % 3 == 0, i % 5 == 0) {
+//                case (true, false):
+//                    print("Fizz")
+//                case (false, true):
+//                    print("Buzz")
+//                case (true, true):
+//                    print("FizzBuzz")
+//                case (false, false):
+//                    print(i)
+//                }
+//
+//                /// https://m.habr.com/ru/post/540136/
+////                if (0 == i % 3) {
+////                    if (0 == i % 5) {
+////                        print("FizzBuzz");
+////                    } else {
+////                        print("Fizz");
+////                    }
+////                } else if (0 == i % 5) {
+////                    print("Buzz");
+////                } else {
+////                    print("%d", i);
+////                }
+            
+            
+        }
+    )
+    
+    
+}
