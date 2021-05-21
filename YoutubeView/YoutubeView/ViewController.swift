d201a82f8d6078cee70d07896ceb7e2131fe1982
Yaroslav Bondar <bonyadmitr@gmail.com>
2021-05-21 16:03:14 +0300

    created project YoutubeView

---
+++ YoutubeView/YoutubeView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YoutubeView
+//
+//  Created by Yaroslav Bondar on 21.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
