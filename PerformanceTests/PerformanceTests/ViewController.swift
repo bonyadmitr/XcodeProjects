4e4af2c1ea1cd203890d56c62b26dfee4a3b4d22
Yaroslav Bondar <bonyadmitr@gmail.com>
2021-05-31 02:02:43 +0300

    enum Food

--- PerformanceTests/PerformanceTests/ViewController.swift
+++ PerformanceTests/PerformanceTests/ViewController.swift
@@ -209,3 +209,48 @@ extension String {
     
 }
 
+enum Food {
+    case beef
+    case broccoli
+    case chicken
+    case greenPepper
+    case lettuce
+    case onion
+    case redPepper
+    case spinach
+    case artichoke
+    case cabbage
+    case celery
+    case kale
+    case radish
+    case squash
+    case parsley
+    case yellowPepper
+    
+    func isVegetable() -> Bool {
+        return self == .broccoli ||
+            self == .greenPepper ||
+            self == .lettuce ||
+            self == .onion ||
+            self == .redPepper ||
+            self == .spinach ||
+            self == .artichoke ||
+            self == .cabbage ||
+            self == .celery ||
+            self == .kale ||
+            self == .radish ||
+            self == .squash ||
+            self == .parsley ||
+            self == .yellowPepper
+    }
+    
+    func isVegetableSwitch() -> Bool {
+        switch self {
+        case .broccoli, .greenPepper, .lettuce, .onion, .redPepper, .spinach, .artichoke, .cabbage, .celery, .kale, .radish, .squash, .parsley, .yellowPepper:
+            return true
+        case .beef, .chicken:
+            return false
+        }
+    }
+    
+}
