afe379e6b8fab22201b32894d2c8e96962a833fa
Yaroslav Bondar <bonyadmitr@gmail.com>
2021-06-01 17:22:20 +0300

    filePath

--- PerformanceTests/PerformanceTestsTests/NSStringTests.swift
+++ PerformanceTests/PerformanceTestsTests/NSStringTests.swift
@@ -11,11 +11,16 @@ import XCTest
 
 class NSStringTests: XCTestCase {
     
+    private let filePath = "/Users/yaroslav/XcodeProjects/PerformanceTests/PerformanceTests.xcodeproj"
 
     override func setUpWithError() throws {
         print()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
+//
+//    override func tearDownWithError() throws {
+//        // Put teardown code here. This method is called after the invocation of each test method in the class.
+//    }
     func test_stringConcatenation() {
         measure {
             for _ in 1...1_000_000 {
