cf1a134b4f71b44591293d655f68667dea8a3c81
Yaroslav Bondar <bonyadmitr@gmail.com>
2021-06-03 18:15:14 +0300

    extension Object: DetachableObject {
        func detached()

--- RealmTests/RealmTests/ViewController.swift
+++ RealmTests/RealmTests/ViewController.swift
@@ -30,6 +30,24 @@ class IdObject: Object {
         return #keyPath(id)
     }
 }
+extension Object: DetachableObject {
+    
+    func detached() -> Self {
+        let detached = type(of: self).init()
+        for property in objectSchema.properties {
+            guard let value = value(forKey: property.name) else {
+                continue
+            }
+            if let detachable = value as? DetachableObject {
+                detached.setValue(detachable.detached(), forKey: property.name)
+            } else { // Then it is a primitive
+                detached.setValue(value, forKey: property.name)
+            }
+        }
+        return detached
+    }
+}
+
 extension List: DetachableObject {
     func detached() -> List<Element> {
         let result = List<Element>()
