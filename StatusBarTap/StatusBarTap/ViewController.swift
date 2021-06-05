77ea30f4c8ddced7b7c4a5c86ce3db70d9570fdf
Yaroslav Bondar <bonyadmitr@gmail.com>
2021-06-05 09:29:58 +0300

    class SwizzlingManager

--- StatusBarTap/StatusBarTap/ViewController.swift
+++ StatusBarTap/StatusBarTap/ViewController.swift
@@ -33,3 +33,50 @@ extension ViewController: UIScrollViewDelegate, UITableViewDelegate {
         return true
     }
 }
+final class SwizzlingManager {
+    
+    static func swizzleMethod(_ class_: AnyClass, from origin: String, to override: String, isClassMethod: Bool) {
+        swizzleMethod(class_, from: Selector(origin), to: Selector(override), isClassMethod: isClassMethod)
+    }
+    
+    /// https://github.com/inamiy/Swizzle/blob/master/Swizzle/Swizzle.swift
+    static func swizzleMethod(_ class_: AnyClass, from origin: Selector, to override: Selector, isClassMethod: Bool) {
+        
+        let aClass: AnyClass = isClassMethod ? object_getClass(class_) ?? class_ : class_
+        
+        guard
+            let method1 = class_getInstanceMethod(aClass, origin),
+            let method2 = class_getInstanceMethod(aClass, override)
+        else {
+            assertionFailure("object_getClass nil: \(object_getClass(class_) == nil)")
+            return
+        }
+        
+        if class_addMethod(aClass, origin, method_getImplementation(method2), method_getTypeEncoding(method2)) {
+            class_replaceMethod(aClass, override, method_getImplementation(method1), method_getTypeEncoding(method1))
+        } else {
+            method_exchangeImplementations(method1, method2)
+        }
+    }
+    
+    /// Instance-method swizzling.
+    static func swizzleInstanceMethod(_ class_: AnyClass, from sel1: Selector, to sel2: Selector) {
+        swizzleMethod(class_, from: sel1, to: sel2, isClassMethod: false)
+    }
+    
+    /// Instance-method swizzling for unsafe raw-string.
+    /// - Note: This is useful for non-`#selector`able methods e.g. `dealloc`, private ObjC methods.
+    static func swizzleInstanceMethodString(_ class_: AnyClass, from sel1: String, to sel2: String) {
+        swizzleInstanceMethod(class_, from: Selector(sel1), to: Selector(sel2))
+    }
+    
+    /// Class-method swizzling.
+    static func swizzleClassMethod(_ class_: AnyClass, from sel1: Selector, to sel2: Selector) {
+        swizzleMethod(class_, from: sel1, to: sel2, isClassMethod: true)
+    }
+    
+    /// Class-method swizzling for unsafe raw-string.
+    static func swizzleClassMethodString(_ class_: AnyClass, from sel1: String, to sel2: String) {
+        swizzleClassMethod(class_, from: Selector(sel1), to: Selector(sel2))
+    }
+}
