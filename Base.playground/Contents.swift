efe3c16433426b1c57de777cb7377b4796f01974
Yaroslav Bondar <bonyadmitr@gmail.com>
2021-11-08 09:11:55 +0300

    temp code

--- Base.playground/Contents.swift
+++ Base.playground/Contents.swift
@@ -1,2 +1,13 @@
 import Foundation
 
+
+// 🍑🍓🍒🥑🍌🍎🍊🍍🍋🥥🍇🍉🥝🍏🍅🍈🍐🫐🥭
+// for _ in 1...10 { print("🍏") }
+
+/// playground error in iOS
+/// solution: open right panael - playground settings - macOS
+
+/// we essentially tell the playground to keep running until we tell it to stop — which we do using a call to .finishExecution()
+//import PlaygroundSupport
+//PlaygroundPage.current.needsIndefiniteExecution = true
+//PlaygroundPage.current.finishExecution()
