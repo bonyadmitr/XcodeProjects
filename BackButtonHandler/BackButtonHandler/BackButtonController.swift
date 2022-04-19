bd12486cf479480b6fcdafe20e4139daf894cb15
Yaroslav Bondar <bonyadmitr@gmail.com>
2022-04-19 10:43:16 +0300

    logic

--- BackButtonHandler/BackButtonHandler/BackButtonController.swift
+++ BackButtonHandler/BackButtonHandler/BackButtonController.swift
@@ -13,16 +13,22 @@ class BackButtonController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        navigationItem.leftBarButtonItem = BackButtonItem(action: {
-            
+        
+//        let backBTN = UIBarButtonItem(image: UIImage(named: "Icon-Back"),
+//                                      style: .plain,
+//                                      target: navigationController,
+//                                      action: #selector(UINavigationController.popViewController(animated:)))
+//        navigationItem.leftBarButtonItem = backBTN
+        
+        
+        
+        navigationItem.leftBarButtonItem = BackButtonItem(title: previousNavigationVCTitle(), action: { [weak self] in
             let vc = UIAlertController(title: "Exit without save?", message: nil, preferredStyle: .actionSheet)
-            let yesAction = UIAlertAction(title: "Yes", style: .default) { _ in
-                self.navigationController?.popViewController(animated: true)
-            }
-            let noAction = UIAlertAction(title: "No", style: .cancel, handler: nil)
-            vc.addAction(yesAction)
-            vc.addAction(noAction)
-            self.present(vc, animated: true, completion: nil)
+            vc.addAction(UIAlertAction(title: "Yes", style: .default) { _ in
+                self?.navigationController?.popViewController(animated: true)
+            })
+            vc.addAction(UIAlertAction(title: "No", style: .cancel, handler: nil))
+            self?.present(vc, animated: true, completion: nil)
         })
     }
 }
