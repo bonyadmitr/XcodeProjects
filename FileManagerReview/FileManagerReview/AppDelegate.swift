18e32cae49c2a0b5349e041e0d9c0ff2635567d5
Yaroslav Bondar <bonyadmitr@gmail.com>
2022-05-11 20:55:40 +0300

    func coordinate(readingItemAt url:

--- FileManagerReview/FileManagerReview/AppDelegate.swift
+++ FileManagerReview/FileManagerReview/AppDelegate.swift
@@ -396,4 +396,25 @@ Syncing with cloud services requires the ability to handle updates to files whil
     }
 }
  
+ 
+ /// NSFileCoordinator+Coordinate
+ extension NSFileCoordinator {
+    func coordinate(readingItemAt url: URL, options: NSFileCoordinator.ReadingOptions = []) throws -> URL {
+        var fcError: NSError?
+        var resultUrl: URL?
+        
+        coordinate(readingItemAt: url, options: .forUploading, error: &fcError) { trueUrl in
+            resultUrl = trueUrl
+        }
+        
+        if let resultUrl = resultUrl {
+            return resultUrl
+        } else if let error = fcError {
+            throw error
+        } else {
+            let unknownError = NSError(domain: NSCocoaErrorDomain, code: NSFeatureUnsupportedError, userInfo: [:])
+            throw unknownError
+        }
+    }
+}
  */
