61eb4512edd3e50f5ac9e1b9f9224ab7ef683fd0
Yaroslav Bondar <bonyadmitr@gmail.com>
2022-06-21 03:05:03 +0300

    var remoteSize

--- FileManagerReview/FileManagerReview/AppDelegate.swift
+++ FileManagerReview/FileManagerReview/AppDelegate.swift
@@ -837,4 +837,28 @@ var extensionInfo: (mime: String, uti: String, desc: String) {
  public init(fileURLWithFileSystemRepresentation path: UnsafePointer<Int8>, isDirectory: Bool, relativeTo baseURL: URL?)
  
  
+ 
+ 
+ 
+ 
+ 
+ 
+extension NSURL {
+    var remoteSize: Int64 {
+        var contentLength: Int64 = NSURLSessionTransferSizeUnknown
+        let request = NSMutableURLRequest(URL: self, cachePolicy: NSURLRequestCachePolicy.ReloadIgnoringLocalAndRemoteCacheData, timeoutInterval: 30.0);
+        request.HTTPMethod = "HEAD";
+        request.setValue("", forHTTPHeaderField: "Accept-Encoding")
+        request.timeoutInterval = 2;
+        let group = dispatch_group_create()
+        dispatch_group_enter(group)
+        NSURLSession.sharedSession().dataTaskWithRequest(request, completionHandler: { (data, response, error) -> Void in
+            contentLength = response?.expectedContentLength ?? NSURLSessionTransferSizeUnknown
+            dispatch_group_leave(group)
+        }).resume()
+        dispatch_group_wait(group, dispatch_time(DISPATCH_TIME_NOW, Int64(5 * NSEC_PER_SEC)))
+        return contentLength
+    }
+}
+
  */
