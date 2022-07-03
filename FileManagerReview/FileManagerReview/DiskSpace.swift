836d092c0686c113c12242d69205893ced295b4e
Yaroslav Bondar <bonyadmitr@gmail.com>
2022-07-03 08:22:20 +0300

    roundedDecimalPlaces

--- FileManagerReview/FileManagerReview/DiskSpace.swift
+++ FileManagerReview/FileManagerReview/DiskSpace.swift
@@ -9,3 +9,15 @@ import Foundation
 
 // TODO: there is DiskSpace project
 /// another possible name `VolumeStorage`
+extension Double {
+    
+    /// Rounds the double to decimal places value
+    /// inspired https://stackoverflow.com/a/32581409/5893286
+    func roundedDecimalPlaces(to places: Int) -> Double {
+        /// instead of `round((value * 100.0)) / 100.0`
+        let divisor = pow(10.0, Double(places))
+        return (self * divisor).rounded() / divisor
+    }
+    
+}
+
