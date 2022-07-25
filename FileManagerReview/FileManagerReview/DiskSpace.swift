91eec992c620b209d8d0dd164372168376d0ebcc
Yaroslav Bondar <bonyadmitr@gmail.com>
2022-07-26 00:22:49 +0300

    func humanReadableByteCount(bytes: Int)

--- FileManagerReview/FileManagerReview/DiskSpace.swift
+++ FileManagerReview/FileManagerReview/DiskSpace.swift
@@ -362,3 +362,10 @@ extension Double {
     
 }
 
+func humanReadableByteCount(bytes: Int) -> String {
+    if (bytes < 1000) { return "\(bytes) B" }
+    let exp = Int(log2(Double(bytes)) / log2(1000.0))
+    let unit = ["KB", "MB", "GB", "TB", "PB", "EB"][exp - 1]
+    let number = Double(bytes) / pow(1000, Double(exp))
+    return String(format: "%.1f %@", number, unit)
+}
