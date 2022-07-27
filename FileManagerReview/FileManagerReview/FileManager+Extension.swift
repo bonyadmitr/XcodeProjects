3af23a7a81e0728f577ae6a1f07546ed418e6816
Yaroslav Bondar <bonyadmitr@gmail.com>
2022-07-28 01:22:48 +0300

    links

--- FileManagerReview/FileManagerReview/FileManager+Extension.swift
+++ FileManagerReview/FileManagerReview/FileManager+Extension.swift
@@ -4,3 +4,9 @@
 //
 //  Created by Yaroslav Bondar on 14.11.2021.
 //
+
+/// apple source by SymbolExtractor https://github.com/Polidea/SiriusObfuscator-SymbolExtractorAndRenamer/tree/master/swift-corelibs-foundation/Foundation
+///
+
+/// `A World Without (NS)FileManager` good article name with working with POSIX api https://appnroll.com/blog/a-world-without-nsfilemanager/
+/// apple source by SymbolExtractor https://github.com/Polidea/SiriusObfuscator-SymbolExtractorAndRenamer/blob/master/swift-corelibs-foundation/Foundation/FileManager.swift
