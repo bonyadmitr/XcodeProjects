261552592498662752c044d27f3f2b5c7e38190b
Yaroslav Bondar <bonyadmitr@gmail.com>
2023-05-26 15:49:45 +0300

    textInputBar.translatesAutoresizingMaskIntoConstraints

--- TextInputBar/TextInputBar/ViewController2.swift
+++ TextInputBar/TextInputBar/ViewController2.swift
@@ -14,5 +14,25 @@ class ViewController2: UIViewController {
     
     private lazy var bottomTextInputBarConstraint = textInputBar.bottomAnchor.constraint(equalTo: view.bottomAnchor)
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(#function)
+        
+        //        if #available(iOS 15.0, *) {
+        //            UITableView.appearance().sectionHeaderTopPadding = 0
+        //        }
+        
+        
+        view.addSubview(textInputBar)
+        
+        textInputBar.translatesAutoresizingMaskIntoConstraints = false
+        textInputBar.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        textInputBar.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        bottomTextInputBarConstraint.isActive = true
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(onTap))
+        view.addGestureRecognizer(tapGesture)
+        
+    }
     
 }
