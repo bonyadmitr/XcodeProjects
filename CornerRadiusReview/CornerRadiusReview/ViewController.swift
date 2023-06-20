5a0538ad70f281c81be70c987e92ced1988c8a70
Yaroslav Bondar <bonyadmitr@gmail.com>
2023-06-20 10:09:34 +0300

    columnLayout

--- CornerRadiusReview/CornerRadiusReview/ViewController.swift
+++ CornerRadiusReview/CornerRadiusReview/ViewController.swift
@@ -7,3 +7,27 @@
 
 import UIKit
 
+    private let columnLayout: UICollectionViewCompositionalLayout = {
+        
+        let deviceInset: CGFloat = 16
+        
+        let columns: CGFloat = 4
+        let inset: CGFloat = 4
+        
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1 / columns),
+                                              heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        item.contentInsets = NSDirectionalEdgeInsets(top: inset, leading: inset, bottom: inset, trailing: inset)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                               heightDimension: .absolute(100))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize,
+                                                       subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = NSDirectionalEdgeInsets(top: inset, leading: deviceInset - inset, bottom: inset, trailing: deviceInset - inset)
+        
+        
+        return UICollectionViewCompositionalLayout(section: section)
+    }()
