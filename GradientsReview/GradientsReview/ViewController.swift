c172f1328ef1896293db9101e851df0b68ba1764
Yaroslav Bondar <bonyadmitr@gmail.com>
2023-11-10 09:29:34 +0300

    GradientView

--- GradientsReview/GradientsReview/ViewController.swift
+++ GradientsReview/GradientsReview/ViewController.swift
@@ -30,3 +30,19 @@ class ViewController: UIViewController {
 //    override class var layerClass: AnyClass { CAGradientLayer.self }
 //    var gradientLayer: CAGradientLayer { layer as! CAGradientLayer }
 //}
+class GradientView: UIView {
+    
+    override class var layerClass: AnyClass {
+        CAGradientLayer.self
+    }
+    
+    var gradientLayer: CAGradientLayer {
+        layer as! CAGradientLayer
+    }
+    
+    /// topToBottom is default direction for CAGradientLayer
+    func setColors(_ colors: [UIColor]) {
+        assert(!colors.isEmpty)
+        gradientLayer.colors = colors.map { $0.cgColor }
+    }
+}
