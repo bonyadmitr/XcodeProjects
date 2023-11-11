2c85ea7db7da79dbb9949a11aae1fdcf92fd2a58
Yaroslav Bondar <bonyadmitr@gmail.com>
2023-11-11 19:46:29 +0300

    temp code

--- GradientsReview/GradientsReview/ViewController.swift
+++ GradientsReview/GradientsReview/ViewController.swift
@@ -143,3 +143,17 @@ class GradientView: UIView {
 //        .init(startPoint: CGPoint(x: 1, y: 0),
 //              endPoint: CGPoint(x: 0, y: 0))
 //    }
+//    
+//    /// default state for CAGradientLayer
+//    static var topToBottom: Self {
+//        .init(startPoint: CGPoint(x: 0, y: 0),
+//              endPoint: CGPoint(x: 0, y: 1))
+//    }
+//    
+//    static var bottomToTop: Self {
+//        .init(startPoint: CGPoint(x: 0, y: 1),
+//              endPoint: CGPoint(x: 0, y: 0))
+//    }
+//    
+//}
+
