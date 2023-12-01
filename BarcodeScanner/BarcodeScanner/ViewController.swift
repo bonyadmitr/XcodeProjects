eb599363b07f5cc10a9effc3622dbef2397b9594
Yaroslav Bondar <bonyadmitr@gmail.com>
2023-12-01 19:25:10 +0300

    country barcode links

---
+++ BarcodeScanner/BarcodeScanner/ViewController.swift
@@ -0,0 +1,8 @@
+/*
+ country barcode
+ 
+ json https://github.com/samayo/country-json/blob/master/src/country-by-barcode-prefix.json
+ json https://github.com/openfoodfacts/openfoodfacts-server/issues/409#issuecomment-397683769
+ Barcodes of Private Labels https://docs.google.com/spreadsheets/d/1wPciFWMvn9K0rIMW17Tgq6IDkCtH11QgoTsSaQ31ghM/edit#gid=1228909259
+ 
+ */
