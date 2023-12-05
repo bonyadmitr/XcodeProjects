d29fc3ace4dcb3fc45ba06af2d4c454800d10402
Yaroslav Bondar <bonyadmitr@gmail.com>
2023-12-05 19:50:44 +0300

    links

--- BarcodeScanner/BarcodeScanner/ViewController.swift
+++ BarcodeScanner/BarcodeScanner/ViewController.swift
@@ -4,5 +4,8 @@
  json https://github.com/samayo/country-json/blob/master/src/country-by-barcode-prefix.json
  json https://github.com/openfoodfacts/openfoodfacts-server/issues/409#issuecomment-397683769
  Barcodes of Private Labels https://docs.google.com/spreadsheets/d/1wPciFWMvn9K0rIMW17Tgq6IDkCtH11QgoTsSaQ31ghM/edit#gid=1228909259
+ js code https://gist.github.com/bitbybit/5797334
+ wiki https://en.wikipedia.org/wiki/List_of_GS1_country_codes
+ ru https://geo.koltyrin.ru/shtrih_kod_strany.php + https://e-dz.ru/shtrih-kod
  
  */
