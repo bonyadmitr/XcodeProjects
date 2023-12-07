cffac7959d12ccfb7d306f4cd0a638dcff259404
Yaroslav Bondar <bonyadmitr@gmail.com>
2023-12-07 12:52:19 +0300

    links

--- BarcodeScanner/BarcodeScanner/ViewController.swift
+++ BarcodeScanner/BarcodeScanner/ViewController.swift
@@ -8,4 +8,8 @@
  wiki https://en.wikipedia.org/wiki/List_of_GS1_country_codes
  ru https://geo.koltyrin.ru/shtrih_kod_strany.php + https://e-dz.ru/shtrih-kod
  
+ VNBarcodeSymbology https://developer.apple.com/documentation/vision/vnbarcodesymbology
+ !!! iOS 16 problem https://stackoverflow.com/q/73867713/5893286
+ 
+ 
  */
