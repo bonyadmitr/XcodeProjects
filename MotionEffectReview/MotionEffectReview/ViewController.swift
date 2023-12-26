1a31c126bbf427a4d5ccd4ffedeff7ce51941d05
Yaroslav Bondar <bonyadmitr@gmail.com>
2023-12-26 10:20:43 +0300

    MotionEffectReview

---
+++ MotionEffectReview/MotionEffectReview/ViewController.swift
@@ -0,0 +1,16 @@
+    func applyMotionEffect(to view: UIView, magnitude: Float) {
+        
+        //#keyPath(UIView.center.x)
+        let xMotion = UIInterpolatingMotionEffect(keyPath: "center.x", type: .tiltAlongHorizontalAxis)
+        xMotion.minimumRelativeValue = -magnitude
+        xMotion.maximumRelativeValue = magnitude
+        
+        let yMotion = UIInterpolatingMotionEffect(keyPath: "center.y", type: .tiltAlongVerticalAxis)
+        yMotion.minimumRelativeValue = -magnitude
+        yMotion.maximumRelativeValue = magnitude
+        
+        let group = UIMotionEffectGroup()
+        group.motionEffects = [xMotion, yMotion]
+        
+        view.addMotionEffect(group)
+    }
