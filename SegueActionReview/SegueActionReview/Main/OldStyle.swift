84df37ed58b350d58dc4e70873889765c43f7fa1
Yaroslav Bondar <bonyadmitr@gmail.com>
2024-02-14 10:20:06 +0300

    Old Style copy

--- SegueActionReview/SegueActionReview/Main/OldStyle.swift
+++ SegueActionReview/SegueActionReview/Main/OldStyle.swift
@@ -168,3 +168,28 @@ StoryboardInitable
     }
 
 */
+
+
+// MARK: - Old Style copy
+
+
+/*
+ 
+final class OldStyleTextController: UIViewController {
+    
+    @IBOutlet private weak var textLabel: UILabel!
+    
+    var text: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        guard let text = text else {
+            assertionFailure()
+            return
+        }
+        
+        textLabel.text = text
+    }
+    
+}
