99162caed57a8cdde303440e7548328f6207af6c
Yaroslav Bondar <bonyadmitr@gmail.com>
2024-02-15 22:09:39 +0300

    StoryboardInitable

--- SegueActionReview/SegueActionReview/Main/StoryboardInitable.swift
+++ SegueActionReview/SegueActionReview/Main/StoryboardInitable.swift
@@ -1,3 +1,13 @@
 protocol StoryboardInitable: UIViewController {
     static func _initFromStoryboard <T: UIViewController>(storyboardName: String?, identifier: String?, bundle: Bundle?, _ creator: @escaping ((NSCoder) -> T?)) -> T?
 }
+extension StoryboardInitable {
+    static func _initFromStoryboard <T: UIViewController>(storyboardName: String? = nil, identifier: String? = nil, bundle: Bundle? = nil, _ creator: @escaping ((NSCoder) -> T?)) -> T? {
+        let storyboard = UIStoryboard(name: storyboardName ?? String(describing: T.self), bundle: bundle)
+        if let identifier {
+            return storyboard.instantiateViewController(identifier: identifier, creator: creator)
+        } else {
+            return storyboard.instantiateInitialViewController(creator: creator)
+        }
+    }
+}
