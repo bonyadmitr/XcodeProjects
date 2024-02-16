71950571fc1f7dcc592bda90fd795d960a378082
Yaroslav Bondar <bonyadmitr@gmail.com>
2024-02-16 07:07:35 +0300

    onShowTextCell

---
+++ SegueActionReview/SegueActionReview/Main/TableController.swift
@@ -0,0 +1,6 @@
+    
+    //@IBSegueAction private func onShowTextCell(_ coder: NSCoder, sender: UITableViewCell) -> UIViewController? {
+    //    let text = sender.textLabel?.text ?? "nil"
+    //    return TextController(coder: coder, text: text)
+    //}
+    
