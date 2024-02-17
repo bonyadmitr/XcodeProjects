9863d60cd89f2bcf0c27e9e561902006243220f9
Yaroslav Bondar <bonyadmitr@gmail.com>
2024-02-17 14:20:46 +0300

    onSUIText

--- SegueActionReview/SegueActionReview/Main/TableController.swift
+++ SegueActionReview/SegueActionReview/Main/TableController.swift
@@ -8,3 +8,9 @@
         TextController(coder: coder, text: text)
     }
     
+    @IBSegueAction private func onSUIText(_ coder: NSCoder, sender: UITableViewCell) -> UIViewController? {
+        let text = sender.textLabel?.text ?? "nil"
+        return SwiftUIView(text: text).hostingController(coder: coder)
+    }
+
+    
