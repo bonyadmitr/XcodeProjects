291116afc39dbdfa96621aa6c2ca47d661ce0659
Yaroslav Bondar <bonyadmitr@gmail.com>
2024-02-17 14:20:57 +0300

    numberOfRowsInSection

--- SegueActionReview/SegueActionReview/Main/TableController.swift
+++ SegueActionReview/SegueActionReview/Main/TableController.swift
@@ -1,3 +1,13 @@
+//
+//  TableController.swift
+//  SegueActionReview
+//
+//  Created by Yaroslav Bondar on 29.01.2024.
+//
+
+import UIKit
+
+final class TableController: UITableViewController {
     
     //@IBSegueAction private func onShowTextCell(_ coder: NSCoder, sender: UITableViewCell) -> UIViewController? {
     //    let text = sender.textLabel?.text ?? "nil"
@@ -14,3 +24,24 @@
     }
 
     
+    // MARK: - override tableView
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        100
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Cell \(indexPath.row + 1)"
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        tableView.visibleCells
+//        tableView.indexPath(for: <#T##UITableViewCell#>)
+        
+//        let text = "performSegue from cell \(indexPath.row + 1)"
+//        performSegue(withIdentifier: "onShowText", sender: text)
+    }
+    
+}
