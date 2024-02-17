7035647f92ef7f73f78fc41b23fb95e7b4f7f65a
Yaroslav Bondar <bonyadmitr@gmail.com>
2024-02-17 14:21:06 +0300

    sharedClose

--- SegueActionReview/SegueActionReview/Main/UIViewController+.swift
+++ SegueActionReview/SegueActionReview/Main/UIViewController+.swift
@@ -11,3 +11,16 @@
             }
 #endif
             navigationController.popViewController(animated: true)
+            
+        } else {
+#if DEBUG
+            if let presentingViewController {
+                print("sharedClose dismiss from: \(String(describing: type(of: self))), to: \(String(describing: type(of: presentingViewController)))")
+            } else {
+                print("sharedClose dismiss no action")
+            }
+#endif
+            /// doc `presentingViewController?.dismiss(animated: true)`
+            dismiss(animated: true)
+        }
+    }
