8e5b79570bbf96597208de967276ebda6b56e8cf
Yaroslav Bondar <bonyadmitr@gmail.com>
2024-02-20 02:19:19 +0300

    comments

--- SegueActionReview/SegueActionReview/Main/ViewController.swift
+++ SegueActionReview/SegueActionReview/Main/ViewController.swift
@@ -7,3 +7,11 @@
 
 import UIKit
 
+/*
+ TODO
+ pass through nav controller
+ tabBar https://useyourloaf.com/blog/using-@ibsegueaction-with-tab-bar-controllers/
+ unwind segue
+ UIStoryboardUnwindSegueSource
+ full exa,ple: func showPreview(coder: NSCoder, sender: Any?, segueIdentifier: String?)
+
